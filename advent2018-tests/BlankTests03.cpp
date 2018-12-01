6cb3d70eda85c54f2556f6e9b358dec971a9a95e
Gary Hildebrand <ghilde@microsoft.com>
2018-12-01 15:25:41 -0800

    Blank class and unit test modules for future work.

---
+++ advent2018-tests/BlankTests03.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "../advent2018/BlankClass.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Advent2018;
+
+namespace advent2018tests
+{
+    TEST_CLASS(Blank03Tests)
+    {
+    public:
+        TEST_METHOD(BlankTest)
+        {
+            BlankClass blank;
+            blank.method1("0");
+            Assert::AreEqual(0, (int)blank.method2());
+        }
+    };
+}
