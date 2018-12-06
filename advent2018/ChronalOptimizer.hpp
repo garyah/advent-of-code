ba497d841e0aff6a4d27dcfc5144292b01f69b23
Gary Hildebrand <ghilde@microsoft.com>
2018-12-05 23:30:30 -0800

    This recursive stuff is bogus!

--- advent2018/ChronalOptimizer.hpp
+++ advent2018/ChronalOptimizer.hpp
@@ -18,16 +18,49 @@ namespace Advent2018
         {
 			unsigned x = 0, y = 0;
             (void)sscanf_s(input, "%u, %u", &x, &y);
+
 			if (m_coordinateGrid.size() < y + 1) resizeGridToHeightNeeded(y + 1);
 			if (m_coordinateGrid[0].size() < x + 1) resizeGridToWidthNeeded(x + 1);
 			++m_idOfLastCoordinate;
-			m_coordinateGrid[y][x] = m_idOfLastCoordinate;
+			//m_coordinateGrid[y][x] = m_idOfLastCoordinate;
+
+			m_coordinateStore.resize(m_idOfLastCoordinate + 1);
+			m_coordinateStore[m_idOfLastCoordinate] = { x, y };
 		}
 
-		unsigned method2()
+		unsigned getSizeOfLargestArea()
         {
-			return 0;
-            //return m_someField;
+			for (unsigned id = 1; id <= m_idOfLastCoordinate; ++id)
+			{
+				unsigned x = m_coordinateStore[id].x;
+				unsigned y = m_coordinateStore[id].y;
+				findProximates(x, y, id);
+
+				m_coordinateCounts.resize(id + 1);
+				m_coordinateCounts[id] = 0;
+			}
+
+			for (unsigned y = 0; y < m_coordinateGrid.size(); ++y)
+			{
+				for (unsigned x = 0; x < m_coordinateGrid[y].size(); ++x)
+				{
+					unsigned id = m_coordinateGrid[y][x];
+					if (id != 0 && id != (unsigned)-1)
+					{
+						if (x != 0 && x != m_coordinateGrid[y].size() - 1 && y != 0 && y != m_coordinateGrid.size() - 1)
+						{
+							m_coordinateCounts[id] += 1;
+						}
+					}
+				}
+			}
+
+			unsigned maxArea = 0;
+			for (unsigned id = 1; id <= m_idOfLastCoordinate; ++id)
+			{
+				if (m_coordinateCounts[id] > maxArea) maxArea = m_coordinateCounts[id];
+			}
+			return maxArea;
         }
 
         //int64_t getSomeField() { return m_someField; }
@@ -35,7 +68,13 @@ namespace Advent2018
     private:
 		typedef std::vector<unsigned> CoordinateGridRow;
 		typedef std::vector<CoordinateGridRow> CoordinateGrid;
-        //typedef std::unordered_set<int64_t> SomeSetType;
+		typedef struct
+		{
+			unsigned x, y;
+		} Coordinate;
+		typedef std::vector<Coordinate> CoordinateStore;
+		typedef std::vector<unsigned> CoordinateCounts;
+		//typedef std::unordered_set<int64_t> SomeSetType;
         //typedef std::unordered_map<int64_t, unsigned> SomeMapType;
 
 		void resizeGridToHeightNeeded(unsigned gridHeightNeeded)
@@ -65,8 +104,36 @@ namespace Advent2018
 			}
 		}
 
+		void findProximates(unsigned x, unsigned y, unsigned id)
+		{
+			if (m_coordinateGrid[y][x] == 0) m_coordinateGrid[y][x] = id;
+			else if (m_coordinateGrid[y][x] == id) return;
+			else if (m_coordinateGrid[y][x] != (unsigned)-1)
+			{
+				// another coordinate got here first, arbitrate
+				auto thisCoordinate = m_coordinateStore[id];
+				auto distanceToThis = abs((int)x - (int)thisCoordinate.x) + abs((int)y - (int)thisCoordinate.y);
+				auto otherCoordinate = m_coordinateStore[m_coordinateGrid[y][x]];
+				auto distanceToOther = abs((int)x - (int)otherCoordinate.x) + abs((int)y - (int)otherCoordinate.y);
+				if (distanceToThis < distanceToOther) m_coordinateGrid[y][x] = id; // new winner
+				else if (distanceToThis == distanceToOther) m_coordinateGrid[y][x] = (unsigned)-1; // mark tie
+			}
+			auto xUpper = m_coordinateGrid[y].size() - 1;
+			auto yUpper = m_coordinateGrid.size() - 1;
+			if (x > 0 && y > 0 && x < xUpper && y < yUpper)                             findProximates(x - 1, y - 1, id);
+			if (y > 0 && y < yUpper)                                                    findProximates(x,     y - 1, id);
+			if (x < xUpper && y > 0 && x > 0 && y < yUpper)                             findProximates(x + 1, y - 1, id);
+			if (x > 0 && x < xUpper)                                                    findProximates(x - 1, y, id);
+			if (x < xUpper && x > 0)                                                    findProximates(x + 1, y, id);
+			if (x > 0 && y < yUpper && x < xUpper && y > 0)                             findProximates(x - 1, y + 1, id);
+			if (y < yUpper && y > 0)                                                    findProximates(x,     y + 1, id);
+			if (x < xUpper && y < yUpper && x > 0 && y > 0)                             findProximates(x + 1, y + 1, id);
+		}
+
 		unsigned m_idOfLastCoordinate;
 		CoordinateGrid m_coordinateGrid;
+		CoordinateStore m_coordinateStore;
+		CoordinateCounts m_coordinateCounts;
 		//int m_someField;
         //SomeSetType m_someSet;
         //SomeMapType m_someMap;
