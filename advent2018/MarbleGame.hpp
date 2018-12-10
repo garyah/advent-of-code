45ea7eefe23037d119bee07f7eb2822d8fb4cdaf
Gary Hildebrand <ghilde@microsoft.com>
2018-12-09 20:20:39 -0800

    Switch to iterators, and all the answers are wrong!

--- advent2018/MarbleGame.hpp
+++ advent2018/MarbleGame.hpp
@@ -9,7 +9,7 @@ namespace Advent2018
     public:
         MarbleGame() :
             m_numPlayers(0), m_lastMarble(0),
-			m_currentPlayer(0), m_indexOfCurrentMarble(0),
+			m_currentPlayer(0), //m_indexOfCurrentMarble(0),
 			m_winningScore(0)
         {}
 
@@ -36,11 +36,11 @@ namespace Advent2018
 				{
 					//std::cout << currentMarble << " = " << currentMarble / 23 << " * 23" << std::endl;
 					advanceCurrentPlayerScore(currentMarble);
-					removeMarble(getMarbleIndexFromCurrent(-7));
+					removeMarble(getMarbleIteratorCounterclockwise(7));
 				}
 				else
 				{
-					addMarbleBetweenTwo(currentMarble, getMarbleIndexFromCurrent(1), getMarbleIndexFromCurrent(2));
+					addMarbleBetweenTwo(currentMarble, getMarbleIteratorClockwise(1), getMarbleIteratorClockwise(2));
 				}
 
 				if (currentMarble % 100000 == 0) std::cout << currentMarble << " ";
@@ -54,10 +54,10 @@ namespace Advent2018
 		void logMarbles()
 		{
 			std::cout << "[" << getCurrentPlayer() << "] ";
-			auto itCurrent = iteratorAtIndex(m_marbles, m_indexOfCurrentMarble);
+			//auto itCurrent = iteratorAtIndex(m_marbles, m_indexOfCurrentMarble);
 			for (auto it = m_marbles.begin(); it != m_marbles.end(); ++it)
 			{
-				auto isCurrent = (it == itCurrent);
+				auto isCurrent = (it == m_iteratorOfCurrentMarble);
 				std::cout << (isCurrent ? "(" : "") << *it << (isCurrent ? ")" : "") << " ";
 			}
 			std::cout << std::endl;
@@ -72,63 +72,92 @@ namespace Advent2018
 			}
 		}
 
-		void removeMarble(size_t indexToRemove)
+		void removeMarble(std::list<unsigned>::iterator iteratorToRemove)
 		{
-			auto it = iteratorAtIndex(m_marbles, indexToRemove);
-			advanceCurrentPlayerScore(*it);
-			(void)m_marbles.erase(it);
-			m_indexOfCurrentMarble = ((indexToRemove == m_marbles.size()) ? 0 : indexToRemove);
+			//auto it = iteratorAtIndex(m_marbles, indexToRemove);
+			advanceCurrentPlayerScore(*iteratorToRemove);
+			m_iteratorOfCurrentMarble = m_marbles.erase(iteratorToRemove);
+			//m_indexOfCurrentMarble = ((indexToRemove == m_marbles.size()) ? 0 : indexToRemove);
 		}
 
-		void addMarbleBetweenTwo(unsigned marbleToAdd, unsigned beforeIndex, unsigned afterIndex)
+		void addMarbleBetweenTwo(unsigned marbleToAdd, std::list<unsigned>::iterator beforeIt, std::list<unsigned>::iterator afterIt)
 		{
 			auto indexOfLast = m_marbles.size() - 1;
-			if (beforeIndex == indexOfLast && afterIndex == 0)
+			if (beforeIt == --(m_marbles.end()) && afterIt == m_marbles.begin())
 			{
 				addMarbleToEnd(marbleToAdd);
 			}
 			else
 			{
-				addMarbleToMiddle(marbleToAdd, afterIndex);
+				addMarbleToMiddle(marbleToAdd, afterIt);
 			}
 		}
 
 		void addMarbleToEnd(unsigned marbleToadd)
 		{
 			m_marbles.push_back(marbleToadd);
-			m_indexOfCurrentMarble = m_marbles.size() - 1;
+			//m_indexOfCurrentMarble = m_marbles.size() - 1;
+			m_iteratorOfCurrentMarble = --(m_marbles.end());
+			// NOW DON'T NEED TO!  m_reverseIteratorOfCurrentMarble = m_marbles.rbegin();
 		}
 
-		void addMarbleToMiddle(unsigned marbleToAdd, size_t indexToAddBefore)
+		void addMarbleToMiddle(unsigned marbleToAdd, std::list<unsigned>::iterator iteratorToAddBefore)
 		{
-			auto it = iteratorAtIndex(m_marbles, indexToAddBefore);
-			(void)m_marbles.insert(it, marbleToAdd);
-			m_indexOfCurrentMarble = indexToAddBefore;
+			//auto it = iteratorAtIndex(m_marbles, indexToAddBefore);
+			m_iteratorOfCurrentMarble = m_marbles.insert(iteratorToAddBefore, marbleToAdd);
+			//m_indexOfCurrentMarble = indexToAddBefore;
+			// CAN'T DO THIS, AND NOW DON'T NEED TO!  m_reverseIteratorOfCurrentMarble = m_iteratorOfCurrentMarble;
 		}
 
-		std::list<unsigned>::iterator iteratorAtIndex(std::list<unsigned>& list, size_t index)
+		//std::list<unsigned>::iterator iteratorAtIndex(std::list<unsigned>& list, size_t index)
+		//{
+		//	auto it = list.begin();
+		//	for (size_t i = 0; i < index; ++i, ++it);
+		//	return it;
+		//}
+
+		//size_t getMarbleIndexFromCurrent(int offset)
+		//{
+		//	auto targetIndex = (int)m_indexOfCurrentMarble + offset;
+		//	auto validIfNonNegativeResult = targetIndex % m_marbles.size();
+		//	//if (offset == -7 && targetIndex >= 0)
+		//	//{
+		//	//	std::cout << "m_indexOfCurrentMarble = " << m_indexOfCurrentMarble << " (-7)-> " << targetIndex;
+		//	//	std::cout << ", modulo size of " << m_marbles.size() << " is " << validIfNonNegativeResult << std::endl;
+		//	//}
+		//	if (targetIndex >= 0) return validIfNonNegativeResult;
+		//	targetIndex += (int)m_marbles.size();
+		//	//{
+		//	//	std::cout << "m_indexOfCurrentMarble = " << m_indexOfCurrentMarble << " (-7)-> " << targetIndex;
+		//	//	std::cout << "  since index underflowed" << std::endl;
+		//	//}
+		//	return targetIndex;
+		//}
+
+		std::list<unsigned>::iterator getMarbleIteratorClockwise(unsigned offset)
 		{
-			auto it = list.begin();
-			for (size_t i = 0; i < index; ++i, ++it);
+			auto i = 0u;
+			auto it = m_iteratorOfCurrentMarble;
+			for (; i < offset && it != m_marbles.end(); ++i, ++it);
+			if (i < offset)
+			{
+				it = --(m_marbles.begin());
+				for (auto j = 0u; j < offset - i; ++j, ++it);
+			}
 			return it;
 		}
 
-		size_t getMarbleIndexFromCurrent(int offset)
+		std::list<unsigned>::iterator getMarbleIteratorCounterclockwise(unsigned offset)
 		{
-			auto targetIndex = (int)m_indexOfCurrentMarble + offset;
-			auto validIfNonNegativeResult = targetIndex % m_marbles.size();
-			//if (offset == -7 && targetIndex >= 0)
-			//{
-			//	std::cout << "m_indexOfCurrentMarble = " << m_indexOfCurrentMarble << " (-7)-> " << targetIndex;
-			//	std::cout << ", modulo size of " << m_marbles.size() << " is " << validIfNonNegativeResult << std::endl;
-			//}
-			if (targetIndex >= 0) return validIfNonNegativeResult;
-			targetIndex += (int)m_marbles.size();
-			//{
-			//	std::cout << "m_indexOfCurrentMarble = " << m_indexOfCurrentMarble << " (-7)-> " << targetIndex;
-			//	std::cout << "  since index underflowed" << std::endl;
-			//}
-			return targetIndex;
+			auto i = 0u;
+			auto it = m_iteratorOfCurrentMarble;
+			for (; i < offset && it != --(m_marbles.begin()); ++i, --it);
+			if (i < offset)
+			{
+				it = m_marbles.end();
+				for (auto j = 0u; j < offset - i; ++j, --it);
+			}
+			return it;
 		}
 
 		void advanceCurrentPlayerScore(unsigned scoreToAdd)
@@ -149,7 +178,7 @@ namespace Advent2018
 		unsigned getNumPlayers() { return m_numPlayers; }
 		unsigned getLastMarble() { return m_lastMarble; }
 		unsigned getCurrentPlayer() { return m_currentPlayer + 1; }
-		size_t getIndexOfCurrentMarble() { return m_indexOfCurrentMarble; }
+		//size_t getIndexOfCurrentMarble() { return m_indexOfCurrentMarble; }
 		unsigned getWinningScore() { return m_winningScore; }
 
     private:
@@ -160,7 +189,9 @@ namespace Advent2018
 		unsigned m_numPlayers;
 		unsigned m_lastMarble;
 		unsigned m_currentPlayer;
-		size_t m_indexOfCurrentMarble;
+		//size_t m_indexOfCurrentMarble;
+		std::list<unsigned>::iterator m_iteratorOfCurrentMarble;
+		//std::list<unsigned>::reverse_iterator m_reverseIteratorOfCurrentMarble;
 		unsigned m_winningScore;
 
 		Marbles m_marbles;
