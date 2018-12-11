6b47dceb594242c9b7eaf891f6b98701504ad7a6
Gary Hildebrand <ghilde@microsoft.com>
2018-12-10 22:17:14 -0800

    Day 11 part 1 done!

--- 11/11b.cpp
+++ 11/11b.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "../advent2018/BlankClass.hpp"
+#include "../advent2018/PowerFinder.hpp"
 
 using namespace Advent2018;
 
 int main()
 {
-	BlankClass blank;
-	do
-	{
-		char line[80 + 1] = { 0 };
-		std::cin.getline(line, _countof(line));
-		//auto xPos = 0, yPos = 0;
-		//auto xVel = 0, yVel = 0;
-		//(void)sscanf_s(line, "position=< %d, %d> velocity=< %d, %d>",
-		//	&xPos, &yPos, &xVel, &yVel);
-		blank.method1(/*xPos, yPos, xVel, yVel*/line);
-	} while (!std::cin.eof());
+	PowerFinder finder;
+	//do
+	//{
+	//	char line[80 + 1] = { 0 };
+	//	std::cin.getline(line, _countof(line));
+	//	//auto xPos = 0, yPos = 0;
+	//	//auto xVel = 0, yVel = 0;
+	//	//(void)sscanf_s(line, "position=< %d, %d> velocity=< %d, %d>",
+	//	//	&xPos, &yPos, &xVel, &yVel);
+	//	finder.method1(/*xPos, yPos, xVel, yVel*/line);
+	//} while (!std::cin.eof());
 
-	//blank.statPoints();
-	std::cout << blank.method2() << std::endl;
+	//finder.statPoints();
+	auto number = 0u;
+	std::cin >> number;
+	//finder.method1(number);
+	std::cout << finder.method2(number) << std::endl;
 	return 0;
 }
---
+++ advent2018/PowerFinder.hpp
@@ -0,0 +1,95 @@
+#include <unordered_map>
+#include <unordered_set>
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+namespace Advent2018
+{
+    class PowerFinder
+    {
+    public:
+        PowerFinder() :
+            m_someField(0)
+        {
+        }
+
+        void method1(unsigned number)
+        {
+            //int64_t someVariable = 0;
+            //(void)sscanf_s(input, "%lld", &someVariable);
+            //m_someField = someVariable;
+        }
+
+        std::string method2(unsigned gridSerial)
+        {
+			auto maxPower = -1000;
+			auto xMax = 0u;
+			auto yMax = 0u;
+			auto x = 1u;
+			auto y = 1u;
+			auto size = 300u - 2u;
+			for (; y <= size; ++y)
+			{
+				for (x = 1u; x <= size; ++x)
+				{
+					auto power3x3 = 0;
+					for (auto yThis = y; yThis < y + 3; ++yThis)
+					{
+						for (auto xThis = x; xThis < x + 3; ++xThis)
+						{
+							auto rackId = xThis + 10u;
+							auto power = rackId * yThis;
+							power += gridSerial;
+							power *= rackId;
+							power = power / 100 % 10;
+							int finalPower = (int)power - 5;
+							if (xThis == 3u && yThis == 5u && gridSerial == 8u
+								|| xThis == 122u && yThis == 79u && gridSerial == 57u
+								|| xThis == 217u && yThis == 196u && gridSerial == 39u
+								|| xThis == 101u && yThis == 153u && gridSerial == 71u)
+							{
+								std::cout << finalPower << std::endl;
+							}
+
+							power3x3 += finalPower;
+						}
+					}
+					if (x == 33u && y == 45u && gridSerial == 18)
+					{
+						std::cout << power3x3 << std::endl;
+					}
+					if (x == 21u && y == 61u && gridSerial == 42)
+					{
+						std::cout << power3x3 << std::endl;
+					}
+					if (power3x3 > maxPower)
+					{
+						maxPower = power3x3;
+						xMax = x;
+						yMax = y;
+					}
+				}
+			}
+
+			std::cout << "x= " << x << " y= " << y << std::endl;
+			std::cout << "maxPower= " << maxPower << std::endl;
+			char result[80 + 1] = { 0 };
+			(void)snprintf(result, _countof(result), "%u,%u", xMax, yMax);
+			std::string resultString(result);
+            return resultString;
+        }
+
+        int64_t getSomeField() { return m_someField; }
+
+    private:
+        typedef std::vector<int64_t> SomeVectorType;
+        typedef std::unordered_set<int64_t> SomeSetType;
+        typedef std::unordered_map<int64_t, unsigned> SomeMapType;
+
+        int64_t m_someField;
+        SomeVectorType m_someVector;
+        SomeSetType m_someSet;
+        SomeMapType m_someMap;
+    };
+}
