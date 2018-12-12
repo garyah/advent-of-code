f2a7851e068a3ad0828e95a1c79b39841845eb23
Gary Hildebrand <ghilde@microsoft.com>
2018-12-11 17:24:50 -0800

    Scripts to drive sample or problem input runs, keeping redirection aspect.  Also dummy input and output for day 12.

---
+++ 12/runb.cmd
@@ -0,0 +1,4 @@
+..\x64\Release\12b.exe < sample_input_1.txt > sample_output_1b.txt
+type sample_output_1b.txt
+REM ..\x64\Release\12b.exe < problem_input.txt > problem_output_b.txt
+REM type problem_output_b.txt
