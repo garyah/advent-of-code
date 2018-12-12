01a2806aba35f0b4dd8a4219f927d19065b9842d
Gary Hildebrand <ghilde@microsoft.com>
2018-12-12 02:13:10 -0800

    Output is good now!  And put the script back to normal.

--- 12/runad.cmd
+++ 12/runad.cmd
@@ -1,4 +1,4 @@
 ..\x64\Debug\12a.exe < sample_input_1.txt > sample_output_1a.txt
 type sample_output_1a.txt
-@REM ..\x64\Debug\12a.exe < problem_input.txt > problem_output_a.txt
-@REM type problem_output_a.txt
+..\x64\Debug\12a.exe < problem_input.txt > problem_output_a.txt
+type problem_output_a.txt
