a96e00acad15b28d03624a93a36732dcd21f29cb
Gary Hildebrand <ghilde@microsoft.com>
2018-12-11 17:37:48 -0800

    Run scripts and output files for more days.

---
+++ 13/runad.cmd
@@ -0,0 +1,4 @@
+..\x64\Debug\13a.exe < sample_input_1.txt > sample_output_1a.txt
+type sample_output_1a.txt
+REM ..\x64\Debug\13a.exe < problem_input.txt > problem_output_a.txt
+REM type problem_output_a.txt
---
+++ 14/runa.cmd
@@ -0,0 +1,4 @@
+..\x64\Release\14a.exe < sample_input_1.txt > sample_output_1a.txt
+type sample_output_1a.txt
+REM ..\x64\Release\14a.exe < problem_input.txt > problem_output_a.txt
+REM type problem_output_a.txt
