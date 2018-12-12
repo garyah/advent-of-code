d9ab91be42c18936b99fe12813d5545a5837ee9f
Gary Hildebrand <ghilde@microsoft.com>
2018-12-12 02:57:52 -0800

    Rename class, a bit late.

--- 12/12a.cpp
+++ 12/12a.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "../advent2018/BlankClass.hpp"
+#include "../advent2018/PlantForecaster.hpp"
 
 using namespace Advent2018;
 
@@ -9,26 +9,26 @@ int main()
 {
 	// unit testing
 	{
-		BlankClass blank;
-		blank.executeRule("..#..", "..#..", '.');
-		std::cout << "executeRule test 1: for ..#.. => .  expected . actual " << blank.getRuleOutput() << std::endl;
+		PlantForecaster forecaster;
+		forecaster.executeRule("..#..", "..#..", '.');
+		std::cout << "executeRule test 1: for ..#.. => .  expected . actual " << forecaster.getRuleOutput() << std::endl;
 	}
 	{
-		BlankClass blank;
-		blank.executeRule("##.##", "##.##", '.');
-		std::cout << "executeRule test 2: for ##.## => .  expected . actual " << blank.getRuleOutput() << std::endl;
+		PlantForecaster forecaster;
+		forecaster.executeRule("##.##", "##.##", '.');
+		std::cout << "executeRule test 2: for ##.## => .  expected . actual " << forecaster.getRuleOutput() << std::endl;
 	}
 	{
-		BlankClass blank;
-		blank.executeRule(".##.#", ".##.#", '#');
-		std::cout << "executeRule test 3: for .##.# => #  expected # actual " << blank.getRuleOutput() << std::endl;
+		PlantForecaster forecaster;
+		forecaster.executeRule(".##.#", ".##.#", '#');
+		std::cout << "executeRule test 3: for .##.# => #  expected # actual " << forecaster.getRuleOutput() << std::endl;
 	}
 
 	char line[200 + 1] = { 0 };
 	std::cin.getline(line, _countof(line));
 	char initialState[120 + 1] = { 0 };
 	(void)sscanf_s(line, "initial state: %s", initialState, 121);
-	BlankClass blank(initialState);
+	PlantForecaster forecaster(initialState);
 	std::cin.getline(line, _countof(line));
 	do
 	{
@@ -36,17 +36,17 @@ int main()
 		char pattern[5 + 1] = { 0 };
 		char outcome = 0;
 		(void)sscanf_s(line, "%s %*s %c", pattern, 6, &outcome);
-		blank.addRule(pattern, outcome);
+		forecaster.addRule(pattern, outcome);
 	} while (!std::cin.eof());
 
-	std::cout << blank.getCurrentState() << std::endl;
+	std::cout << forecaster.getCurrentState() << std::endl;
 	for (int i = 1; i <= 20; ++i)
 	{
-		blank.processState();
-		std::cout << blank.getCurrentState() << std::endl;
-		//std::cout << blank.getNumPlants() << std::endl;
+		forecaster.processState();
+		std::cout << forecaster.getCurrentState() << std::endl;
+		//std::cout << forecaster.getNumPlants() << std::endl;
 	}
-	blank.countPlants();
-	std::cout << blank.getNumPlants() << std::endl;
+	forecaster.countPlants();
+	std::cout << forecaster.getNumPlants() << std::endl;
 	return 0;
 }
---
+++ advent2018/PlantForecaster.hpp
@@ -0,0 +1,99 @@
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace Advent2018
+{
+	class PlantForecaster
+    {
+    public:
+		typedef struct
+		{
+			std::string pattern;
+			char outcome;
+		} Rule;
+		typedef std::vector<Rule> Rules;
+		typedef std::unordered_set<unsigned> SomeSet;
+		typedef std::unordered_map<unsigned, unsigned> SomeMap;
+
+		PlantForecaster(const char *initialState = "") :
+				m_currentState("..."),
+				m_padding(3),
+				m_numPlants(0)
+		{
+			m_currentState += initialState;
+			m_currentState += "...";
+		}
+
+        void addRule(const char *pattern, char outcome)
+        {
+			Rule rule = {pattern, outcome};
+			m_rules.push_back(rule);
+        }
+
+		void processState()
+		{
+			std::string result(m_currentState.substr(0, 2));
+			size_t i = 0;
+			for (i = 2; i < m_currentState.size() - 2; ++i)
+			{
+				executeRules(m_currentState.substr(i - 2, 5).c_str());
+				result += m_ruleOutput;
+			}
+			result += m_currentState.substr(i, 2);
+
+			if (result[2] == '#')
+			{
+				(void)result.insert(result.begin(), '.');
+				++m_padding;
+			}
+			if (result[result.size() - 1 - 2] == '#') result += '.';
+
+			m_currentState.assign(result);
+		}
+
+		void countPlants()
+		{
+			size_t sum = 0, pos = 0;
+			while ((pos = m_currentState.find('#', pos)) != std::string::npos) sum += pos++ - m_padding;
+			m_numPlants = sum;
+		}
+
+		void executeRules(const char *input)
+		{
+			for (auto it = m_rules.begin(); it != m_rules.end(); ++it)
+			{
+				if (executeRule(input, it->pattern.c_str(), it->outcome)) return;
+			}
+			// just to accommodate incomplete rule sets, such as with sample input
+			m_ruleOutput = '.';
+		}
+
+		bool executeRule(const char *input, const char *pattern, char outcome)
+		{
+			bool gotMatch = false;
+			std::string ruleOutput(input);
+			if (ruleOutput.compare(pattern) == 0)
+			{
+				ruleOutput[2] = outcome;
+				gotMatch = true;
+			}
+			m_ruleOutput = ruleOutput[2];
+			return gotMatch;
+		}
+
+		const char *getCurrentState() { return m_currentState.c_str(); }
+		char getRuleOutput() { return m_ruleOutput; }
+		size_t getNumPlants() { return m_numPlants; }
+
+    private:
+		std::string m_currentState;
+		char m_ruleOutput;
+		size_t m_padding;
+		size_t m_numPlants;
+		int m_dummy;
+
+		Rules m_rules;
+    };
+}
