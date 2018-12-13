70600d68276aaa1b7f3ee77b6a7bcd132b091054
Gary Hildebrand <ghilde@microsoft.com>
2018-12-13 00:47:49 -0800

    Restore BlankClass, after having been used for day 12.

--- advent2018/BlankClass.hpp
+++ advent2018/BlankClass.hpp
@@ -1,6 +1,6 @@
-#include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <string>
 #include <vector>
 
 namespace Advent2018
@@ -8,92 +8,49 @@ namespace Advent2018
 	class BlankClass
     {
     public:
-		typedef struct
-		{
-			std::string pattern;
-			char outcome;
-		} Rule;
-		typedef std::vector<Rule> Rules;
+		typedef std::vector<unsigned> SomeVector;
 		typedef std::unordered_set<unsigned> SomeSet;
 		typedef std::unordered_map<unsigned, unsigned> SomeMap;
 
-		BlankClass(const char *initialState = "") :
-				m_currentState("..."),
-				m_padding(3),
-				m_numPlants(0)
+		BlankClass(unsigned field1 = 0,
+				   int field2 = 0,
+				   const char *field3 = "",
+				   int dummy = 0) :
+			m_field1(field1),
+			m_field2(field2),
+			m_field3(field3),
+			m_dummy(dummy)
 		{
-			m_currentState += initialState;
-			m_currentState += "...";
-		}
-
-        void addRule(const char *pattern, char outcome)
-        {
-			Rule rule = {pattern, outcome};
-			m_rules.push_back(rule);
         }
 
-		void processState()
+        void method1(unsigned first = 0u, int second = 0, const char *third = "")
+        {
+        }
+
+		void method2()
+        {
+        }
+
+		void helper1(unsigned first = 0u, int second = 0, const char *third = "")
 		{
-			std::string result(m_currentState.substr(0, 2));
-			size_t i = 0;
-			for (i = 2; i < m_currentState.size() - 2; ++i)
-			{
-				executeRules(m_currentState.substr(i - 2, 5).c_str());
-				result += m_ruleOutput;
-			}
-			result += m_currentState.substr(i, 2);
-
-			if (result[2] == '#')
-			{
-				(void)result.insert(result.begin(), '.');
-				++m_padding;
-			}
-			if (result[result.size() - 1 - 2] == '#') result += '.';
-
-			m_currentState.assign(result);
 		}
 
-		void countPlants()
+		void helper2()
 		{
-			size_t sum = 0, pos = 0;
-			while ((pos = m_currentState.find('#', pos)) != std::string::npos) sum += pos++ - m_padding;
-			m_numPlants = sum;
 		}
 
-		void executeRules(const char *input)
-		{
-			for (auto it = m_rules.begin(); it != m_rules.end(); ++it)
-			{
-				if (executeRule(input, it->pattern.c_str(), it->outcome)) return;
-			}
-			// just to accommodate incomplete rule sets, such as with sample input
-			m_ruleOutput = '.';
-		}
-
-		bool executeRule(const char *input, const char *pattern, char outcome)
-		{
-			bool gotMatch = false;
-			std::string ruleOutput(input);
-			if (ruleOutput.compare(pattern) == 0)
-			{
-				ruleOutput[2] = outcome;
-				gotMatch = true;
-			}
-			m_ruleOutput = ruleOutput[2];
-			return gotMatch;
-		}
-
-		const char *getCurrentState() { return m_currentState.c_str(); }
-		char getRuleOutput() { return m_ruleOutput; }
-		size_t getNumPlants() { return m_numPlants; }
+		unsigned getField1() { return m_field1; }
+		int getField2() { return m_field2; }
+		const char *getField3() { return m_field3.c_str(); }
 
     private:
-		std::string m_currentState;
-		char m_ruleOutput;
-		size_t m_padding;
-		size_t m_numPlants;
+		unsigned m_field1;
+		int m_field2;
+		std::string m_field3;
 		int m_dummy;
 
-		Rules m_rules;
+		SomeVector m_someVector;
+        SomeSet m_someSet;
+        SomeMap m_someMap;
     };
 }
