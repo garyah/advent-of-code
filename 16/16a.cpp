e41fbe1235d6fc8b83b72a6e50d0d969550adc82
Gary Hildebrand <ghilde@microsoft.com>
2018-12-15 11:16:35 -0800

    New blank template set for days 16 through 20.

--- 16/16a.cpp
+++ 16/16a.cpp
@@ -1,20 +1,43 @@
+#include <iostream>
 #include <stdio.h>
 
-#include "../common/InputReader.hpp"
-#include "../advent2017/ProgramDancer.hpp"
+#include "../advent2018/CombatGame.hpp"
 
-using namespace Common;
-using namespace Advent2017;
+using namespace std;
+using namespace Advent2018;
 
 int main()
 {
-    char *line = NULL;
-    while ((line = InputReader::ReadLine()) != NULL)
-    {
-        std::string finalPositions;
-        ProgramDancer::DanceWithNumberOfProgramsAndMoveListAndReturnFinalPositions(
-            16, line, finalPositions);
-        (void)printf("%s\n", finalPositions.data());
-    }
-    return 0;
+	// unit testing
+	{
+		CombatGame game(0, 0, "");
+		auto out1 = 0u;
+		string out2;
+		game.helper1(0u, 0, out1, out2);
+		cout << "helper1 test 1: expected 0 and ||, actual ";
+		cout << out1 << " and |" << out2 << "|" << endl;
+	}
+	{
+		CombatGame game(0, 0, "");
+		game.method1(0u, 0, "");
+		cout << "method1 test 1: expected 0, actual " << game._field1 << endl;
+	}
+
+	CombatGame game(0, 0, "");
+	do
+	{
+		char line[80 + 1] = { 0 };
+		cin.getline(line, _countof(line));
+		auto first = 0u;
+		auto second = 0;
+		char third[10 + 1] = { 0 };
+		(void)sscanf_s(line, "unsigned %u, int %d, string %10s",
+			&first, &second, third, 10);
+		game.method1(first, second, third);
+	} while (!cin.eof());
+
+	game.method2();
+	cout << game._field1 << ", " << game._field2 << ", |" << game._field3 << "|" << endl;
+	cout << game._field1 << endl;
+	return 0;
 }
--- 18/18b.cpp
+++ 18/18b.cpp
@@ -1,20 +1,28 @@
+#include <iostream>
 #include <stdio.h>
 
-#include "../common/InputReader.hpp"
-#include "../advent2017/AssemblyParser.hpp"
+#include "../advent2018/CombatGame.hpp"
 
-using namespace Common;
-using namespace Advent2017;
+using namespace std;
+using namespace Advent2018;
 
 int main()
 {
-    AssemblyParser parser;
-    char *line = NULL;
-    while ((line = InputReader::ReadLine()) != NULL)
-    {
-        parser.parseInstruction(line);
-    }
-    parser.executeProgramV2();
-    (void)printf("%u\n", parser.getnumberOfSends());
-    return 0;
+	CombatGame game(0, 0, "");
+	do
+	{
+		char line[80 + 1] = { 0 };
+		cin.getline(line, _countof(line));
+		auto first = 0u;
+		auto second = 0;
+		char third[10 + 1] = { 0 };
+		(void)sscanf_s(line, "unsigned %u, int %d, string %10s",
+			&first, &second, third, 10);
+		game.method1(first, second, third);
+	} while (!cin.eof());
+
+	game.method2();
+	cout << game._field1 << ", " << game._field2 << ", |" << game._field3 << "|" << endl;
+	cout << game._field1 << endl;
+	return 0;
 }
