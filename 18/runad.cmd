be6c4483c0ca0fe084107ddd2b7faf20dc4cd567
Gary Hildebrand <ghilde@microsoft.com>
2018-12-18 00:31:26 -0800

    Update script, correct problem input for end of file, save (correct) problem output.

--- 18/runad.cmd
+++ 18/runad.cmd
@@ -6,5 +6,5 @@ type sample_output_1a.txt
 @REM type sample_output_3a.txt
 @REM ..\x64\Debug\18a.exe < sample_input_4.txt > sample_output_4a.txt
 @REM type sample_output_4a.txt
-@REM ..\x64\Debug\18a.exe < problem_input.txt > problem_output_a.txt
-@REM type problem_output_a.txt
+..\x64\Debug\18a.exe < problem_input.txt > problem_output_a.txt
+type problem_output_a.txt
