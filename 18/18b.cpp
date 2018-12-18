5b4e0b269973207e2d787e4261fa83b3d2de767d
Gary Hildebrand <ghilde@microsoft.com>
2018-12-17 22:17:49 -0800

    Problem input, prep for day 18 part 1.

--- 18/18b.cpp
+++ 18/18b.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "../advent2018/BlankClass.hpp"
+#include "../advent2018/LumberCalculator.hpp"
 
 using namespace std;
 using namespace Advent2018;
 
 int main()
 {
-	BlankClass blank(0, 0, "");
+	LumberCalculator calculator(0, 0, "");
 	do
 	{
 		char line[80 + 1] = { 0 };
@@ -18,11 +18,11 @@ int main()
 		char third[10 + 1] = { 0 };
 		(void)sscanf_s(line, "unsigned %u, int %d, string %10s",
 			&first, &second, third, 10);
-		blank.method1(first, second, third);
+		calculator.method1(first, second, third);
 	} while (!cin.eof());
 
-	blank.method2();
-	cout << blank._field1 << ", " << blank._field2 << ", |" << blank._field3 << "|" << endl;
-	cout << blank._field1 << endl;
+	calculator.method2();
+	cout << calculator._field1 << ", " << calculator._field2 << ", |" << calculator._field3 << "|" << endl;
+	cout << calculator._field1 << endl;
 	return 0;
 }
