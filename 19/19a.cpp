9730cc03f4bc92bd13f2f2e567c6b798d618349d
Gary Hildebrand <ghilde@microsoft.com>
2018-12-18 21:48:56 -0800

    Prep for day 19.

--- 19/19a.cpp
+++ 19/19a.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "../advent2018/CombatGame.hpp"
+#include "../advent2018/BlankClass.hpp"
 
 using namespace std;
 using namespace Advent2018;
@@ -10,20 +10,20 @@ int main()
 {
 	// unit testing
 	{
-		CombatGame game(0, 0, "");
+		BlankClass blank(0, 0, "");
 		auto out1 = 0u;
 		string out2;
-		game.helper1(0u, 0, out1, out2);
+		blank.helper1(0u, 0, out1, out2);
 		cout << "helper1 test 1: expected 0 and ||, actual ";
 		cout << out1 << " and |" << out2 << "|" << endl;
 	}
 	{
-		CombatGame game(0, 0, "");
-		game.method1(0u, 0, "");
-		cout << "method1 test 1: expected 0, actual " << game._field1 << endl;
+		BlankClass blank(0, 0, "");
+		blank.method1(0u, 0, "");
+		cout << "method1 test 1: expected 0, actual " << blank._field1 << endl;
 	}
 
-	CombatGame game(0, 0, "");
+	BlankClass blank(0, 0, "");
 	do
 	{
 		char line[80 + 1] = { 0 };
@@ -33,11 +33,11 @@ int main()
 		char third[10 + 1] = { 0 };
 		(void)sscanf_s(line, "unsigned %u, int %d, string %10s",
 			&first, &second, third, 10);
-		game.method1(first, second, third);
+		blank.method1(first, second, third);
 	} while (!cin.eof());
 
-	game.method2();
-	cout << game._field1 << ", " << game._field2 << ", |" << game._field3 << "|" << endl;
-	cout << game._field1 << endl;
+	blank.method2();
+	cout << blank._field1 << ", " << blank._field2 << ", |" << blank._field3 << "|" << endl;
+	cout << blank._field1 << endl;
 	return 0;
 }
