3573e645afbd27e307de2134c48859ba72eb317b
Gary Hildebrand <ghilde@microsoft.com>
2018-12-19 21:06:19 -0800

    Prep for day 20.

--- 20/20b.cpp
+++ 20/20b.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "../advent2018/CombatGame.hpp"
+#include "../advent2018/BlankClass.hpp"
 
 using namespace std;
 using namespace Advent2018;
 
 int main()
 {
-	CombatGame game(0, 0, "");
+	BlankClass blank(0, 0, "");
 	do
 	{
 		char line[80 + 1] = { 0 };
@@ -18,11 +18,11 @@ int main()
 		char third[10 + 1] = { 0 };
 		(void)sscanf_s(line, "unsigned %u, int %d, string %10s",
 			&first, &second, third, 10);
-		game.method1(first, second, third);
+		blank.method1(first, second, third);
 	} while (!cin.eof());
 
-	game.method2();
-	cout << game._field1 << ", " << game._field2 << ", |" << game._field3 << "|" << endl;
-	cout << game._field1 << endl;
+	blank.method2();
+	cout << blank._field1 << ", " << blank._field2 << ", |" << blank._field3 << "|" << endl;
+	cout << blank._field1 << endl;
 	return 0;
 }
