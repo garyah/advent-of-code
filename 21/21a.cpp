3dae6035281e6534f95f59692e2f953d01da2d4f
Gary Hildebrand <ghilde@microsoft.com>
2018-12-20 21:24:07 -0800

    Switch drivers for day 21 to continue use of assembly parser from days 16 and 19.

--- 21/21a.cpp
+++ 21/21a.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "../advent2018/BlankClass.hpp"
+#include "../advent2018/AssemblyParser.hpp"
 
 using namespace std;
 using namespace Advent2018;
@@ -9,35 +9,33 @@ using namespace Advent2018;
 int main()
 {
 	// unit testing
-	{
-		BlankClass blank(0, 0, "");
-		auto out1 = 0u;
-		string out2;
-		blank.helper1(0u, 0, out1, out2);
-		cout << "helper1 test 1: expected 0 and ||, actual ";
-		cout << out1 << " and |" << out2 << "|" << endl;
-	}
-	{
-		BlankClass blank(0, 0, "");
-		blank.method1(0u, 0, "");
-		cout << "method1 test 1: expected 0, actual " << blank._field1 << endl;
-	}
+	//{
+	//	BlankClass blank(0, 0, "");
+	//	auto out1 = 0u;
+	//	string out2;
+	//	blank.helper1(0u, 0, out1, out2);
+	//	cout << "helper1 test 1: expected 0 and ||, actual ";
+	//	cout << out1 << " and |" << out2 << "|" << endl;
+	//}
+	//{
+	//	BlankClass blank(0, 0, "");
+	//	blank.method1(0u, 0, "");
+	//	cout << "method1 test 1: expected 0, actual " << blank._field1 << endl;
+	//}
 
-	BlankClass blank(0, 0, "");
+	AssemblyParser parser;
+	char line[80 + 1] = { 0 };
+	cin.getline(line, _countof(line));
+	auto ipRegNum = 0u;
+	(void)sscanf_s(line, "#ip %u", &ipRegNum);
+	parser.setIpRegNum(ipRegNum);
 	do
 	{
-		char line[80 + 1] = { 0 };
 		cin.getline(line, _countof(line));
-		auto first = 0u;
-		auto second = 0;
-		char third[10 + 1] = { 0 };
-		(void)sscanf_s(line, "unsigned %u, int %d, string %10s",
-			&first, &second, third, 10);
-		blank.method1(first, second, third);
+		parser.parseInstruction(line);
 	} while (!cin.eof());
 
-	blank.method2();
-	cout << blank._field1 << ", " << blank._field2 << ", |" << blank._field3 << "|" << endl;
-	cout << blank._field1 << endl;
+	parser.executeProgram(1);
+	cout << parser.getRegisterZero() << endl;
 	return 0;
 }
--- 21/21b.cpp
+++ 21/21b.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "../advent2018/BlankClass.hpp"
+#include "../advent2018/AssemblyParser.hpp"
 
 using namespace std;
 using namespace Advent2018;
 
 int main()
 {
-	BlankClass blank(0, 0, "");
+	AssemblyParser parser;
+	char line[80 + 1] = { 0 };
+	cin.getline(line, _countof(line));
+	auto ipRegNum = 0u;
+	(void)sscanf_s(line, "#ip %u", &ipRegNum);
+	parser.setIpRegNum(ipRegNum);
 	do
 	{
-		char line[80 + 1] = { 0 };
 		cin.getline(line, _countof(line));
-		auto first = 0u;
-		auto second = 0;
-		char third[10 + 1] = { 0 };
-		(void)sscanf_s(line, "unsigned %u, int %d, string %10s",
-			&first, &second, third, 10);
-		blank.method1(first, second, third);
+		parser.parseInstruction(line);
 	} while (!cin.eof());
 
-	blank.method2();
-	cout << blank._field1 << ", " << blank._field2 << ", |" << blank._field3 << "|" << endl;
-	cout << blank._field1 << endl;
+	parser.executeProgram(1);
+	cout << parser.getRegisterZero() << endl;
 	return 0;
 }
