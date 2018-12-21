d65f0d66c6b99041b9ea4440d5fa4af71f705e2a
Gary Hildebrand <ghilde@microsoft.com>
2018-12-20 21:27:42 -0800

    Run scripts changed again, to not redirect output.

--- 21/runa.cmd
+++ 21/runa.cmd
@@ -1,5 +1,5 @@
-..\x64\Release\21a.exe < sample_input_1.txt > sample_output_1a.txt
-type sample_output_1a.txt
+@REM ..\x64\Release\21a.exe < sample_input_1.txt > sample_output_1a.txt
+@REM type sample_output_1a.txt
 @REM ..\x64\Release\21a.exe < sample_input_2.txt > sample_output_2a.txt
 @REM type sample_output_1a.txt
 @REM ..\x64\Release\21a.exe < sample_input_3.txt > sample_output_3a.txt
@@ -7,4 +7,5 @@ type sample_output_1a.txt
 @REM ..\x64\Release\21a.exe < sample_input_4.txt > sample_output_4a.txt
 @REM type sample_output_1a.txt
 @REM ..\x64\Release\21a.exe < problem_input.txt > problem_output_a.txt
+..\x64\Release\21a.exe < problem_input.txt
 @REM type problem_output_a.txt
