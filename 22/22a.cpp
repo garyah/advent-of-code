9759f7a6281623f530c05dfe796ecf1adbde30c1
Gary Hildebrand <ghilde@microsoft.com>
2018-12-21 21:57:01 -0800

    Prep day 22.

--- 22/22a.cpp
+++ 22/22a.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "../advent2018/BlankClass.hpp"
+#include "../advent2018/CaveAssessor.hpp"
 
 using namespace std;
 using namespace Advent2018;
@@ -10,20 +10,20 @@ int main()
 {
 	// unit testing
 	{
-		BlankClass blank(0, 0, "");
+		CaveAssessor assessor(0, 0, "");
 		auto out1 = 0u;
 		string out2;
-		blank.helper1(0u, 0, out1, out2);
+		assessor.helper1(0u, 0, out1, out2);
 		cout << "helper1 test 1: expected 0 and ||, actual ";
 		cout << out1 << " and |" << out2 << "|" << endl;
 	}
 	{
-		BlankClass blank(0, 0, "");
-		blank.method1(0u, 0, "");
-		cout << "method1 test 1: expected 0, actual " << blank._field1 << endl;
+		CaveAssessor assessor(0, 0, "");
+		assessor.method1(0u, 0, "");
+		cout << "method1 test 1: expected 0, actual " << assessor._field1 << endl;
 	}
 
-	BlankClass blank(0, 0, "");
+	CaveAssessor assessor(0, 0, "");
 	do
 	{
 		char line[80 + 1] = { 0 };
@@ -33,11 +33,11 @@ int main()
 		char third[10 + 1] = { 0 };
 		(void)sscanf_s(line, "unsigned %u, int %d, string %10s",
 			&first, &second, third, 10);
-		blank.method1(first, second, third);
+		assessor.method1(first, second, third);
 	} while (!cin.eof());
 
-	blank.method2();
-	cout << blank._field1 << ", " << blank._field2 << ", |" << blank._field3 << "|" << endl;
-	cout << blank._field1 << endl;
+	assessor.method2();
+	cout << assessor._field1 << ", " << assessor._field2 << ", |" << assessor._field3 << "|" << endl;
+	cout << assessor._field1 << endl;
 	return 0;
 }
--- 22/22b.cpp
+++ 22/22b.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "../advent2018/BlankClass.hpp"
+#include "../advent2018/CaveAssessor.hpp"
 
 using namespace std;
 using namespace Advent2018;
 
 int main()
 {
-	BlankClass blank(0, 0, "");
+	CaveAssessor assessor(0, 0, "");
 	do
 	{
 		char line[80 + 1] = { 0 };
@@ -18,11 +18,11 @@ int main()
 		char third[10 + 1] = { 0 };
 		(void)sscanf_s(line, "unsigned %u, int %d, string %10s",
 			&first, &second, third, 10);
-		blank.method1(first, second, third);
+		assessor.method1(first, second, third);
 	} while (!cin.eof());
 
-	blank.method2();
-	cout << blank._field1 << ", " << blank._field2 << ", |" << blank._field3 << "|" << endl;
-	cout << blank._field1 << endl;
+	assessor.method2();
+	cout << assessor._field1 << ", " << assessor._field2 << ", |" << assessor._field3 << "|" << endl;
+	cout << assessor._field1 << endl;
 	return 0;
 }
