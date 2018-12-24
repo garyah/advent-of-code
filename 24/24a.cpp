8e4e335a1e85c993de12738449cbff772f470e5a
Gary Hildebrand <ghilde@microsoft.com>
2018-12-24 00:57:22 -0800

    Prep day 24.

--- 24/24a.cpp
+++ 24/24a.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "../advent2018/BlankClass.hpp"
+#include "../advent2018/ImmuneSimulator.hpp"
 
 using namespace std;
 using namespace Advent2018;
@@ -9,21 +9,21 @@ using namespace Advent2018;
 int main()
 {
 	// unit testing
-	{
-		BlankClass blank(0, 0, "");
-		auto out1 = 0u;
-		string out2;
-		blank.helper1(0u, 0, out1, out2);
-		cout << "helper1 test 1: expected 0 and ||, actual ";
-		cout << out1 << " and |" << out2 << "|" << endl;
-	}
-	{
-		BlankClass blank(0, 0, "");
-		blank.method1(0u, 0, "");
-		cout << "method1 test 1: expected 0, actual " << blank._field1 << endl;
-	}
+	//{
+	//	ImmuneSimulator simulator(0, 0, "");
+	//	auto out1 = 0u;
+	//	string out2;
+	//	simulator.helper1(0u, 0, out1, out2);
+	//	cout << "helper1 test 1: expected 0 and ||, actual ";
+	//	cout << out1 << " and |" << out2 << "|" << endl;
+	//}
+	//{
+	//	ImmuneSimulator simulator(0, 0, "");
+	//	simulator.method1(0u, 0, "");
+	//	cout << "method1 test 1: expected 0, actual " << simulator._field1 << endl;
+	//}
 
-	BlankClass blank(0, 0, "");
+	ImmuneSimulator simulator(0, 0, "");
 	do
 	{
 		char line[80 + 1] = { 0 };
@@ -33,11 +33,11 @@ int main()
 		char third[10 + 1] = { 0 };
 		(void)sscanf_s(line, "unsigned %u, int %d, string %10s",
 			&first, &second, third, 10);
-		blank.method1(first, second, third);
+		simulator.method1(first, second, third);
 	} while (!cin.eof());
 
-	blank.method2();
-	cout << blank._field1 << ", " << blank._field2 << ", |" << blank._field3 << "|" << endl;
-	cout << blank._field1 << endl;
+	simulator.method2();
+	cout << simulator._field1 << ", " << simulator._field2 << ", |" << simulator._field3 << "|" << endl;
+	cout << simulator._field1 << endl;
 	return 0;
 }
