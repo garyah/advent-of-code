a01cbe7fe916b5a70cffa392562ca55bd91c327a
Gary Hildebrand <ghilde@microsoft.com>
2018-12-25 18:38:04 -0800

    Prep day 25.

--- 25/25a.cpp
+++ 25/25a.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "../advent2018/BlankClass.hpp"
+#include "../advent2018/ConstellationFinder.hpp"
 
 using namespace std;
 using namespace Advent2018;
@@ -9,35 +9,32 @@ using namespace Advent2018;
 int main()
 {
 	// unit testing
-	{
-		BlankClass blank(0, 0, "");
-		auto out1 = 0u;
-		string out2;
-		blank.helper1(0u, 0, out1, out2);
-		cout << "helper1 test 1: expected 0 and ||, actual ";
-		cout << out1 << " and |" << out2 << "|" << endl;
-	}
-	{
-		BlankClass blank(0, 0, "");
-		blank.method1(0u, 0, "");
-		cout << "method1 test 1: expected 0, actual " << blank._field1 << endl;
-	}
+	//{
+	//	ConstellationFinder finder(0, 0, "");
+	//	auto out1 = 0u;
+	//	string out2;
+	//	finder.helper1(0u, 0, out1, out2);
+	//	cout << "helper1 test 1: expected 0 and ||, actual ";
+	//	cout << out1 << " and |" << out2 << "|" << endl;
+	//}
+	//{
+	//	ConstellationFinder finder(0, 0, "");
+	//	finder.addPoint(0u, 0, "");
+	//	cout << "addPoint test 1: expected 0, actual " << finder._field1 << endl;
+	//}
 
-	BlankClass blank(0, 0, "");
+	ConstellationFinder finder(0, 0, "");
 	do
 	{
 		char line[80 + 1] = { 0 };
 		cin.getline(line, _countof(line));
-		auto first = 0u;
-		auto second = 0;
-		char third[10 + 1] = { 0 };
-		(void)sscanf_s(line, "unsigned %u, int %d, string %10s",
-			&first, &second, third, 10);
-		blank.method1(first, second, third);
+		auto x = 0, y = 0, z = 0, t = 0;
+		(void)sscanf_s(line, "%u, %u, %u, %u", &x, &y, &z, &t);
+		finder.addPoint(x, y, z, t);
 	} while (!cin.eof());
 
-	blank.method2();
-	cout << blank._field1 << ", " << blank._field2 << ", |" << blank._field3 << "|" << endl;
-	cout << blank._field1 << endl;
+	//finder.method2();
+	//cout << finder._field1 << ", " << finder._field2 << ", |" << finder._field3 << "|" << endl;
+	//cout << finder._field1 << endl;
 	return 0;
 }
--- 25/25b.cpp
+++ 25/25b.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "../advent2018/BlankClass.hpp"
+#include "../advent2018/ConstellationFinder.hpp"
 
 using namespace std;
 using namespace Advent2018;
 
 int main()
 {
-	BlankClass blank(0, 0, "");
+	ConstellationFinder finder(0, 0, "");
 	do
 	{
 		char line[80 + 1] = { 0 };
 		cin.getline(line, _countof(line));
-		auto first = 0u;
-		auto second = 0;
-		char third[10 + 1] = { 0 };
-		(void)sscanf_s(line, "unsigned %u, int %d, string %10s",
-			&first, &second, third, 10);
-		blank.method1(first, second, third);
+		auto x = 0, y = 0, z = 0, t = 0;
+		(void)sscanf_s(line, "%u, %u, %u, %u", &x, &y, &z, &t);
+		finder.addPoint(x, y, z, t);
 	} while (!cin.eof());
 
-	blank.method2();
-	cout << blank._field1 << ", " << blank._field2 << ", |" << blank._field3 << "|" << endl;
-	cout << blank._field1 << endl;
+	//finder.method2();
+	//cout << finder._field1 << ", " << finder._field2 << ", |" << finder._field3 << "|" << endl;
+	//cout << finder._field1 << endl;
 	return 0;
 }
