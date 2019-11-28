07a9f08ae019deaa47725ce57633a65a80234e2a
Gary Hildebrand <garyah@gmail.com>
2019-11-28 00:45:21 -0800

    Get rid of commented out test code.

--- advent2019/01/puzzle.js
+++ advent2019/01/puzzle.js
@@ -10,16 +10,6 @@ Puzzle.prototype.parse = function(lines) {
 Puzzle.prototype.solve = function(data) {
   var total = 0;
   for (var num of data) {
-    //console.log(num, parseInt(num));
-    // if (num == NaN) console.log("num == NaN");
-    // if (num === NaN) console.log("num === NaN");
-    // if (num != NaN) console.log("num != NaN");
-    // if (num !== NaN) console.log("num !== NaN");
-    // var parsedNum = parseInt(num);
-    // if (parsedNum == NaN) console.log("parsedNum == NaN");
-    // if (parsedNum === NaN) console.log("parsedNum === NaN");
-    // if (parsedNum != NaN) console.log("parsedNum != NaN");
-    // if (parsedNum !== NaN) console.log("parsedNum !== NaN");
     if (num === num) total += num;
   }
   return total;
