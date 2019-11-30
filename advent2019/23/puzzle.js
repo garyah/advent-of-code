93c5583f49e22ea1746412bc3cde5ea1cf383dd2
Gary Hildebrand <garyah@gmail.com>
2019-11-30 12:48:43 -0800

    Change order of functions in puzzle.
    Just to reinforce notion of solve the puzzle, before parsing work.

--- advent2019/23/puzzle.js
+++ advent2019/23/puzzle.js
@@ -1,11 +1,11 @@
 function Puzzle() {
 }
-Puzzle.prototype.parse = (lines) => {
-  return lines.map((line) => parseInt(line)).filter((num) => num === num);
-};
 Puzzle.prototype.solve = (data) => {
   return data.reduce((sum, num) => sum + num);
 }
+Puzzle.prototype.parse = (lines) => {
+  return lines.map((line) => parseInt(line)).filter((num) => num === num);
+};
 Puzzle.prototype.solve_p2 = (changes) => {
   let seenFrequencies = new Set([0]);
   let frequency = 0;
