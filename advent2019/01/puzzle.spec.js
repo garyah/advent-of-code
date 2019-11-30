1783423c7e0e4f245a44dfb9b4a6b4773f26487b
Gary Hildebrand <garyah@gmail.com>
2019-11-30 10:16:12 -0800

    Refactor to re-order tests, consolidate source.

--- advent2019/01/puzzle.spec.js
+++ advent2019/01/puzzle.spec.js
@@ -3,21 +3,20 @@ describe("puzzle", function() {
   var Puzzle = require('./puzzle');
   var parser = new Parser();
   var puzzle = new Puzzle();
-  xit("should be able solve with my input", function(done) {
-    // "adventYYYY/DD/input.txt" for specific file, undefined for stdin
-    parser.readLines(undefined, (lines) => {
-      var data = puzzle.parse(lines);
-      var answer = puzzle.solve(data);
-      expect(answer).toEqual(0);
-      done();
-    });
-  })
-  it("should be able to parse input", function() {
-    var data = puzzle.parse('+1 +3 +2'.split(' '));
-    expect(data).toEqual([1, 3, 2]);
-  });
   it("should be able to solve puzzle", function() {
     var data = [1, 1, -2];
     expect(puzzle.solve(data)).toEqual(0);
   });
+  it("should be able to parse input", function() {
+    var data = puzzle.parse('+1 +3 +2'.split(' '));
+    expect(data).toEqual([1, 3, 2]);
+  });
+  xit("should be able solve with my input", function(done) {
+    // "adventYYYY/DD/input.txt" for specific file, undefined for stdin
+    parser.readLines(undefined, (lines) => {
+      var data = puzzle.parse(lines);
+      expect(puzzle.solve(data)).toEqual(0);
+      done();
+    });
+  })
 });
