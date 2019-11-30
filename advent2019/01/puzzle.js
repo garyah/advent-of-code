54e9dbd592254f8cd6c3b3deb0c2bf88a265d440
Gary Hildebrand <garyah@gmail.com>
2019-11-30 12:31:02 -0800

    Solution for 2018 day 1 part 2 becomes template for puzzles.

--- advent2019/01/puzzle.js
+++ advent2019/01/puzzle.js
@@ -6,4 +6,15 @@ Puzzle.prototype.parse = (lines) => {
 Puzzle.prototype.solve = (data) => {
   return data.reduce((sum, num) => sum + num);
 }
+Puzzle.prototype.solve_p2 = (changes) => {
+  let seenFrequencies = new Set([0]);
+  let frequency = 0;
+  while (true) {
+    for (const change of changes) {
+      frequency += change;
+      if (seenFrequencies.has(frequency)) return frequency;
+      seenFrequencies.add(frequency);
+    }
+  }
+}
 module.exports = Puzzle;
