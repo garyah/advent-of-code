369b2ed45e0b89f66f21373dfc2e18587cdc62ee
Gary Hildebrand <garyah@gmail.com>
2019-12-01 22:25:22 -0800

    Solve 2019 day 2 part 2.
    Solution completion and commit delayed by strange problem where VS Code cannot debug Jasmine tests anymore, even with signout and signin of computer.

--- advent2019/02/puzzle.js
+++ advent2019/02/puzzle.js
@@ -5,27 +5,35 @@ Puzzle.prototype.solve = (program = []) => {
     if (program[ip] === 99) break;
     if (program[ip] === 1) program[program[ip+3]] = program[program[ip+1]] + program[program[ip+2]];
     if (program[ip] === 2) program[program[ip+3]] = program[program[ip+1]] * program[program[ip+2]];
-    console.log(program[ip]);
+    // console.log(program[ip]);
     ip += 4;
   }
-  return program;
+  return program[0];
   // return data.reduce((sum, num) => {
   //   return sum + num;
   // }, 0);
 }
 Puzzle.prototype.parse = (lines = ['']) => {
-  //return lines[0].split(',').parseInt();
   return lines[0].split(',').map((value) => parseInt(value)).filter((num) => num === num);
 };
-Puzzle.prototype.solve_p2 = (data) => {
-  return 0;
-  let seenFrequencies = new Set([0]);
-  let frequency = 0;
-  while (true) {
-    for (const change of data) {
-      frequency += change;
-      if (seenFrequencies.has(frequency)) return frequency;
-      seenFrequencies.add(frequency);
+Puzzle.prototype.solve_p2 = (program = []) => {
+  // save
+  let savedData = [];
+  for (const code of program) savedData.push(code);
+  //console.log(savedData.length, savedData[0]);
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      program[1] = noun;
+      program[2] = verb;
+      const possibleGood = Puzzle.prototype.solve(program);
+      //if (noun % 100 === 0 && verb % 10 === 0) console.log(possibleGood);
+      if (possibleGood === 19690720) {
+        console.log("found it!  noun = " + noun + " and verb = " + verb);
+        return 100 * noun + verb;
+      }
+      // restore
+      program = [];
+      for (const code of savedData) program.push(code);
     }
   }
 }
--- advent2019/02/puzzle.spec.js
+++ advent2019/02/puzzle.spec.js
@@ -17,24 +17,26 @@ describe("2019 day 2", function() {
     }
     done();
   });
-  it("should be able to solve puzzle", () => {
-    const data = [[1,9,10,3,2,3,11,0,99,30,40,50], [2,3,0,3,99], [2,4,4,5,99,0]];
-    const actual = data.map((data) => puzzle.solve(data));
-    const expected = [[3500,9,10,70,
-      2,3,11,0,
-      99,
-      30,40,50], [2,3,0,6,99], [2,4,4,5,99,9801]];
-    expect(actual).toEqual(expected);
-  });
-  it("should be able to parse input", () => {
-    const data = puzzle.parse(['1,2,3']);
-    expect(data).toEqual([1, 2, 3]);
-  });
+  // it("should be able to solve puzzle", () => {
+  //   const data = [[1,9,10,3,2,3,11,0,99,30,40,50], [2,3,0,3,99], [2,4,4,5,99,0]];
+  //   const actual = data.map((data) => puzzle.solve(data));
+  //   const expected = [[3500,9,10,70,
+  //     2,3,11,0,
+  //     99,
+  //     30,40,50], [2,3,0,6,99], [2,4,4,5,99,9801]];
+  //   expect(actual).toEqual(expected);
+  // });
+  // it("should be able to parse input", () => {
+  //   const data = puzzle.parse(['1,2,3']);
+  //   expect(data).toEqual([1, 2, 3]);
+  // });
   it("should be able solve puzzle with my input", () => {
     const data = puzzle.parse(lines);
+    data[1] = 12;
+    data[2] = 2;
     const answer = puzzle.solve(data);
-    console.log("part 1 answer is " + answer[0]);
-    expect(answer[0]).toEqual(6327510);
+    console.log("part 1 answer is " + answer);
+    expect(answer).toEqual(6327510);
   });
   // it("should be able to solve puzzle part 2", () => {
   //   const data = [[], [], []];
@@ -42,10 +44,10 @@ describe("2019 day 2", function() {
   //   const expected = [1, 1, 1];
   //   expect(actual).toEqual(expected);
   // });
-  // it("should be able solve puzzle part 2 with my input", () => {
-  //   const data = puzzle.parse(lines);
-  //   const answer = puzzle.solve_p2(data);
-  //   console.log("part 2 answer is " + answer);
-  //   expect(answer).toEqual(0);
-  // });
+  it("should be able solve puzzle part 2 with my input", () => {
+    const data = puzzle.parse(lines);
+    const answer = puzzle.solve_p2(data);
+    console.log("part 2 answer is " + answer);
+    expect(answer).toEqual(4112);
+  });
 });
