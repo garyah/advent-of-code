1ca10631f73d8deb067a3b9695015179c0ebf3a1
Gary Hildebrand <garyah@gmail.com>
2019-12-01 20:29:50 -0800

    Copy template to remaining days.

--- advent2019/15/puzzle.js
+++ advent2019/15/puzzle.js
@@ -1,16 +1,19 @@
 function Puzzle() {
 }
 Puzzle.prototype.solve = (data) => {
-  return data.reduce((sum, num) => sum + num);
+  return data.reduce((sum, num) => {
+    return sum + num;
+  }, 0);
 }
 Puzzle.prototype.parse = (lines) => {
   return lines.map((line) => parseInt(line)).filter((num) => num === num);
 };
-Puzzle.prototype.solve_p2 = (changes) => {
+Puzzle.prototype.solve_p2 = (data) => {
+  return 0;
   let seenFrequencies = new Set([0]);
   let frequency = 0;
   while (true) {
-    for (const change of changes) {
+    for (const change of data) {
       frequency += change;
       if (seenFrequencies.has(frequency)) return frequency;
       seenFrequencies.add(frequency);
