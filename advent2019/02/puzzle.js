ce17026faf79308430e550a4aad82a482864cfc4
Gary Hildebrand <garyah@gmail.com>
2019-12-01 23:14:02 -0800

    Cleanups.
    Also fix significant bug to potentially run an instruction twice, specifically if an add modified its own opcode into a multiply.  Surprisingly didn't cause a problem with real input, only with test case that had inadvertently been skipped to afterwards!

--- advent2019/02/puzzle.js
+++ advent2019/02/puzzle.js
@@ -1,21 +1,24 @@
 function Puzzle() {
 }
-Puzzle.prototype.solve = (program = []) => {
+Puzzle.prototype.transform = (program = []) => {
   for (let ip = 0; ip < program.length; ) {
     if (program[ip] === 99) break;
     if (program[ip] === 1) program[program[ip+3]] = program[program[ip+1]] + program[program[ip+2]];
-    if (program[ip] === 2) program[program[ip+3]] = program[program[ip+1]] * program[program[ip+2]];
+    else if (program[ip] === 2) program[program[ip+3]] = program[program[ip+1]] * program[program[ip+2]];
     // console.log(program[ip]);
     ip += 4;
   }
-  return program[0];
-  // return data.reduce((sum, num) => {
-  //   return sum + num;
-  // }, 0);
+  // console.log('transformed program: ' + program);
+  return program;
 }
 Puzzle.prototype.parse = (lines = ['']) => {
   return lines[0].split(',').map((value) => parseInt(value)).filter((num) => num === num);
 };
+Puzzle.prototype.solve = (program = []) => {
+  program[1] = 12;
+  program[2] = 2;
+  return Puzzle.prototype.transform(program)[0];
+}
 Puzzle.prototype.solve_p2 = (program = []) => {
   // save
   let savedData = [];
@@ -25,7 +28,7 @@ Puzzle.prototype.solve_p2 = (program = []) => {
     for (let verb = 0; verb < 100; verb++) {
       program[1] = noun;
       program[2] = verb;
-      const possibleGood = Puzzle.prototype.solve(program);
+      const possibleGood = Puzzle.prototype.transform(program)[0];
       //if (noun % 100 === 0 && verb % 10 === 0) console.log(possibleGood);
       if (possibleGood === 19690720) {
         console.log("found it!  noun = " + noun + " and verb = " + verb);
--- advent2019/02/puzzle.spec.js
+++ advent2019/02/puzzle.spec.js
@@ -17,33 +17,35 @@ describe("2019 day 2", function() {
     }
     done();
   });
-  // it("should be able to solve puzzle", () => {
-  //   const data = [[1,9,10,3,2,3,11,0,99,30,40,50], [2,3,0,3,99], [2,4,4,5,99,0]];
-  //   const actual = data.map((data) => puzzle.solve(data));
-  //   const expected = [[3500,9,10,70,
-  //     2,3,11,0,
-  //     99,
-  //     30,40,50], [2,3,0,6,99], [2,4,4,5,99,9801]];
-  //   expect(actual).toEqual(expected);
-  // });
-  // it("should be able to parse input", () => {
-  //   const data = puzzle.parse(['1,2,3']);
-  //   expect(data).toEqual([1, 2, 3]);
-  // });
+  it("should be able to transform parsed input", () => {
+    const data = [
+      [1,9,10,3,2,3,11,0,99,30,40,50],
+      [1,0,0,0,99], [2,3,0,3,99],
+      [2,4,4,5,99,0], [1,1,1,4,99,5,6,0,99]
+    ];
+    const actual = data.map((data) => puzzle.transform(data));
+    const expected = [
+      [3500,9,10,70,
+      2,3,11,0,
+      99,
+      30,40,50],
+      [2,0,0,0,99], [2,3,0,6,99],
+      [2,4,4,5,99,9801], [30,1,1,4,2,5,6,0,99]
+    ];
+    // console.log('actual: ', actual);
+    // console.log('expected: ', expected);
+    expect(actual).toEqual(expected);
+  });
+  it("should be able to parse input", () => {
+    const data = puzzle.parse(['1,2,3']);
+    expect(data).toEqual([1, 2, 3]);
+  });
   it("should be able solve puzzle with my input", () => {
     const data = puzzle.parse(lines);
-    data[1] = 12;
-    data[2] = 2;
     const answer = puzzle.solve(data);
     console.log("part 1 answer is " + answer);
     expect(answer).toEqual(6327510);
   });
-  // it("should be able to solve puzzle part 2", () => {
-  //   const data = [[], [], []];
-  //   const actual = data.map((data) => puzzle.solve_p2(data));
-  //   const expected = [1, 1, 1];
-  //   expect(actual).toEqual(expected);
-  // });
   it("should be able solve puzzle part 2 with my input", () => {
     const data = puzzle.parse(lines);
     const answer = puzzle.solve_p2(data);
