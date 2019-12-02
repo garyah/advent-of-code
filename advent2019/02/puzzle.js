3c410d7deaaf9a1d10ff426dc7aa7f432daf7df6
Gary Hildebrand <garyah@gmail.com>
2019-12-01 21:27:14 -0800

    Solve 2019 day 2 part 1.

--- advent2019/02/puzzle.js
+++ advent2019/02/puzzle.js
@@ -1,12 +1,21 @@
 function Puzzle() {
 }
-Puzzle.prototype.solve = (data) => {
-  return data.reduce((sum, num) => {
-    return sum + num;
-  }, 0);
+Puzzle.prototype.solve = (program = []) => {
+  for (let ip = 0; ip < program.length; ) {
+    if (program[ip] === 99) break;
+    if (program[ip] === 1) program[program[ip+3]] = program[program[ip+1]] + program[program[ip+2]];
+    if (program[ip] === 2) program[program[ip+3]] = program[program[ip+1]] * program[program[ip+2]];
+    console.log(program[ip]);
+    ip += 4;
+  }
+  return program;
+  // return data.reduce((sum, num) => {
+  //   return sum + num;
+  // }, 0);
 }
-Puzzle.prototype.parse = (lines) => {
-  return lines.map((line) => parseInt(line)).filter((num) => num === num);
+Puzzle.prototype.parse = (lines = ['']) => {
+  //return lines[0].split(',').parseInt();
+  return lines[0].split(',').map((value) => parseInt(value)).filter((num) => num === num);
 };
 Puzzle.prototype.solve_p2 = (data) => {
   return 0;
--- advent2019/02/puzzle.spec.js
+++ advent2019/02/puzzle.spec.js
@@ -3,7 +3,7 @@ describe("2019 day 2", function() {
   const Puzzle = require('./puzzle');
   const parser = new Parser();
   const puzzle = new Puzzle();
-  const readInputFile = false; // change to true to read input file for all tests that need it
+  const readInputFile = true; // change to true to read input file for all tests that need it
   let lines = [];
   beforeAll((done) => {
     console.log("2019 day 2:");
@@ -18,27 +18,30 @@ describe("2019 day 2", function() {
     done();
   });
   it("should be able to solve puzzle", () => {
-    const data = [[], [], []];
+    const data = [[1,9,10,3,2,3,11,0,99,30,40,50], [2,3,0,3,99], [2,4,4,5,99,0]];
     const actual = data.map((data) => puzzle.solve(data));
-    const expected = [1, 1, 1];
+    const expected = [[3500,9,10,70,
+      2,3,11,0,
+      99,
+      30,40,50], [2,3,0,6,99], [2,4,4,5,99,9801]];
     expect(actual).toEqual(expected);
   });
   it("should be able to parse input", () => {
-    const data = puzzle.parse('+1 +3 +2'.split(' '));
-    expect(data).toEqual([1, 3, 2]);
+    const data = puzzle.parse(['1,2,3']);
+    expect(data).toEqual([1, 2, 3]);
   });
-  // it("should be able solve puzzle with my input", () => {
-  //   const data = puzzle.parse(lines);
-  //   const answer = puzzle.solve(data);
-  //   console.log("part 1 answer is " + answer);
-  //   expect(answer).toEqual(0);
+  it("should be able solve puzzle with my input", () => {
+    const data = puzzle.parse(lines);
+    const answer = puzzle.solve(data);
+    console.log("part 1 answer is " + answer[0]);
+    expect(answer[0]).toEqual(6327510);
+  });
+  // it("should be able to solve puzzle part 2", () => {
+  //   const data = [[], [], []];
+  //   const actual = data.map((data) => puzzle.solve_p2(data));
+  //   const expected = [1, 1, 1];
+  //   expect(actual).toEqual(expected);
   // });
-  it("should be able to solve puzzle part 2", () => {
-    const data = [[], [], []];
-    const actual = data.map((data) => puzzle.solve_p2(data));
-    const expected = [1, 1, 1];
-    expect(actual).toEqual(expected);
-  });
   // it("should be able solve puzzle part 2 with my input", () => {
   //   const data = puzzle.parse(lines);
   //   const answer = puzzle.solve_p2(data);
