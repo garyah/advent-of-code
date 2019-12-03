d88f93b2fa6c548f168ca9f4cc3b302fadf445a3
Gary Hildebrand <garyah@gmail.com>
2019-12-03 09:56:35 -0800

    Improve template: only part 1 to start, part 2 will be separate files; only comment out expects in tests, not whole test.

--- advent2019/04/puzzle.js
+++ advent2019/04/puzzle.js
@@ -8,16 +8,4 @@ Puzzle.prototype.solve = (data) => {
 Puzzle.prototype.parse = (lines) => {
   return lines.map((line) => parseInt(line)).filter((num) => num === num);
 };
-Puzzle.prototype.solve_p2 = (data) => {
-  return 0;
-  let seenFrequencies = new Set([0]);
-  let frequency = 0;
-  while (true) {
-    for (const change of data) {
-      frequency += change;
-      if (seenFrequencies.has(frequency)) return frequency;
-      seenFrequencies.add(frequency);
-    }
-  }
-}
 module.exports = Puzzle;
