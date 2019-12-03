08050b6ea3ed52571d03ad46d1c36b7d967308fe
Gary Hildebrand <garyah@gmail.com>
2019-12-03 00:13:39 -0800

    2019 day 3 add missing test cases from samples.

--- advent2019/03/puzzle.spec.js
+++ advent2019/03/puzzle.spec.js
@@ -18,9 +18,13 @@ describe("2019 day 3", function() {
     done();
   });
   it("should be able to solve puzzle", () => {
-    const data = [["R8,U5,L5,D3", "U7,R6,D4,L4"]];
+    const data = [
+      ["R8,U5,L5,D3", "U7,R6,D4,L4"],
+      ["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"],
+      ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]
+    ];
     const actual = data.map((data) => puzzle.solve(data));
-    const expected = [6];
+    const expected = [6, 159, 135];
     expect(actual).toEqual(expected);
   });
   it("should be able to parse input", () => {
@@ -34,9 +38,13 @@ describe("2019 day 3", function() {
     expect(answer).toEqual(217);
   });
   it("should be able to solve puzzle part 2", () => {
-    const data = [["R8,U5,L5,D3", "U7,R6,D4,L4"]];
+    const data = [
+      ["R8,U5,L5,D3", "U7,R6,D4,L4"],
+      ["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"],
+      ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]
+    ];
     const actual = data.map((data) => puzzle.solve_p2(data));
-    const expected = [30];
+    const expected = [30, 610, 410];
     expect(actual).toEqual(expected);
   });
   it("should be able solve puzzle part 2 with my input", () => {
