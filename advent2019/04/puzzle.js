9fd1470c52ddd4bc00f1b61e05e96e070920085b
Gary Hildebrand <garyah@gmail.com>
2019-12-03 09:57:34 -0800

    Improve template: always have default argument.

--- advent2019/04/puzzle.js
+++ advent2019/04/puzzle.js
@@ -1,11 +1,11 @@
 function Puzzle() {
 }
-Puzzle.prototype.solve = (data) => {
+Puzzle.prototype.solve = (data = [0]) => {
   return data.reduce((sum, num) => {
     return sum + num;
   }, 0);
 }
-Puzzle.prototype.parse = (lines) => {
+Puzzle.prototype.parse = (lines = ['']) => {
   return lines.map((line) => parseInt(line)).filter((num) => num === num);
 };
 module.exports = Puzzle;
