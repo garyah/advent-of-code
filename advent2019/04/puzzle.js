2c837a72ec008558abee347cf3c1a9efcc795d80
Gary Hildebrand <garyah@gmail.com>
2019-12-03 20:12:08 -0800

    Even better syntax for exporting, dummy helper functions and variables.

--- advent2019/04/puzzle.js
+++ advent2019/04/puzzle.js
@@ -1,11 +1,20 @@
-var junk = () => 1;
-solve = (data = [0]) => {
-  junk();
+let var1 = 0;
+let var2 = '';
+const fn1 = (arg1 = 0, arg2 = '', arg3 = []) => {
+  return 0;
+};
+const fn2 = () => {
+  return '';
+};
+const solve = (data = [0]) => {
+  fn1();
   return data.reduce((sum, num) => {
     return sum + num;
   }, 0);
 }
-parse = (lines = ['']) => {
+const parse = (lines = ['']) => {
+  // return lines[0]; // use for one line string input
+  // return lines;    // use for multi-line string input
   return lines.map((line) => parseInt(line)).filter((num) => num === num);
 };
-module.exports = {solve, parse};
+module.exports = {var1, var2, fn1, fn2, solve, parse};
