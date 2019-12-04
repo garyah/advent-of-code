229b8e477997091b810de16637fa87e071ebac97
Gary Hildebrand <garyah@gmail.com>
2019-12-03 16:02:19 -0800

    Update remaining days with latest template.

--- advent2019/06/puzzle.js
+++ advent2019/06/puzzle.js
@@ -1,23 +1,11 @@
 function Puzzle() {
 }
-Puzzle.prototype.solve = (data) => {
+Puzzle.prototype.solve = (data = [0]) => {
   return data.reduce((sum, num) => {
     return sum + num;
   }, 0);
 }
-Puzzle.prototype.parse = (lines) => {
+Puzzle.prototype.parse = (lines = ['']) => {
   return lines.map((line) => parseInt(line)).filter((num) => num === num);
 };
-Puzzle.prototype.solve_p2 = (data) => {
-  return 0;
-  let seenFrequencies = new Set([0]);
-  let frequency = 0;
-  while (true) {
-    for (const change of data) {
-      frequency += change;
-      if (seenFrequencies.has(frequency)) return frequency;
-      seenFrequencies.add(frequency);
-    }
-  }
-}
 module.exports = Puzzle;
--- advent2019/12/puzzle.spec.js
+++ advent2019/12/puzzle.spec.js
@@ -1,4 +1,4 @@
-describe("2019 day ?", function() {
+describe("2019 day ? part 1", function() {
   const Parser = require('../../common/parser');
   const Puzzle = require('./puzzle');
   const parser = new Parser();
@@ -6,7 +6,7 @@ describe("2019 day ?", function() {
   const readInputFile = false; // change to true to read input file for all tests that need it
   let lines = [];
   beforeAll((done) => {
-    console.log("2019 day ?:");
+    console.log("2019 day ? part 1:");
     if (readInputFile) {
       // "adventYYYY/DD/input.txt" for specific file, undefined for stdin
       parser.readLines("adventYYYY/DD/input.txt", (linesRead) => {
@@ -21,28 +21,16 @@ describe("2019 day ?", function() {
     const data = [[], [], []];
     const actual = data.map((data) => puzzle.solve(data));
     const expected = [1, 1, 1];
-    expect(actual).toEqual(expected);
+    // expect(actual).toEqual(expected);
   });
   it("should be able to parse input", () => {
     const data = puzzle.parse('+1 +3 +2'.split(' '));
     expect(data).toEqual([1, 3, 2]);
   });
-  // it("should be able solve puzzle with my input", () => {
-  //   const data = puzzle.parse(lines);
-  //   const answer = puzzle.solve(data);
-  //   console.log("part 1 answer is " + answer);
-  //   expect(answer).toEqual(0);
-  // });
-  it("should be able to solve puzzle part 2", () => {
-    const data = [[], [], []];
-    const actual = data.map((data) => puzzle.solve_p2(data));
-    const expected = [1, 1, 1];
-    expect(actual).toEqual(expected);
+  it("should be able solve puzzle with my input", () => {
+    const data = puzzle.parse(lines);
+    const answer = puzzle.solve(data);
+    console.log("part 1 answer is " + answer);
+    // expect(answer).toEqual(0);
   });
-  // it("should be able solve puzzle part 2 with my input", () => {
-  //   const data = puzzle.parse(lines);
-  //   const answer = puzzle.solve_p2(data);
-  //   console.log("part 2 answer is " + answer);
-  //   expect(answer).toEqual(0);
-  // });
 });
