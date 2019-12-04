a783cc1cbb635e915ce55cfb9a7ebfdb04e66236
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2019-12-03 22:28:34 -0800

    Cleanups.

--- advent2019/04/puzzle.js
+++ advent2019/04/puzzle.js
@@ -1,13 +1,4 @@
-let var1 = 0;
-let var2 = '';
-const fn1 = (arg1 = 0, arg2 = '', arg3 = []) => {
-  return 0;
-};
-const fn2 = () => {
-  return '';
-};
 const solve = (data = [1, 100]) => {
-  //fn1();
   const low = data[0];
   const high = data[1];
   let count = 0;
@@ -38,8 +29,6 @@ const solve = (data = [1, 100]) => {
   return count;
 }
 const parse = (lines = ['']) => {
-  // return lines[0]; // use for one line string input
-  // return lines;    // use for multi-line string input
   return lines.map((line) => parseInt(line)).filter((num) => num === num);
 };
-module.exports = {var1, var2, fn1, fn2, solve, parse};
+module.exports = {solve, parse};
