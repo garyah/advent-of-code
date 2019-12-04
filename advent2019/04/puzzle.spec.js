bb0a18d9e459a18446ef9b13c6e19822e6c13c1d
Gary Hildebrand <garyah@gmail.com>
2019-12-03 20:18:13 -0800

    One more thing to prep 2019 day 4.

--- advent2019/04/puzzle.spec.js
+++ advent2019/04/puzzle.spec.js
@@ -1,11 +1,11 @@
-describe("2019 day ? part 1", function() {
+describe("2019 day 4 part 1", function() {
   const Parser = require('../../common/parser');
   const puzzle = require('./puzzle');
   const parser = new Parser();
   const readInputFile = true; // change to true to read input file for all tests that need it
   let lines = [];
   beforeAll((done) => {
-    console.log("2019 day ? part 1:");
+    console.log("2019 day 4 part 1:");
     if (readInputFile) {
       // "adventYYYY/DD/input.txt" for specific file, undefined for stdin
       parser.readLines("advent2019/04/input.txt", (linesRead) => {
