027234fef6893446373a59ecfb887dac050bfad4
Gary Hildebrand <garyah@gmail.com>
2019-12-05 10:13:31 -0800

    Copy day 5 to day 6, in case next puzzle based on previous one.

--- advent2019/06/puzzle.spec.js
+++ advent2019/06/puzzle.spec.js
@@ -1,23 +1,74 @@
-describe("2019 day ? part 1", function() {
+describe("2019 day 6", function() {
   // code
-  let var1 = 0;
-  let var2 = '';
-  const fn1 = (arg1 = 0, arg2 = '', arg3 = []) => {
-    return 0;
+  let input = 0;
+  let output = 0;
+  const transform = (program = []) => {
+    for (let ip = 0; ip < program.length; ) {
+      if (program[ip] === 99) break;
+      let step = 4;
+      const mode_p1 = Math.floor(program[ip] / 100) % 10;
+      const mode_p2 = Math.floor(program[ip] / 1000) % 10;
+      const opcode = program[ip] % 100;
+      if (opcode === 1) {
+        program[program[ip+3]]
+          = (mode_p1 ? program[ip+1] : program[program[ip+1]])
+            + (mode_p2 ? program[ip+2] : program[program[ip+2]]);
+      } else if (opcode === 2) {
+        program[program[ip+3]]
+          = (mode_p1 ? program[ip+1] : program[program[ip+1]])
+            * (mode_p2 ? program[ip+2] : program[program[ip+2]]);
+      } else if (opcode === 3) {
+        program[program[ip+1]] = input;
+        step = 2;
+      } else if (opcode === 4) {
+        output = mode_p1 ? program[ip+1] : program[program[ip+1]];
+        // console.log(output);
+        step = 2;
+      } else if (opcode === 5) { // jump-if-true
+        step = 3;
+        if (mode_p1 ? program[ip+1] : program[program[ip+1]] !== 0) {
+          ip = (mode_p2 ? program[ip+2] : program[program[ip+2]]);
+          step = 0;
+        }
+      } else if (opcode === 6) { // jump-if-false
+        step = 3;
+        if ((mode_p1 ? program[ip+1] : program[program[ip+1]]) === 0) {
+          ip = (mode_p2 ? program[ip+2] : program[program[ip+2]]);
+          step = 0;
+        }
+      } else if (opcode === 7) { // less than
+        if ((mode_p1 ? program[ip+1] : program[program[ip+1]])
+            < (mode_p2 ? program[ip+2] : program[program[ip+2]])) {
+          program[program[ip+3]] = 1;
+        } else {
+          program[program[ip+3]] = 0;
+        }
+      } else if (opcode === 8) { // equals
+        if ((mode_p1 ? program[ip+1] : program[program[ip+1]])
+            === (mode_p2 ? program[ip+2] : program[program[ip+2]])) {
+          program[program[ip+3]] = 1;
+        } else {
+          program[program[ip+3]] = 0;
+        }
+      }
+      // console.log(program[ip]);
+      ip += step;
+    }
+    // console.log('transformed program: ' + program);
+    return program;
   };
-  const fn2 = () => {
-    return '';
-  };
-  const solve = (data = [0]) => {
-    fn1();
-    return data.reduce((sum, num) => {
-      return sum + num;
-    }, 0);
+  const solve = (program = []) => {
+    input = 1;
+    transform(program);
+    return output;
+  }
+  const solve_p2 = (program = []) => {
+    input = 5;
+    transform(program);
+    return output;
   }
   const parse = (lines = ['']) => {
-    // return lines[0]; // use for one line string input
-    // return lines;    // use for multi-line string input
-    return lines.map((line) => parseInt(line)).filter((num) => num === num);
+    return lines[0].split(',').map((value) => parseInt(value)).filter((num) => num === num);
   };
 
 
@@ -25,7 +76,9 @@ describe("2019 day ? part 1", function() {
 
 
 
-  
+
+
+
 
 
 
@@ -33,28 +86,105 @@ describe("2019 day ? part 1", function() {
 
 
   // tests
-  it('fn1() returns number 0', () => {
-    expect(fn1()).toEqual(0);
-  });
-  it('fn2() returns empty string', () => {
-    expect(fn2()).toEqual('');
-  });
-  it("should be able to solve puzzle", () => {
-    const data = [[], [], []];
-    const actual = data.map((data) => puzzle.solve(data));
-    const expected = [1, 1, 1];
-    // expect(actual).toEqual(expected);
-  });
   it("should be able to parse input", () => {
-    const data = puzzle.parse('+1 +3 +2'.split(' '));
-    expect(data).toEqual([1, 3, 2]);
+    const data = parse(['1,2,3']);
+    expect(data).toEqual([1, 2, 3]);
+  });
+  it("should be able to transform parsed input", () => {
+    input = 0;
+    const data = [
+      [3,0,4,0,99],
+      [1002,4,3,4,33]
+    ];
+    const actual = data.map((data) => transform(data));
+    const expected = [
+      [0,0,4,0,99],
+      [1002,4,3,4,99]
+    ];
+    // console.log('actual: ', actual);
+    // console.log('expected: ', expected);
+    expect(actual).toEqual(expected);
   });
   it("should be able solve puzzle with my input", () => {
-    // const data = [0];
-    const data = puzzle.parse(lines);
-    const answer = puzzle.solve(data);
+    const data = parse(lines);
+    const answer = solve(data);
     console.log("part 1 answer is " + answer);
-    // expect(answer).toEqual(0);
+    expect(answer).toEqual(2845163);
+  });
+  it("should be able to compare correctly", () => {
+    input = 0;
+    const data = [
+      [8, [3,9,8,9,10,9,4,9,99,-1,8]], [7, [3,9,8,9,10,9,4,9,99,-1,8]],
+      [7, [3,9,7,9,10,9,4,9,99,-1,8]], [8, [3,9,7,9,10,9,4,9,99,-1,8]],
+      [8, [3,3,1108,-1,8,3,4,3,99]], [7, [3,3,1108,-1,8,3,4,3,99]],
+      [7, [3,3,1107,-1,8,3,4,3,99]], [8, [3,3,1107,-1,8,3,4,3,99]],
+    ];
+    const actual = data.map(([inputValue, data]) => {
+      input = inputValue;
+      transform(data);
+      return output;
+    });
+    const expected = [
+      1, 0,
+      1, 0,
+      1, 0,
+      1, 0,
+    ];
+    // console.log('actual: ', actual);
+    // console.log('expected: ', expected);
+    expect(actual).toEqual(expected);
+  });
+  it("should be able to handle jumps correctly", () => {
+    input = 0;
+    const data = [
+      [1, [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]],
+      [0, [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]],
+      [1, [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]],
+      [0, [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]],
+    ];
+    const actual = data.map(([inputValue, data]) => {
+      input = inputValue;
+      transform(data);
+      return output;
+    });
+    const expected = [
+      1, 0,
+      1, 0,
+    ];
+    // console.log('actual: ', actual);
+    // console.log('expected: ', expected);
+    expect(actual).toEqual(expected);
+  });
+  it("should be able to handle complex programs", () => {
+    input = 0;
+    const data = [
+      [7, [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+           1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+           999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]],
+      [8, [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+           1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+           999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]],
+      [9, [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+           1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+           999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]],
+    ];
+    const actual = data.map(([inputValue, data]) => {
+      input = inputValue;
+      transform(data);
+      return output;
+    });
+    const expected = [
+      999, 1000, 1001
+    ];
+    // console.log('actual: ', actual);
+    // console.log('expected: ', expected);
+    expect(actual).toEqual(expected);
+  });
+  it("should be able solve puzzle part 2 with my input", () => {
+    const data = parse(lines);
+    const answer = solve_p2(data);
+    console.log("part 2 answer is " + answer);
+    expect(answer).toEqual(9436229);
   });
 
 
@@ -72,13 +202,13 @@ describe("2019 day ? part 1", function() {
   const Parser = require('../../common/parser');
   const puzzle = require('./puzzle');
   const parser = new Parser();
-  const readInputFile = false; // change to true to read input file for all tests that need it
+  const readInputFile = true; // change to true to read input file for all tests that need it
   let lines = [];
   beforeAll((done) => {
-    console.log("2019 day ? part 1:");
+    console.log("2019 day 6:");
     if (readInputFile) {
       // "adventYYYY/DD/input.txt" for specific file, undefined for stdin
-      parser.readLines("adventYYYY/DD/input.txt", (linesRead) => {
+      parser.readLines("advent2019/05/input.txt", (linesRead) => {
         lines = linesRead;
         done();
       });
