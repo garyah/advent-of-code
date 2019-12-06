7ab6d2f8150e3ff90848c5c27cfe07c8e7328381
Gary Hildebrand <garyah@gmail.com>
2019-12-05 20:55:15 -0800

    More changes to template, fully prepping day 6.

--- advent2019/06/puzzle.spec.js
+++ advent2019/06/puzzle.spec.js
@@ -1,5 +1,30 @@
 describe("2019 day 6", function() {
-  // code
+  // new code
+  let var1 = 0;
+  let var2 = '';
+  const fn1 = (arg1 = 0, arg2 = '', arg3 = []) => {
+    if (1) {}
+    else if (1) {}
+    else {}
+    for (let i = 0;; i++) { break; continue; }
+    for (const item of arg3) {}
+    return 0;
+  };
+  const fn2 = () => {
+    return '';
+  };
+  const solve = (data = [0]) => {
+    fn1();
+    return data.reduce((sum, num) => {
+      return sum + num;
+    }, 0);
+  }
+  const parse = (lines = ['']) => {
+    // return lines[0]; // use for one line string input
+    // return lines;    // use for multi-line string input
+    return lines.map((line) => parseInt(line)).filter((num) => num === num);
+  };
+  // 2019 day 5 code (extended intcode computer, based on day 2)
   let input = 0;
   let output = 0;
   const transform = (program = []) => {
@@ -57,19 +82,100 @@ describe("2019 day 6", function() {
     // console.log('transformed program: ' + program);
     return program;
   };
-  const solve = (program = []) => {
+  const solve_19_05 = (program = []) => {
     input = 1;
     transform(program);
     return output;
   }
-  const solve_p2 = (program = []) => {
+  const solve_19_05_p2 = (program = []) => {
     input = 5;
     transform(program);
     return output;
   }
-  const parse = (lines = ['']) => {
+  const parse_19_05 = (lines = ['']) => {
     return lines[0].split(',').map((value) => parseInt(value)).filter((num) => num === num);
   };
+  // 2019 day 3 code (intersection of wires in a 2D grid)
+  function isPositionSeen_p2(
+    seenPositions = new Map([[0, new Map([[0, 0]])]]),
+    position = {x: 0, y: 0, steps: 0},
+    canUpdate = true) {
+  if (seenPositions.has(position.x) && seenPositions.get(position.x).has(position.y)) return true;
+  if (canUpdate) {
+    if (!seenPositions.has(position.x)) seenPositions.set(position.x, new Map());
+    seenPositions.get(position.x).set(position.y, position.steps);
+  }
+  return false;
+  }
+  function updatePositions_p2(
+      wire = 0,
+      seenPositions = new Map([[0, new Map([[0, 0]])]]),
+      bestIntersected = {x: 0, y: 0, steps: 0},
+      position = {x: 0, y: 0, steps: 0}) {
+    if (wire === 0) {
+      isPositionSeen_p2(seenPositions, position, true); // just to update seen positions
+      return;
+    }
+    if (isPositionSeen_p2(seenPositions, position, false)) { // not updating seen positions here
+      const currentSteps = position.steps + seenPositions.get(position.x).get(position.y);
+      //console.log('got intersection, position = ', position, ' steps from other wire = ', currentSteps - position.steps);
+      const previousSteps = bestIntersected.steps;
+      if (previousSteps === 0 || currentSteps < previousSteps) {
+        bestIntersected.x = position.x;
+        bestIntersected.y = position.y;
+        bestIntersected.steps = currentSteps;
+        //console.log('updated bestIntersected = ', bestIntersected);
+      }
+    }
+  }
+  const solve_19_03_p2 = (data = ['', '']) => {
+    let seenPositions = new Map([[0, new Map([[0, 0]])]]);
+    let bestIntersected = {x: 0, y: 0, steps: 0};
+    let position = {x: 0, y: 0, steps: 0};
+    //console.log('bestIntersected = ', bestIntersected);
+    for (let wire = 0; wire < 2; wire++) {
+      position.x = 0;
+      position.y = 0;
+      position.steps = 1;
+      data[wire].split(',').reduce((heading, move) => {
+        if (move[0] === 'U') heading = 0;
+        else if (move[0] === 'R') heading = 90;
+        else if (move[0] === 'D') heading = 180;
+        else if (move[0] === 'L') heading = 270;
+        let x = 0, y = 0;
+        switch (heading) {
+          case 0:
+          default:
+            for (y = position.y + 1; y <= position.y + parseInt(move.substr(1)); ++y, ++(position.steps))
+              updatePositions_p2(wire, seenPositions, bestIntersected, {x: position.x, y: y, steps: position.steps});
+            position.y = y - 1;
+            //console.log('position = ', position);
+            break;
+          case 90:
+            for (x = position.x + 1; x <= position.x + parseInt(move.substr(1)); ++x, ++(position.steps))
+              updatePositions_p2(wire, seenPositions, bestIntersected, {x: x, y: position.y, steps: position.steps});
+            position.x = x - 1;
+            //console.log('position = ', position);
+            break;
+          case 180:
+            for (y = position.y - 1; y >= position.y - parseInt(move.substr(1)); --y, ++(position.steps))
+              updatePositions_p2(wire, seenPositions, bestIntersected, {x: position.x, y: y, steps: position.steps});
+            position.y = y + 1;
+            //console.log('position = ', position);
+            break;
+          case 270:
+            for (x = position.x - 1; x >= position.x - parseInt(move.substr(1)); --x, ++(position.steps))
+              updatePositions_p2(wire, seenPositions, bestIntersected, {x: x, y: position.y, steps: position.steps});
+            position.x = x + 1;
+            //console.log('position = ', position);
+            break;
+        }
+        return heading;
+      }, 0);
+    }
+    //console.log('bestIntersected = ', bestIntersected);
+    return bestIntersected.steps;
+  }
 
 
 
@@ -85,12 +191,48 @@ describe("2019 day 6", function() {
 
 
 
-  // tests
-  it("should be able to parse input", () => {
-    const data = parse(['1,2,3']);
+  // new tests
+  it('fn1() returns number 0', () => {
+    expect(fn1()).toEqual(
+      0
+      );
+  });
+  it('fn2() returns empty string', () => {
+    expect(fn2()).toEqual(
+      ''
+      );
+  });
+  it("can solve puzzle", () => {
+    const data = [
+      [],
+    ];
+    const actual = data.map((data) => solve(data));
+    const expected = [
+      1,
+    ];
+    // expect(actual).toEqual(expected);
+  });
+  it("can parse input", () => {
+    const data = parse(
+      '+1 +3 +2'
+      .split(
+        ' '
+        ));
+    expect(data).toEqual([1, 3, 2]);
+  });
+  it("can solve puzzle with my input", () => {
+    // const data = [0];
+    const data = parse(lines);
+    const answer = solve(data);
+    console.log("part 1 answer is " + answer);
+    // expect(answer).toEqual(0);
+  });
+  // 2019 day 5 tests (extended intcode computer, based on day 2)
+  it("can parse input", () => {
+    const data = parse_19_05(['1,2,3']);
     expect(data).toEqual([1, 2, 3]);
   });
-  it("should be able to transform parsed input", () => {
+  it("can transform parsed input", () => {
     input = 0;
     const data = [
       [3,0,4,0,99],
@@ -105,13 +247,13 @@ describe("2019 day 6", function() {
     // console.log('expected: ', expected);
     expect(actual).toEqual(expected);
   });
-  it("should be able solve puzzle with my input", () => {
-    const data = parse(lines);
-    const answer = solve(data);
-    console.log("part 1 answer is " + answer);
+  it("can solve day 2019 day 5 puzzle with my input", () => {
+    const data = parse_19_05(lines);
+    const answer = solve_19_05(data);
+    // console.log("part 1 answer is " + answer);
     expect(answer).toEqual(2845163);
   });
-  it("should be able to compare correctly", () => {
+  it("can compare correctly", () => {
     input = 0;
     const data = [
       [8, [3,9,8,9,10,9,4,9,99,-1,8]], [7, [3,9,8,9,10,9,4,9,99,-1,8]],
@@ -134,7 +276,7 @@ describe("2019 day 6", function() {
     // console.log('expected: ', expected);
     expect(actual).toEqual(expected);
   });
-  it("should be able to handle jumps correctly", () => {
+  it("can handle jumps correctly", () => {
     input = 0;
     const data = [
       [1, [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]],
@@ -155,7 +297,7 @@ describe("2019 day 6", function() {
     // console.log('expected: ', expected);
     expect(actual).toEqual(expected);
   });
-  it("should be able to handle complex programs", () => {
+  it("can handle complex programs", () => {
     input = 0;
     const data = [
       [7, [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
@@ -180,10 +322,10 @@ describe("2019 day 6", function() {
     // console.log('expected: ', expected);
     expect(actual).toEqual(expected);
   });
-  it("should be able solve puzzle part 2 with my input", () => {
-    const data = parse(lines);
-    const answer = solve_p2(data);
-    console.log("part 2 answer is " + answer);
+  it("can solve day 2019 day 5 puzzle part 2 with my input", () => {
+    const data = parse_19_05(lines);
+    const answer = solve_19_05_p2(data);
+    // console.log("part 2 answer is " + answer);
     expect(answer).toEqual(9436229);
   });
 
@@ -208,7 +350,7 @@ describe("2019 day 6", function() {
     console.log("2019 day 6:");
     if (readInputFile) {
       // "adventYYYY/DD/input.txt" for specific file, undefined for stdin
-      parser.readLines("advent2019/05/input.txt", (linesRead) => {
+      parser.readLines("advent2019/06/input.txt", (linesRead) => {
         lines = linesRead;
         done();
       });
