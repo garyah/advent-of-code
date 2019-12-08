eacfd0e4f7473612ee5ee9522030225201b323c9
Gary Hildebrand <garyah@gmail.com>
2019-12-07 21:20:48 -0800

    Prep day 2017 day 7 and 8.

--- advent2019/07/puzzle.spec.js
+++ advent2019/07/puzzle.spec.js
@@ -1,4 +1,4 @@
-describe("2019 day 6", function() {
+describe("2019 day 7", function() {
   // new code
   let var1 = 0;
   let var2 = '';
@@ -227,107 +227,6 @@ describe("2019 day 6", function() {
     console.log("part 1 answer is " + answer);
     // expect(answer).toEqual(0);
   });
-  // 2019 day 5 tests (extended intcode computer, based on day 2)
-  it("can parse input", () => {
-    const data = parse_19_05(['1,2,3']);
-    expect(data).toEqual([1, 2, 3]);
-  });
-  it("can transform parsed input", () => {
-    input = 0;
-    const data = [
-      [3,0,4,0,99],
-      [1002,4,3,4,33]
-    ];
-    const actual = data.map((data) => transform(data));
-    const expected = [
-      [0,0,4,0,99],
-      [1002,4,3,4,99]
-    ];
-    // console.log('actual: ', actual);
-    // console.log('expected: ', expected);
-    expect(actual).toEqual(expected);
-  });
-  it("can solve day 2019 day 5 puzzle with my input", () => {
-    const data = parse_19_05(lines);
-    const answer = solve_19_05(data);
-    // console.log("part 1 answer is " + answer);
-    expect(answer).toEqual(2845163);
-  });
-  it("can compare correctly", () => {
-    input = 0;
-    const data = [
-      [8, [3,9,8,9,10,9,4,9,99,-1,8]], [7, [3,9,8,9,10,9,4,9,99,-1,8]],
-      [7, [3,9,7,9,10,9,4,9,99,-1,8]], [8, [3,9,7,9,10,9,4,9,99,-1,8]],
-      [8, [3,3,1108,-1,8,3,4,3,99]], [7, [3,3,1108,-1,8,3,4,3,99]],
-      [7, [3,3,1107,-1,8,3,4,3,99]], [8, [3,3,1107,-1,8,3,4,3,99]],
-    ];
-    const actual = data.map(([inputValue, data]) => {
-      input = inputValue;
-      transform(data);
-      return output;
-    });
-    const expected = [
-      1, 0,
-      1, 0,
-      1, 0,
-      1, 0,
-    ];
-    // console.log('actual: ', actual);
-    // console.log('expected: ', expected);
-    expect(actual).toEqual(expected);
-  });
-  it("can handle jumps correctly", () => {
-    input = 0;
-    const data = [
-      [1, [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]],
-      [0, [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]],
-      [1, [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]],
-      [0, [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]],
-    ];
-    const actual = data.map(([inputValue, data]) => {
-      input = inputValue;
-      transform(data);
-      return output;
-    });
-    const expected = [
-      1, 0,
-      1, 0,
-    ];
-    // console.log('actual: ', actual);
-    // console.log('expected: ', expected);
-    expect(actual).toEqual(expected);
-  });
-  it("can handle complex programs", () => {
-    input = 0;
-    const data = [
-      [7, [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
-           1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-           999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]],
-      [8, [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
-           1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-           999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]],
-      [9, [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
-           1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-           999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]],
-    ];
-    const actual = data.map(([inputValue, data]) => {
-      input = inputValue;
-      transform(data);
-      return output;
-    });
-    const expected = [
-      999, 1000, 1001
-    ];
-    // console.log('actual: ', actual);
-    // console.log('expected: ', expected);
-    expect(actual).toEqual(expected);
-  });
-  it("can solve day 2019 day 5 puzzle part 2 with my input", () => {
-    const data = parse_19_05(lines);
-    const answer = solve_19_05_p2(data);
-    // console.log("part 2 answer is " + answer);
-    expect(answer).toEqual(9436229);
-  });
 
 
 
@@ -344,13 +243,13 @@ describe("2019 day 6", function() {
   const Parser = require('../../common/parser');
   const puzzle = require('./puzzle');
   const parser = new Parser();
-  const readInputFile = true; // change to true to read input file for all tests that need it
+  const readInputFile = false; // change to true to read input file for all tests that need it
   let lines = [];
   beforeAll((done) => {
-    console.log("2019 day 6:");
+    console.log("2019 day 7:");
     if (readInputFile) {
       // "adventYYYY/DD/input.txt" for specific file, undefined for stdin
-      parser.readLines("advent2019/06/input.txt", (linesRead) => {
+      parser.readLines("advent2019/07/input.txt", (linesRead) => {
         lines = linesRead;
         done();
       });
