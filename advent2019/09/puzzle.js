7898dace4a6334ea499dd83b47bb17972c7a9ba1
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2019-12-08 23:31:37 -0800

    Cleanups.

--- advent2019/09/puzzle.js
+++ advent2019/09/puzzle.js
@@ -1,4 +1,5 @@
-// 2019 day 7 code (extended intcode computer, based on days 2 and 5)
+// 2019 day 9 code (extended intcode computer, based on days 2 and 5)
+// inadvertently (and regrettably) based on day 7 at start as well!
 // console.log('puzzle object reaches here, input, output: ', input, output);
 let input = -1;
 let output = -1;
@@ -24,7 +25,10 @@ const transform = (inputProgram = []) => {
   // console.log(program);
   // console.log('input, output before program execution: ', input, output);
   for (ip = programs[ampIndex].startIp; ip < program.length; ) {
-    if (program[ip] === 99) { programs[ampIndex].startIp = -1; return -1; }
+    if (program[ip] === 99) {
+      programs[ampIndex].startIp = -1;
+      return output;
+    }
     let step = 4;
     const mode_p1 = Math.floor(program[ip] / 100) % 10;
     const mode_p2 = Math.floor(program[ip] / 1000) % 10;
@@ -48,7 +52,7 @@ const transform = (inputProgram = []) => {
       // console.log(program);
       // console.log('output before mod: ', output);
       output = ((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]);
-      console.log('output after mod: ', output);
+      // console.log('output after mod: ', output);
       programs[ampIndex].startIp = ip + 2;
       step = 2;
       // return programs[ampIndex].startIp;
@@ -91,43 +95,8 @@ const transform = (inputProgram = []) => {
 };
 const solve = (program = []) => {
   // console.log('program = ', program);
-  let numCombinations = 0;
-  let maxOutput = -1;
-  phases = [0, 1, 2, 3, 4];
-  for (let p0 = 0; p0 < 5; p0++) {
-    for (let p1 = 0; p1 < 5; p1++) {
-      if (p1 === p0) continue;
-      for (let p2 = 0; p2 < 5; p2++) {
-        if (p2 === p1 || p2 === p0) continue;
-        for (let p3 = 0; p3 < 5; p3++) {
-          if (p3 === p2 || p3 === p1 || p3 === p0) continue;
-          for (let p4 = 0; p4 < 5; p4++) {
-            if (p4 === p3 || p4 === p2 || p4 === p1 || p4 === p0) continue;
-            phases = [p0, p1, p2, p3, p4];
-            input = 0;
-            for (ampIndex = 0; ampIndex < 5; ampIndex++) {
-              programs[ampIndex] = {memory: [], startIp: 0, isResumed: false, relativeBase: 0};
-              programs[ampIndex].memory = program.map((value) => {
-                // if (ampIndex === 0) console.log(value);
-                return value;
-              });
-              for (let n = 0; n < 1000; n++) programs[ampIndex].memory.push([0, 0, 0, 0, 0]);
-              // console.log('ampIndex = ', ampIndex, 'programs.length = ', programs.length);
-              // printProgram(programs[ampIndex].memory);
-              // console.log('ampIndex = ', ampIndex, 'input = ', input);
-              transform();
-              // console.log('ampIndex = ', ampIndex, 'output = ', output);
-              input = output;
-            }
-            if (output > maxOutput) {
-              maxOutput = output;
-              // console.log('found new max of ', maxOutput,
-              //             ' with phase setting sequence of ', p0, p1, p2, p3, p4);
-            }
-            numCombinations++;
-  }}}}}
-  // console.log('numCombinations = ', numCombinations);
-  return maxOutput;
+  input = 1;
+  return transform(program);
 }
 const printProgram = (programMemory = [], startIp = 0) => {
   let programString = '[' +  programMemory.join(',') + ']';
@@ -137,62 +106,8 @@ const printProgram = (programMemory = [], startIp = 0) => {
 }
 const solve_p2 = (program = []) => {
   // console.log('program = ', program);
-  let numCombinations = 0;
-  let maxOutput = -1;
-  let phasesOfMax = [];
-  phases = [5, 6, 7, 8, 9];
-  // phases = [9, 8, 7, 6, 5];  // TEST ONLY
-  // phases = [9, 7, 8, 5, 6]; // TEST ONLY
-  for (let p0 = 5; p0 < 10; p0++) {
-    for (let p1 = 5; p1 < 10; p1++) {
-      if (p1 === p0) continue;
-      for (let p2 = 5; p2 < 10; p2++) {
-        if (p2 === p1 || p2 === p0) continue;
-        for (let p3 = 5; p3 < 10; p3++) {
-          if (p3 === p2 || p3 === p1 || p3 === p0) continue;
-          for (let p4 = 5; p4 < 10; p4++) {
-            if (p4 === p3 || p4 === p2 || p4 === p1 || p4 === p0) continue;
-            phases = [p0, p1, p2, p3, p4];
-            // console.log('programs.length = ', programs.length, ' programs = ', programs);
-            for (ampIndex = 0; ampIndex < 5; ampIndex++) {
-              programs[ampIndex] = {memory: [], startIp: 0, isResumed: false, relativeBase: 0};
-              programs[ampIndex].memory = program.map((value) => {
-                // if (ampIndex === 0) console.log(value);
-                return value;
-              });
-              for (let n = 0; n < 1000; n++) programs[ampIndex].memory.push([0, 0, 0, 0, 0]);
-              // console.log('ampIndex = ', ampIndex, 'programs.length = ', programs.length);
-              // printProgram(programs[ampIndex].memory);
-            }
-            input = 0;
-            output = 0;
-            let lastOutput = 0;
-            for (let n = 0; n < 20; n++) { // set maximum limit on how many times to loop, just in case
-              // console.log();
-              for (ampIndex = 0; ampIndex < 5; ampIndex++) {
-                // console.log('program at index ', ampIndex, ' program = ', programs[ampIndex]);
-                // printProgram(programs[ampIndex].memory, programs[ampIndex].startIp);
-                // console.log('ampIndex= ', ampIndex, 'input= ', input);
-                transform();
-                // printProgram(programs[ampIndex].memory, programs[ampIndex].startIp);
-                // console.log('ampIndex=', ampIndex, '\tinput=', input, '\toutput=', output, '\tstartIp=', programs[ampIndex].startIp);
-                if (programs[ampIndex].startIp === -1) { /*console.log('inner got halt, spin #', n, 'ampIndex=', ampIndex);*/ break; }
-                input = output;
-              }
-              lastOutput = output;
-              if (ampIndex < 5 && programs[ampIndex].startIp === -1) { /*console.log("outer got halt");*/ break; }
-            }
-            if (lastOutput > maxOutput) {
-              maxOutput = lastOutput;
-              phasesOfMax = [p0, p1, p2, p3, p4];
-              // console.log('found new max of ', maxOutput,
-              //             ' with phase setting sequence of ', p0, p1, p2, p3, p4);
-            }
-            numCombinations++;
-  }}}}}
-  // console.log('maxOutput=', maxOutput, 'phase setting sequence=[' +  phasesOfMax.join(',') + ']');
-  // console.log('numCombinations = ', numCombinations);
-  return maxOutput;
+  input = 2;
+  return transform(program);
 }
 const parse = (lines = ['']) => {
   return lines[0].split(',').map((value) => parseInt(value)).filter((num) => num === num);
