58f9607c875f89f3410a5a99f16f98a5156a730a
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2019-12-10 23:07:29 -0800

    Solve 2019 day 11 part 1.

--- advent2019/11/puzzle.js
+++ advent2019/11/puzzle.js
@@ -1,20 +1,194 @@
-let var1 = 0;
-let var2 = '';
-const fn1 = (arg1 = 0, arg2 = '', arg3 = []) => {
-  return 0;
+// 2019 day 11 code (extended intcode computer, based on day 9)
+// console.log('puzzle object reaches here, input, output: ', input, output);
+let input = -1;
+let output = -1;
+let phases = [-1, -1, -1, -1, -1];
+let programs = [{memory: [0], startIp: 0, isResumed: false, relativeBase: 0}];
+let ampIndex = 0;
+// console.log('puzzle object reaches here, input, output: ', input, output);
+const setInput = (value = 0) => {
+  input = value;
 };
-const fn2 = () => {
-  return '';
+const getOutput = () => {
+  return output;
 };
-const solve = (data = [0]) => {
-  fn1();
-  return data.reduce((sum, num) => {
-    return sum + num;
-  }, 0);
+const transform = (inputProgram = []) => {
+  programs[ampIndex] = {memory: [], startIp: 0, isResumed: false, relativeBase: 0};
+  programs[ampIndex].memory = inputProgram.map((value) => {
+    // if (ampIndex === 0) console.log(value);
+    return value;
+  });
+  for (let n = 0; n < 1000; n++) programs[ampIndex].memory.push(0);
+  const program = programs[ampIndex].memory;
+  let ip = 0;
+  // console.log(program);
+  // console.log('input, output before program execution: ', input, output);
+  for (ip = programs[ampIndex].startIp; ip < program.length; ) {
+    if (program[ip] === 99) {
+      programs[ampIndex].startIp = -1;
+      return output;
+    }
+    let step = 4;
+    const mode_p1 = Math.floor(program[ip] / 100) % 10;
+    const mode_p2 = Math.floor(program[ip] / 1000) % 10;
+    const mode_p3 = Math.floor(program[ip] / 10000) % 10;
+    const opcode = program[ip] % 100;
+    if (opcode === 1) { // add
+      const result
+        = ((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]])
+          + ((mode_p2 == 2) ? program[programs[ampIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]]);
+      if (mode_p3 == 2) { program[programs[ampIndex].relativeBase + program[ip+3]] = result; } else { program[program[ip+3]] = result; }
+    } else if (opcode === 2) { // multiply
+      const result
+        = ((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]])
+          * ((mode_p2 == 2) ? program[programs[ampIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]]);
+      if (mode_p3 == 2) { program[programs[ampIndex].relativeBase + program[ip+3]] = result; } else { program[program[ip+3]] = result; }
+    } else if (opcode === 3) { // input
+      input = nextInput();
+      if (mode_p1 == 2) { program[programs[ampIndex].relativeBase + program[ip+1]] = input; } else { program[program[ip+1]] = input; }
+      // programs[ampIndex].isResumed = true;
+      step = 2;
+    } else if (opcode === 4) { // output
+      // console.log(program);
+      // console.log('output before mod: ', output);
+      output = ((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]);
+      // console.log('output after mod: ', output);
+      nextOutput(output);
+      programs[ampIndex].startIp = ip + 2;
+      step = 2;
+      // return programs[ampIndex].startIp;
+    } else if (opcode === 5) { // jump-if-true
+      step = 3;
+      if (((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]) !== 0) {
+        ip = (mode_p2 == 2) ? program[programs[ampIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]];
+        step = 0;
+      }
+    } else if (opcode === 6) { // jump-if-false
+      step = 3;
+      if (((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]) === 0) {
+        ip = (mode_p2 == 2) ? program[programs[ampIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]];
+        step = 0;
+      }
+    } else if (opcode === 7) { // less than
+      if (((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]])
+          < ((mode_p2 == 2) ? program[programs[ampIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]])) {
+        if (mode_p3 == 2) { program[programs[ampIndex].relativeBase + program[ip+3]] = 1; } else { program[program[ip+3]] = 1; }
+      } else {
+        if (mode_p3 == 2) { program[programs[ampIndex].relativeBase + program[ip+3]] = 0; } else { program[program[ip+3]] = 0; }
+      }
+    } else if (opcode === 8) { // equals
+      if (((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]])
+          === ((mode_p2 == 2) ? program[programs[ampIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]])) {
+        if (mode_p3 == 2) { program[programs[ampIndex].relativeBase + program[ip+3]] = 1; } else { program[program[ip+3]] = 1; }
+      } else {
+        if (mode_p3 == 2) { program[programs[ampIndex].relativeBase + program[ip+3]] = 0; } else { program[program[ip+3]] = 0; }
+      }
+    } else if (opcode === 9) { // relative base offset
+      programs[ampIndex].relativeBase += ((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]);
+      step = 2;
+    }
+    // console.log(program[ip]);
+    ip += step;
+  }
+  // console.log('transformed program: ' + program);
+  programs[ampIndex].startIp = ip;
+  return programs[ampIndex].startIp;
+};
+let panelMap = [[-1]];
+let currentX = 5000;
+let currentY = 5000;
+let outputState = 0;
+let heading = 0;
+let numOutputs = 0;
+let numPainted = 0;
+const getNumPainted = () => {
+  return numPainted;
+};
+const nextInput = () => {
+  // if (panelMap[currentX][currentY] !== 0 && panelMap[currentX][currentY] !== 1
+  //     && panelMap[currentX][currentY] !== -1)
+  //   console.log("read an invalid value of panel! currentX=", currentX, "currentY=", currentY, "heading=", heading);
+  // if (outputState !== 0)
+  //   console.log("in an invalid output state! state=", outputState, "currentX=", currentX, "currentY=", currentY, "heading=", heading);
+  console.log('panelMap[currentX][currentY]=', panelMap[currentX][currentY]);
+  return panelMap[currentX][currentY] === -1 ? 0 : panelMap[currentX][currentY];
+};
+const nextOutput = (output = 0) => {
+  if (outputState === 0) {
+    outputState = 1;
+    panelMap[currentX][currentY] = output;
+    numOutputs++;
+    if (numOutputs < 20) console.log('currentX=', currentX, 'currentY=', currentY, 'output=', output);
+    return;
+  }
+  if (outputState === 1) {
+    outputState = 0;
+    heading += 360;
+    if (output) heading += 90; else heading -= 90;
+    heading %= 360;
+    if (heading !== 0 && heading !== 90 && heading !== 180 && heading !== 270)
+      console.log("bad heading value!");
+    if (heading === 0) { currentY--; }
+    if (heading === 90) { currentX++; }
+    if (heading === 180) { currentY++; }
+    if (heading === 270) { currentX--; }
+    if (currentX < 0 || currentX > panelMap[0].length
+        || currentY < 0 || currentY > panelMap[0].length) {
+      console.log("ran off the edge! currentX=", currentX, "currentY=", currentY, "heading=", heading);
+    }
+  }
+};
+const initState = () => {
+  for (let x = 0; x < 10000; x++) {
+    panelMap[x] = [];
+    for (let y = 0; y < 10000; y++) {
+      panelMap[x][y] = -1; // initial black
+      // console.log(panelMap);
+    }
+  }
+  // let numPainted0 = 0;
+  // for (let x = 0; x < 10000; x++) {
+  //   for (let y = 0; y < 10000; y++) {
+  //     if (panelMap[x][y] !== -1) numPainted0++;
+  //   }
+  // }
+  // console.log('numPainted0=', numPainted0);
+  currentX = 5000;
+  currentY = 5000;
+  outputState = 0;
+  heading = 0;
+  numOutputs = 0;
+  numPainted = 0;
+};
+const countPainted = () => {
+  numPainted = 0;
+  for (let x = 0; x < 10000; x++) {
+    for (let y = 0; y < 10000; y++) {
+      if (panelMap[x][y] !== -1) numPainted++;
+    }
+  }
+};
+const solve = (program = []) => {
+  // console.log('program = ', program);
+  initState();
+  transform(program);
+  countPainted();
+  console.log('numPainted=', numPainted, 'numOutputs=', numOutputs);
+  return numPainted;
+}
+const printProgram = (programMemory = [], startIp = 0) => {
+  let programString = '[' +  programMemory.join(',') + ']';
+  console.log(
+    'ampIndex= ', ampIndex, ' phase= ', phases[ampIndex],
+    ' input= ', input, ':\t\t', programString, '\toutput= ', output, 'startIp= ', startIp);
+}
+const solve_p2 = (program = []) => {
+  // console.log('program = ', program);
+  input = 2;
+  return transform(program);
 }
 const parse = (lines = ['']) => {
-  // return lines[0]; // use for one line string input
-  // return lines;    // use for multi-line string input
-  return lines.map((line) => parseInt(line)).filter((num) => num === num);
+  return lines[0].split(',').map((value) => parseInt(value)).filter((num) => num === num);
 };
-module.exports = {var1, var2, fn1, fn2, solve, parse};
+module.exports = {setInput, getOutput, transform, solve, solve_p2, parse,
+  getNumPainted, nextInput, nextOutput, initState, countPainted};
--- advent2019/11/puzzle.spec.js
+++ advent2019/11/puzzle.spec.js
@@ -1,4 +1,4 @@
-describe("2019 day 6", function() {
+describe("2019 day 11", function() {
   // new code
   let var1 = 0;
   let var2 = '';
@@ -22,7 +22,7 @@ describe("2019 day 6", function() {
   const parse = (lines = ['']) => {
     // return lines[0]; // use for one line string input
     // return lines;    // use for multi-line string input
-    return lines.map((line) => parseInt(line)).filter((num) => num === num);
+    return lines[0].split(',').map((line) => parseInt(line)).filter((num) => num === num);
   };
 
 
@@ -50,32 +50,79 @@ describe("2019 day 6", function() {
       ''
       );
   });
-  it("can solve puzzle", () => {
-    const data = [
-      [],
-    ];
-    const actual = data.map((data) => solve(data));
-    const expected = [
-      1,
-    ];
+  it("can paint remembering how many painted at least once", () => {
+    puzzle.initState();
+    puzzle.countPainted();
+    expect(puzzle.getNumPainted()).toEqual(0);
+    
+    expect(puzzle.nextInput()).toEqual(0);
+    puzzle.nextOutput(1);
+    puzzle.nextOutput(0);
+    expect(puzzle.nextInput()).toEqual(0);
+    puzzle.nextOutput(0);
+    puzzle.nextOutput(0);
+    puzzle.nextOutput(1);
+    puzzle.nextOutput(0);
+    puzzle.nextOutput(1);
+    puzzle.nextOutput(0);
+    expect(puzzle.nextInput()).toEqual(1);
+    // const data = [
+    //   [],
+    // ];
+    // const actual = data.map((data) => puzzle.solve(data));
+    // const expected = [
+    //   1,
+    // ];
     // expect(actual).toEqual(expected);
   });
   it("can parse input", () => {
-    const data = parse(
-      '+1 +3 +2'
-      .split(
-        ' '
-        ));
-    expect(data).toEqual([1, 3, 2]);
+    const data = puzzle.parse(
+      ['+1,-3,+2']);
+    expect(data).toEqual([1, -3, 2]);
   });
   it("can solve puzzle with my input", () => {
     // const data = [0];
-    const data = parse(lines);
-    const answer = solve(data);
+    const data = puzzle.parse(lines);
+    const answer = puzzle.solve(data);
     console.log("part 1 answer is " + answer);
     // expect(answer).toEqual(0);
   });
 
+  // 2019 day 9 tests (extended intcode computer, based on days 2 and 5)
+  describe("2019 day 9", function() {
+    // it("can handle relative mode", () => {
+    //   const data = [
+    //     [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99],
+    //     [1102,34915192,34915192,7,4,7,99,0],
+    //     [104,1125899906842624,99],
+    //   ];
+    //   const actual = data.map((data) => puzzle.transform(data));
+    //   const expected = [
+    //     99,
+    //     1219070632396864,
+    //     1125899906842624,
+    //   ];
+    //   // console.log('actual=', actual);
+    //   // console.log('actual=', expected);
+    //   expect(actual).toEqual(expected);
+    // });
+    // it("can parse input", () => {
+    //   const data = puzzle.parse(['1,2,3']);
+    //   expect(data).toEqual([1, 2, 3]);
+    // });
+    // it("can solve puzzle with my input", () => {
+    //   const data = puzzle.parse(lines);
+    //   const answer = puzzle.solve(data);
+    //   console.log("part 1 answer is " + answer);
+    //   // expect(answer).toEqual(3533056970);
+    // });
+    // it("can solve puzzle part 2 with my input", () => {
+    //   const data = puzzle.parse(lines);
+    //   const answer = puzzle.solve_p2(data);
+    //   console.log("part 2 answer is " + answer);
+    //   // expect(answer).toEqual(72852);
+    // });
+  });
 
 
 
@@ -91,13 +138,13 @@ describe("2019 day 6", function() {
   const Parser = require('../../common/parser');
   const puzzle = require('./puzzle');
   const parser = new Parser();
-  const readInputFile = false; // change to true to read input file for all tests that need it
+  const readInputFile = true; // change to true to read input file for all tests that need it
   let lines = [];
   beforeAll((done) => {
     console.log("2019 day 6:");
     if (readInputFile) {
       // "adventYYYY/DD/input.txt" for specific file, undefined for stdin
-      parser.readLines("advent2019/09/input.txt", (linesRead) => {
+      parser.readLines("advent2019/11/input.txt", (linesRead) => {
         lines = linesRead;
         done();
       });
