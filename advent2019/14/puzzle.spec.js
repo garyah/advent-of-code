741c2bdb65469e19db703f381e6aaeefbb8b3afc
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2019-12-14 02:38:07 -0800

    Solve 2019 day 14 part 2.  Don't expect to run this again, because it takes 20 minutes to get the answer!
    Clearly there is a more optimal solution.  Maybe one day I'll implement it.

--- advent2019/14/puzzle.spec.js
+++ advent2019/14/puzzle.spec.js
@@ -1,18 +1,5 @@
 describe("2019 day 14", function() {
   // new code
-  let var1 = 0;
-  let var2 = '';
-  const fn1 = (arg1 = 0, arg2 = '', arg3 = []) => {
-    if (1) {}
-    else if (1) {}
-    else {}
-    for (let i = 0;; i++) { break; continue; }
-    for (const item of arg3) {}
-    return 0;
-  };
-  const fn2 = () => {
-    return '';
-  };
   let recipeMap = new Map();
   let usageMap = new Map();
   let totalOre = 0;
@@ -50,7 +37,7 @@ describe("2019 day 14", function() {
         usageMap.set(item.name, onHand);
       }
     }
-  }
+  };
   const solve = (recipes = [[[[]]]]) => {
     recipeMap = new Map();
     for (const recipe of recipes) {
@@ -89,7 +76,17 @@ describe("2019 day 14", function() {
     console.log('totalOre=', totalOre);
   
     return totalOre;
-  }
+  };
+  const solve_p2 = (recipes = [[[[]]]]) => {
+    solve(recipes);
+    let totalFuel = 0;
+    const fuelRecipe = recipeMap.get('FUEL');
+    while (totalOre < 1000 * 1000 * 1000 * 1000) {
+      walkItemList(fuelRecipe.itemList);
+      totalFuel++;
+    }
+    return totalFuel;
+  };
   const parse = (lines = ['']) => {
     // return lines[0]; // use for one line string input
     // return lines;    // use for multi-line string input
@@ -119,16 +116,6 @@ describe("2019 day 14", function() {
 
 
   // new tests
-  it('fn1() returns number 0', () => {
-    expect(fn1()).toEqual(
-      0
-      );
-  });
-  it('fn2() returns empty string', () => {
-    expect(fn2()).toEqual(
-      ''
-      );
-  });
   it("can solve puzzle", () => {
     const data = [
       [
@@ -193,7 +180,7 @@ describe("2019 day 14", function() {
         '5 BHXH, 4 VRPVC => 5 LTCX',
       ],
     ];
-    const actual = data.map((data) => solve(parse(data)));
+    // const actual = data.map((data) => solve(parse(data)));
     const expected = [
         31,
         165,
@@ -224,12 +211,92 @@ describe("2019 day 14", function() {
     expect(data).toEqual(expected);
   });
   it("can solve puzzle with my input", () => {
-    // const data = [0];
     const data = parse(lines);
     // console.log("part 1 data is " + data);
     const answer = solve(data);
     console.log("part 1 answer is " + answer);
     expect(answer).toEqual(1037742);
+  });
+  it("can solve puzzle part 2", () => {
+    const data = [
+      // [
+      //   '10 ORE => 10 A',
+      //   '1 ORE => 1 B',
+      //   '7 A, 1 B => 1 C',
+      //   '7 A, 1 C => 1 D',
+      //   '7 A, 1 D => 1 E',
+      //   '7 A, 1 E => 1 FUEL',
+      // ],
+      // [
+      //   '9 ORE => 2 A',
+      //   '8 ORE => 3 B',
+      //   '7 ORE => 5 C',
+      //   '3 A, 4 B => 1 AB',
+      //   '5 B, 7 C => 1 BC',
+      //   '4 C, 1 A => 1 CA',
+      //   '2 AB, 3 BC, 4 CA => 1 FUEL',
+      // ],
+      // [
+      //   '157 ORE => 5 NZVS',
+      //   '165 ORE => 6 DCFZ',
+      //   '44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL',
+      //   '12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ',
+      //   '179 ORE => 7 PSHF',
+      //   '177 ORE => 5 HKGWZ',
+      //   '7 DCFZ, 7 PSHF => 2 XJWVT',
+      //   '165 ORE => 2 GPVTF',
+      //   '3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT',
+      // ],
+      // [
+      //   '2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG',
+      //   '17 NVRVD, 3 JNWZP => 8 VPVL',
+      //   '53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL',
+      //   '22 VJHF, 37 MNCFX => 5 FWMGM',
+      //   '139 ORE => 4 NVRVD',
+      //   '144 ORE => 7 JNWZP',
+      //   '5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC',
+      //   '5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV',
+      //   '145 ORE => 6 MNCFX',
+      //   '1 NVRVD => 8 CXFTF',
+      //   '1 VJHF, 6 MNCFX => 4 RFSQX',
+      //   '176 ORE => 6 VJHF',
+      // ],
+      [
+        '171 ORE => 8 CNZTR',
+        '7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL',
+        '114 ORE => 4 BHXH',
+        '14 VRPVC => 6 BMBT',
+        '6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL',
+        '6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT',
+        '15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW',
+        '13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW',
+        '5 BMBT => 4 WPTQ',
+        '189 ORE => 9 KTJDG',
+        '1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP',
+        '12 VRPVC, 27 CNZTR => 2 XDBXC',
+        '15 KTJDG, 12 BHXH => 5 XCVML',
+        '3 BHXH, 2 VRPVC => 7 MZWV',
+        '121 ORE => 7 VRPVC',
+        '7 XCVML => 6 RJRHP',
+        '5 BHXH, 4 VRPVC => 5 LTCX',
+      ],
+    ];
+    // const actual = data.map((data) => solve_p2(parse(data)));
+    const expected = [
+        // ???,
+        // ???,
+        // 82892753,
+        // 5586022,
+        460664,
+    ];
+    // expect(actual).toEqual(expected);
+  });
+  it("can solve puzzle part 2 with my input", () => {
+    const data = parse(lines);
+    // console.log("part 1 data is " + data);
+    const answer = solve_p2(data);
+    console.log("part 2 answer is " + answer);
+    expect(answer).toEqual(1572358);
   });
 
 
