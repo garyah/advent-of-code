d306699c084f4f3a5875991df2dd70a7e8699447
Gary Hildebrand <garyah@gmail.com>
2019-12-15 22:30:17 -0800

    Prep 2019 day 16 part 1.

--- advent2019/16/puzzle.spec.js
+++ advent2019/16/puzzle.spec.js
@@ -1,4 +1,4 @@
-describe("2019 day 6", function() {
+describe("2019 day 16", function() {
   // new code
   let var1 = 0;
   let var2 = '';
@@ -91,13 +91,13 @@ describe("2019 day 6", function() {
   const Parser = require('../../common/parser');
   const puzzle = require('./puzzle');
   const parser = new Parser();
-  const readInputFile = false; // change to true to read input file for all tests that need it
+  const readInputFile = true; // change to true to read input file for all tests that need it
   let lines = [];
   beforeAll((done) => {
-    console.log("2019 day 6:");
+    console.log("2019 day 16:");
     if (readInputFile) {
       // "adventYYYY/DD/input.txt" for specific file, undefined for stdin
-      parser.readLines("advent2019/09/input.txt", (linesRead) => {
+      parser.readLines("advent2019/16/input.txt", (linesRead) => {
         lines = linesRead;
         done();
       });
