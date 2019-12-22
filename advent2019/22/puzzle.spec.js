eefc92e50f03d0febc90ceb8745e5448806b203e
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2019-12-21 21:19:43 -0800

    Prep 2019 day 22, and do minimal parse.

--- advent2019/22/puzzle.spec.js
+++ advent2019/22/puzzle.spec.js
@@ -1,4 +1,4 @@
-describe("2019 day 6", function() {
+describe("2019 day 22", function() {
   // new code
   let var1 = 0;
   let var2 = '';
@@ -22,7 +22,15 @@ describe("2019 day 6", function() {
   const parse = (lines = ['']) => {
     // return lines[0]; // use for one line string input
     // return lines;    // use for multi-line string input
-    return lines.map((line) => parseInt(line)).filter((num) => num === num);
+    return lines.map((line) => {
+      if (line.startsWith('deal into new stack')) {
+        return {action: 'deal'};
+      } else if (line.startsWith('cut ')) {
+        return {action: 'cut', amount: parseInt(line.substr(4))};
+      } else if (line.startsWith('deal with increment ')) {
+        return {action: 'deali', amount: parseInt(line.substr(20))};
+      }
+    });
   };
 
 
@@ -54,25 +62,29 @@ describe("2019 day 6", function() {
     const data = [
       [],
     ];
-    const actual = data.map((data) => solve(data));
+    // const actual = data.map((data) => solve(data));
     const expected = [
       1,
     ];
     // expect(actual).toEqual(expected);
   });
   it("can parse input", () => {
-    const data = parse(
-      '+1 +3 +2'
-      .split(
-        ' '
-        ));
-    expect(data).toEqual([1, 3, 2]);
+    const data = parse([
+      'deal into new stack',
+      'cut 7990',
+      'cut -5698',
+      'deal with increment 29',
+    ]);
+    expect(data).toEqual([
+      {action: 'deal'}, {action: 'cut', amount: 7990},
+      {action: 'cut', amount: -5698}, {action: 'deali', amount: 29},
+    ]);
   });
   it("can solve puzzle with my input", () => {
     // const data = [0];
     const data = parse(lines);
-    const answer = solve(data);
-    console.log("part 1 answer is " + answer);
+    // const answer = solve(data);
+    // console.log("part 1 answer is " + answer);
     // expect(answer).toEqual(0);
   });
 
@@ -91,13 +103,13 @@ describe("2019 day 6", function() {
   const Parser = require('../../common/parser');
   const puzzle = require('./puzzle');
   const parser = new Parser();
-  const readInputFile = false; // change to true to read input file for all tests that need it
+  const readInputFile = true; // change to true to read input file for all tests that need it
   let lines = [];
   beforeAll((done) => {
-    console.log("2019 day 6:");
+    console.log("2019 day 22:");
     if (readInputFile) {
       // "adventYYYY/DD/input.txt" for specific file, undefined for stdin
-      parser.readLines("advent2019/09/input.txt", (linesRead) => {
+      parser.readLines("advent2019/22/input.txt", (linesRead) => {
         lines = linesRead;
         done();
       });
