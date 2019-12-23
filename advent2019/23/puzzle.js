8e53fe403f129dbabd77444eb2f03a146e29d35c
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2019-12-23 13:15:41 -0800

    2019 day 23 part 1. Support 50 separate copies of the program, with independent inputs and input state.  Does not yet switch execution between the instances.

--- advent2019/23/puzzle.js
+++ advent2019/23/puzzle.js
@@ -1,33 +1,27 @@
 // 2019 day 23 code (extended intcode computer, based on day 15)
 // console.log('puzzle object reaches here, input, output: ', input, output);
-let input = -1;
+const numCpus = 50;
+// let input = -1;
 let output = -1;
 let phases = [-1, -1, -1, -1, -1];
-let programs = [{memory: [0], startIp: 0, isResumed: false, relativeBase: 0}];
-let ampIndex = 0;
-let inputState = 0;
+let programs = [{memory: [0], startIp: 0, inputState: 0, relativeBase: 0, input: -1}];
+let cpuIndex = 0;
+// let inputState = 0;
 // console.log('puzzle object reaches here, input, output: ', input, output);
-const setInput = (value = 0) => {
-  input = value;
-};
+// const setInput = (value = 0) => {
+//   input = value;
+// };
 const getOutput = () => {
   return output;
 };
-const transform = (inputProgram = []) => {
-  programs[ampIndex] = {memory: [], startIp: 0, isResumed: false, relativeBase: 0};
-  programs[ampIndex].memory = inputProgram.map((value) => {
-    // if (ampIndex === 0) console.log(value);
-    return value;
-  });
-  // console.log('numBlockTiles=', numBlockTiles, 'first memory=', programs[ampIndex].memory[0]);
-  for (let n = 0; n < 1000; n++) programs[ampIndex].memory.push(0);
-  const program = programs[ampIndex].memory;
+const transform = () => {
+  const program = programs[cpuIndex].memory;
   let ip = 0;
   // console.log(program);
   // console.log('input, output before program execution: ', input, output);
-  for (ip = programs[ampIndex].startIp; ip < program.length; ) {
+  for (ip = programs[cpuIndex].startIp; ip < program.length; ) {
     if (program[ip] === 99) {
-      programs[ampIndex].startIp = -1;
+      programs[cpuIndex].startIp = -1;
       return output;
     }
     let step = 4;
@@ -37,65 +31,65 @@ const transform = (inputProgram = []) => {
     const opcode = program[ip] % 100;
     if (opcode === 1) { // add
       const result
-        = ((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]])
-          + ((mode_p2 == 2) ? program[programs[ampIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]]);
-      if (mode_p3 == 2) { program[programs[ampIndex].relativeBase + program[ip+3]] = result; } else { program[program[ip+3]] = result; }
+        = ((mode_p1 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]])
+          + ((mode_p2 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]]);
+      if (mode_p3 == 2) { program[programs[cpuIndex].relativeBase + program[ip+3]] = result; } else { program[program[ip+3]] = result; }
     } else if (opcode === 2) { // multiply
       const result
-        = ((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]])
-          * ((mode_p2 == 2) ? program[programs[ampIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]]);
-      if (mode_p3 == 2) { program[programs[ampIndex].relativeBase + program[ip+3]] = result; } else { program[program[ip+3]] = result; }
+        = ((mode_p1 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]])
+          * ((mode_p2 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]]);
+      if (mode_p3 == 2) { program[programs[cpuIndex].relativeBase + program[ip+3]] = result; } else { program[program[ip+3]] = result; }
     } else if (opcode === 3) { // input
-      input = nextInput();
+      nextInput();
+      const input = programs[cpuIndex].input;
       if (input === -100) return output;
-      if (mode_p1 == 2) { program[programs[ampIndex].relativeBase + program[ip+1]] = input; } else { program[program[ip+1]] = input; }
-      // programs[ampIndex].isResumed = true;
+      if (mode_p1 == 2) { program[programs[cpuIndex].relativeBase + program[ip+1]] = input; } else { program[program[ip+1]] = input; }
       step = 2;
     } else if (opcode === 4) { // output
       // console.log(program);
       // console.log('output before mod: ', output);
-      output = ((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]);
+      output = ((mode_p1 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]);
       // console.log('output after mod: ', output);
       nextOutput(output);
-      programs[ampIndex].startIp = ip + 2;
+      // programs[cpuIndex].startIp = ip + 2;
       step = 2;
-      // return programs[ampIndex].startIp;
+      // return programs[cpuIndex].startIp;
     } else if (opcode === 5) { // jump-if-true
       step = 3;
-      if (((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]) !== 0) {
-        ip = (mode_p2 == 2) ? program[programs[ampIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]];
+      if (((mode_p1 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]) !== 0) {
+        ip = (mode_p2 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]];
         step = 0;
       }
     } else if (opcode === 6) { // jump-if-false
       step = 3;
-      if (((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]) === 0) {
-        ip = (mode_p2 == 2) ? program[programs[ampIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]];
+      if (((mode_p1 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]) === 0) {
+        ip = (mode_p2 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]];
         step = 0;
       }
     } else if (opcode === 7) { // less than
-      if (((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]])
-          < ((mode_p2 == 2) ? program[programs[ampIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]])) {
-        if (mode_p3 == 2) { program[programs[ampIndex].relativeBase + program[ip+3]] = 1; } else { program[program[ip+3]] = 1; }
+      if (((mode_p1 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]])
+          < ((mode_p2 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]])) {
+        if (mode_p3 == 2) { program[programs[cpuIndex].relativeBase + program[ip+3]] = 1; } else { program[program[ip+3]] = 1; }
       } else {
-        if (mode_p3 == 2) { program[programs[ampIndex].relativeBase + program[ip+3]] = 0; } else { program[program[ip+3]] = 0; }
+        if (mode_p3 == 2) { program[programs[cpuIndex].relativeBase + program[ip+3]] = 0; } else { program[program[ip+3]] = 0; }
       }
     } else if (opcode === 8) { // equals
-      if (((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]])
-          === ((mode_p2 == 2) ? program[programs[ampIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]])) {
-        if (mode_p3 == 2) { program[programs[ampIndex].relativeBase + program[ip+3]] = 1; } else { program[program[ip+3]] = 1; }
+      if (((mode_p1 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]])
+          === ((mode_p2 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+2]] : (mode_p2 == 1) ? program[ip+2] : program[program[ip+2]])) {
+        if (mode_p3 == 2) { program[programs[cpuIndex].relativeBase + program[ip+3]] = 1; } else { program[program[ip+3]] = 1; }
       } else {
-        if (mode_p3 == 2) { program[programs[ampIndex].relativeBase + program[ip+3]] = 0; } else { program[program[ip+3]] = 0; }
+        if (mode_p3 == 2) { program[programs[cpuIndex].relativeBase + program[ip+3]] = 0; } else { program[program[ip+3]] = 0; }
       }
     } else if (opcode === 9) { // relative base offset
-      programs[ampIndex].relativeBase += ((mode_p1 == 2) ? program[programs[ampIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]);
+      programs[cpuIndex].relativeBase += ((mode_p1 == 2) ? program[programs[cpuIndex].relativeBase + program[ip+1]] : (mode_p1 == 1) ? program[ip+1] : program[program[ip+1]]);
       step = 2;
     }
     // console.log(program[ip]);
     ip += step;
   }
   // console.log('transformed program: ' + program);
-  programs[ampIndex].startIp = ip;
-  return programs[ampIndex].startIp;
+  programs[cpuIndex].startIp = ip;
+  return programs[cpuIndex].startIp;
 };
 
 let area = [[-1]];
@@ -114,20 +108,18 @@ const getNumPainted = () => {
 };
 const nextInput = () => {
   if (wantToExit) return -100;
-  if (inputState === 1) input = -1;
-  inputState = (inputState === 0) ? 1 : 0;
+  if (programs[cpuIndex].inputState === 1) programs[cpuIndex].input = -1;
+  programs[cpuIndex].inputState = 1;
 
   const date = Date.now();
   let currentDate = null;
   do {
     currentDate = Date.now();
   } while (currentDate - date < 2 * 1000);
-  console.log("request for input, returning", input);
-
-  return input;
+  console.log("request for input=", programs[cpuIndex].input, 'cpu=', cpuIndex);
 };
 const nextOutput = (output = 0) => {
-  console.log('output=', output);
+  console.log('output=', output, 'cpu=', cpuIndex);
   numOutputs++;
 };
 const initState = () => {
@@ -147,7 +139,7 @@ const initState = () => {
   wallsMinY = gridXSize / 2, wallsMaxY = 0;
   wantToExit = false;
   isAggressive = false;
-  inputState = 0;
+  // inputState = 0;
 };
 const countPainted = () => {
   numPainted = 0;
@@ -189,30 +181,43 @@ const printArea = () => {
     console.log(line);
   }
 };
+const execute = (program = []) => {
+  // input = 0; // initially give address network for first computer
+  for (cpuIndex = 0; cpuIndex < numCpus; cpuIndex++) {
+    programs[cpuIndex] = {memory: [], startIp: 0, inputState: 0, relativeBase: 0, input: cpuIndex};
+    programs[cpuIndex].memory = program.map((value) => {
+      // if (cpuIndex === 0) console.log(value);
+      return value;
+    });
+    for (let n = 0; n < 1000; n++) programs[cpuIndex].memory.push(0);
+    // console.log('cpuIndex = ', cpuIndex, 'programs.length = ', programs.length);
+    // printProgram(programs[cpuIndex].memory, programs[cpuIndex].startIp, programs[cpuIndex].input);
+    // console.log('cpuIndex = ', cpuIndex, 'input=', input);
+    transform();
+    // console.log('cpuIndex = ', cpuIndex, 'output=', output);
+    // input = output;
+  }
+}
 const solve = (program = []) => {
   console.log();
   initState();
-  input = 0; // initially give address network for first computer
-  area[currentX][currentY] = 3;
-  transform(program);
+  execute(program);
   // return numBlockTiles;
 }
-const printProgram = (programMemory = [], startIp = 0) => {
+const printProgram = (programMemory = [], startIp = 0, input = 0) => {
   let programString = '[' +  programMemory.join(',') + ']';
   console.log(
-    'ampIndex= ', ampIndex, ' phase= ', phases[ampIndex],
+    'cpuIndex= ', cpuIndex, 
     ' input= ', input, ':\t\t', programString, '\toutput= ', output, 'startIp= ', startIp);
 }
 const solve_p2 = (program = []) => {
+  console.log();
   initState();
-  isAggressive = true;
-  input = 1; // initially go north
-  area[currentX][currentY] = 3;
-  transform(program);
+  execute(program);
   // return numBlockTiles;
 }
 const parse = (lines = ['']) => {
   return lines[0].split(',').map((value) => parseInt(value)).filter((num) => num === num);
 };
-module.exports = {setInput, getOutput, transform, solve, solve_p2, parse,
+module.exports = {transform, solve, printProgram, solve_p2, parse,
   getNumPainted, nextInput, nextOutput, initState, countPainted, printArea};
