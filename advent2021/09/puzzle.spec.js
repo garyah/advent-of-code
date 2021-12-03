0a58ec2fba44fdeed50bd3fa32e56eb7e22a59e2
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2021-12-02 23:05:23 -0800

    Add rest of year's contest days, using baseline puzzle.spec.js from common, no input.txt.

---
+++ advent2021/09/puzzle.spec.js
@@ -0,0 +1,133 @@
+describe("2021 day xx", function() {
+  // new code
+  let var1 = 0;
+  let var2 = '';
+  const fn1 = (arg1 = 0, arg2 = '', arg3 = []) => {
+    if (1) {}
+    else if (1) {}
+    else {}
+    for (let i = 0;; i++) { break; continue; }
+    for (const item of arg3) {}
+    return 0;
+  };
+  const fn2 = () => {
+    return '';
+  };
+  const solve = (data = [0]) => {
+    fn1();
+    return data.reduce((sum, num) => {
+      return sum + num;
+    }, 0);
+  }
+  const solve_p2 = (data = [0]) => {
+    fn1();
+    return data.reduce((sum, num) => {
+      return sum + num;
+    }, 0);
+  }
+  const parse = (lines = ['']) => {
+    // return parser.getFirstLine(lines);
+    // return lines;    // use for multi-line string input
+    // return parser.linesToInts(lines);
+    // return parser.linesToFloats(lines);
+    return parser.linesToDirCommands(lines);
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // new tests
+  it('fn1() returns number 0', () => {
+    expect(fn1()).toEqual(
+      0
+      );
+  });
+  it('fn2() returns empty string', () => {
+    expect(fn2()).toEqual(
+      ''
+      );
+  });
+  it("can parse input", () => {
+    const data = parse(
+      '+1 +3 +2'
+      .split(
+        ' '
+        ));
+    expect(data).toEqual([1, 3, 2]);
+  });
+  it("can solve puzzle", () => {
+    const data = [
+      [],
+    ];
+    // const actual = data.map((data) => solve(data));
+    const expected = [
+      1,
+    ];
+    // expect(actual).toEqual(expected);
+  });
+  it("can solve puzzle p2", () => {
+    const data = [
+      [],
+    ];
+    // const actual = data.map((data) => solve_p2(data));
+    const expected = [
+      1,
+    ];
+    // expect(actual).toEqual(expected);
+  });
+  it("can solve puzzle with my input", () => {
+    // const data = [0];
+    const data = parse(lines);
+    const answer = solve(data);
+    console.log("part 1 answer is " + answer);
+    // expect(answer).toEqual(15416);
+  });
+  it("can solve puzzle p2 with my input", () => {
+    // const data = [0];
+    const data = parse(lines);
+    const answer = solve_p2(data);
+    console.log("part 2 answer is " + answer);
+    // expect(answer).toEqual(15416);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+  // boilerplate
+  const Parser = require('../../common/parser');
+  // const puzzle = require('../../common/puzzleWithCode');
+  const parser = new Parser();
+  const readInputFile = true; // change to true to read input file for all tests that need it
+  let lines = [];
+  beforeAll((done) => {
+    console.log("2021 day xx:");
+    if (readInputFile) {
+      // "adventYYYY/DD/input.txt" for specific file, undefined for stdin
+      parser.readLines("advent2021/xx/input.txt", (linesRead) => {
+        lines = linesRead;
+        done();
+      });
+      return;
+    }
+    done();
+  });
+});
