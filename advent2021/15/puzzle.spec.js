eb80c640921dc56f1490208b38da81a48d5cd6ce
Gary Hildebrand <ghilde@microsoft.com>
2021-12-19 12:12:30 -0800

    Incorrect solution with one-level lookahead on moves, cannot properly solve for 4x4 or greater.

--- advent2021/15/puzzle.spec.js
+++ advent2021/15/puzzle.spec.js
@@ -5,29 +5,58 @@ describe("2021 day 15", function() {
   let var3 = [];
   let var4 = {};
   const initVars = () => {
-    data = [0];
+    riskMap = [''];
     var1 = 0;
     var2 = '';
     var3 = [];
     var4 = {};
-    answer = 0;
+    minRisk = 0;
   };
-  const fn1 = () => {
-    // console.log(...);
-    if (1) {}
-    else if (1) {}
-    else {}
-    var2.split(' ').join(' ');
-    for (let i = 0; i < var3.length; i++) { break; continue; }
-    for (let j = 0;; j++) { break; continue; }
-    for (const item of var3) { break; continue; }
-    var3.map((num) => {
-      return num;
-    }).filter((num) => num === num);
-    var3.reduce((sum, num) => {
-      return sum + num;
-    }, 0);
-    return;
+  const walkMap = () => {
+    // console.log('riskMap.length = ', riskMap.length);
+    // console.log('riskMap[0].length = ', riskMap[0].length);
+    const size = riskMap.length;
+    const numSteps = (size - 1) * 2;
+    console.log('numSteps = ', numSteps);
+    minRisk = 0;
+    let r = 0, c = 0;
+    for (let n = 0; n < numSteps; n++) {
+      console.log('r = ', r, ', c = ', c, ', n = ', n, ', minRisk = ', minRisk);
+      const riskRight = (c + 1 < size) ? parseInt(riskMap[r][c + 1]) : 100000;
+      const riskDown = (r + 1 < size) ? parseInt(riskMap[r + 1][c]) : 100000;
+      // console.log('riskRight = ', riskRight);
+      // console.log('riskDown = ', riskDown);
+      // if (riskRight < riskDown) {
+      //   minRisk += riskRight;
+      //   c += 1;
+      //   continue;
+      // }
+      // if (riskRight > riskDown) {
+      //   minRisk += riskDown;
+      //   r += 1;
+      //   continue;
+      // }
+      const riskRightRight = (c + 2 < size) ? parseInt(riskMap[r][c + 2]) : 100000;
+      const riskRightDown = (r + 1 < size && c + 1 < size) ? parseInt(riskMap[r + 1][c + 1]) : 100000;
+      const riskDownRight = riskRightDown;
+      const riskDownDown = (r + 2 < size) ? parseInt(riskMap[r + 2][c]) : 100000;
+      if (riskRight + Math.min(riskRightRight, riskRightDown) < riskDown + Math.min(riskDownRight, riskDownDown)) {
+        minRisk += riskRight;
+        c += 1;
+        continue;
+      }
+      if (riskRight + Math.min(riskRightRight, riskRightDown) > riskDown + Math.min(riskDownRight, riskDownDown)) {
+        minRisk += riskDown;
+        r += 1;
+        continue;
+      }
+      console.log('could not resolve tie, will move right anyway! r = ', r, ', c = ', c);
+      minRisk += riskRight;
+      c += 1;
+      // break; continue;
+    }
+    console.log('r = ', r, ', c = ', c, ', minRisk = ', minRisk);
+    console.log(' = ');
   };
   const fn2 = () => {
     // console.log(...);
@@ -97,27 +126,20 @@ describe("2021 day 15", function() {
     }, 0);
     return;
   };
-  let answer = 0;
+  let minRisk = 0;
   const solve = () => {
     // console.log(...);
-    fn1();
-    answer = 1;
+    walkMap();
     // console.log(...);
   }
   const solve_p2 = () => {
     // console.log(...);
-    fn1();
-    answer = 2;
+    walkMap();
     // console.log(...);
   }
-  let data = [0];
+  let riskMap = [''];
   const parse = () => {
-    data = parser.lineToIntsComma(lines[0]);
-    // data = parser.linesToInts(lines);
-    // data = lines;    // use for multi-line string input
-    // data = parser.getFirstLine(lines);
-    // data = parser.linesToFloats(lines);
-    // data = parser.linesToDirCommands(lines);
+    riskMap = lines;    // use for multi-line string input
   };
 
 
@@ -135,30 +157,123 @@ describe("2021 day 15", function() {
 
 
   // tests
-  // it('fn1() ...', () => {
-  //   fn1();
-  //   expect(0).toEqual(
-  //     0
-  //     );
-  // });
-  // it("can parse input", () => {
-  //   lines = '+1,+3,+2 1,2,3'
-  //     .split(
-  //       ' '
-  //       );
-  //   // parse();
-  //   // expect(data).toEqual([1, 3, 2]);
-  // });
+  it('walkMap() finds min risk for various 2x2 paths: obvious down, right, and tie', () => {
+    riskMap = [
+      '19',
+      '11'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(2);
+    riskMap = [
+      '11',
+      '91'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(2);
+    riskMap = [
+      '11',
+      '11'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(2);
+  });
+  it('walkMap() finds min risk for various 3x3 paths: obvious down, right, and tie', () => {
+    riskMap = [
+      '199',
+      '199',
+      '111'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(4);
+    riskMap = [
+      '111',
+      '991',
+      '991'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(4);
+    riskMap = [
+      '111',
+      '111',
+      '111'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(4);
+  });
+  it('walkMap() finds min risk for 3x3 path with diagonal path min risk', () => {
+    riskMap = [
+      '199',
+      '919',
+      '991'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(20);
+  });
+  it('walkMap() finds min risk for 3x3 path with tricky risks requiring look-ahead more than one move', () => {
+    riskMap = [
+      '199',
+      '949',
+      '341'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(17);
+  });
+  it('walkMap() finds min risk for various 4x4 paths: obvious down, right, and tie', () => {
+    riskMap = [
+      '1999',
+      '1999',
+      '1999',
+      '1111'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(6);
+    riskMap = [
+      '1111',
+      '9991',
+      '9991',
+      '9991'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(6);
+    riskMap = [
+      '1111',
+      '1111',
+      '1111',
+      '1111'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(6);
+  });
+  it('walkMap() finds min risk for 4x4 path with diagonal path min risk', () => {
+    riskMap = [
+      '1999',
+      '9199',
+      '9919',
+      '9991'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(30);
+  });
+  it('walkMap() finds min risk for 4x4 path with tricky risks requiring look-ahead more than two moves', () => {
+    riskMap = [
+      '1999',
+      '9999',
+      '9499',
+      '3441'
+    ];
+    walkMap();
+    expect(minRisk).toEqual(30);
+  });
   it("can solve puzzle with my input", () => {
-    parse();
-    solve();
-    console.log("\npart 1 answer is " + answer);
+    // parse();
+    // solve();
+    // console.log("\npart 1 answer is " + minRisk);
     // expect(answer).toEqual(1);
   });
   it("can solve puzzle p2 with my input", () => {
-    parse();
+    // parse();
     // solve_p2();
-    console.log("\npart 2 answer is " + answer);
+    // console.log("\npart 2 answer is " + minRisk);
     // expect(answer).toEqual(2);
   });
 
@@ -186,7 +301,7 @@ describe("2021 day 15", function() {
     console.log("2021 day 15:");
     if (readInputFile) {
       // "adventYYYY/DD/input.txt" for specific file, undefined for stdin
-      parser.readLines("advent2021/15/input.txt", (linesRead) => {
+      parser.readLines("advent2021/15/sainput.txt", (linesRead) => {
         lines = linesRead;
         done();
       });
