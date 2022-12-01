22e8b1758bd2b46d9d92287cc67b7c0e01af8650
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2022-11-30 21:59:22 -0800

    Day 1, both stars.

--- advent2022/01/Day01.java
+++ advent2022/01/Day01.java
@@ -1,22 +1,40 @@
 import java.io.*;
+import java.nio.charset.*;
+import java.nio.file.*;
+import java.util.*;
 
 public class Day01 {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        // Solution embedded in main()!
 
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+        Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\01\\input.txt");
+        List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
+        Deque<Integer> maxSums = new ArrayDeque<Integer>();
+        List<Integer> sums = new ArrayList<Integer>();
+        int maxSum = 0;
+        int sum = 0;
+        for (String line : lines) {
+            if (line.length() != 0) {
+                sum += Integer.parseInt(line);
+                continue;
+            }
+            if (sum > maxSum) {
+                maxSum = sum;
+                maxSums.push(maxSum);
+            }
+            sums.add(sum);
+            sum = 0;
+        }
 
-        String n = firstMultipleInput[0];
+        System.out.println("number of sums = " + sums.size());
+        System.out.println("sums = " + sums);
+        System.out.println("maxSum = " + maxSum);
+        System.out.println("number of successive maxSum values = " + maxSums.size());
+        System.out.println("maxSums = " + maxSums);
 
-        int k = Integer.parseInt(firstMultipleInput[1]);
-
-        // int result = Result.superDigit(n, k);
-
-        // bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        Object[] sumArray = sums.toArray();
+        Arrays.sort(sumArray);
+        // System.out.println("sum of top 3 = " + (maxSums.pop() + maxSums.pop() + maxSums.pop()));
+        System.out.println("sum of top 3 = " + ((int)sumArray[sumArray.length - 1] + (int)sumArray[sumArray.length - 2] + (int)sumArray[sumArray.length - 3]));
     }
 }
