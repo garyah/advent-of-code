17ca73837e9ca808220133cc93753aec436e811f
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2022-12-01 22:56:55 -0800

    Day 2, part 2.

--- advent2022/02/Day02.java
+++ advent2022/02/Day02.java
@@ -5,20 +5,17 @@ import java.util.*;
 
 public class Day02 {
     public static void main(String[] args) throws IOException {
-        // Solution embedded in main()!
-
+        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\02\\sample_input.txt");
         Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\02\\input.txt");
         List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
-        // Deque<Integer> maxSums = new ArrayDeque<Integer>();
-        // List<Integer> sums = new ArrayList<Integer>();
-        // int maxSum = 0;
         int totalScore = 0;
+        int totalScorePart2 = 0;
         for (String line : lines) {
             if (line.length() != 0) {
                 String[] fields = line.replaceAll("\\s+$", "").split(" ");
                 // sum += Integer.parseInt(line);
-                int score = processTurn(fields);
-                totalScore += score;
+                totalScore += processTurn(fields);
+                totalScorePart2 += processTurnPart2(fields);
                 continue;
             }
             // if (sum > maxSum) {
@@ -31,16 +28,7 @@ public class Day02 {
 
         System.out.println("# lines = " + lines.size());
         System.out.println("totalScore = " + totalScore);
-
-        // System.out.println("number of sums = " + sums.size());
-        // System.out.println("sums = " + sums);
-        // System.out.println("maxSum = " + maxSum);
-        // System.out.println("number of successive maxSum values = " + maxSums.size());
-        // System.out.println("maxSums = " + maxSums);
-
-        // Object[] sumArray = sums.toArray();
-        // Arrays.sort(sumArray);
-        // System.out.println("sum of top 3 = " + ((int)sumArray[sumArray.length - 1] + (int)sumArray[sumArray.length - 2] + (int)sumArray[sumArray.length - 3]));
+        System.out.println("totalScore for part2 = " + totalScorePart2);
     }
 
     static int processTurn(String[] fields) {
@@ -81,6 +69,49 @@ public class Day02 {
             } if (mine.equals("Z")) {
                 // Scissors
                 return 3 + 3;
+            }
+        }
+        return 0;
+    }
+
+    static int processTurnPart2(String[] fields) {
+        String theirs = fields[0];
+        String mine = fields[1];
+        if (theirs.equals("A")) {
+            // Rock
+            if (mine.equals("X")) {
+                // Lose, Scissors
+                return 3 + 0;
+            } if (mine.equals("Y")) {
+                // Draw, Rock
+                return 1 + 3;
+            } if (mine.equals("Z")) {
+                // Win, Paper
+                return 2 + 6;
+            }
+        } if (theirs.equals("B")) {
+            // Paper
+            if (mine.equals("X")) {
+                // Lose, Rock
+                return 1 + 0;
+            } if (mine.equals("Y")) {
+                // Draw, Paper
+                return 2 + 3;
+            } if (mine.equals("Z")) {
+                // Win, Scissors
+                return 3 + 6;
+            }
+        } if (theirs.equals("C")) {
+            // Scissors
+            if (mine.equals("X")) {
+                // Lose, Paper
+                return 2 + 0;
+            } if (mine.equals("Y")) {
+                // Draw, Scissors
+                return 3 + 3;
+            } if (mine.equals("Z")) {
+                // Win, Rock
+                return 1 + 6;
             }
         }
         return 0;
