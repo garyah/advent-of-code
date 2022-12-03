06446c65fae874195e77952530999f04ac244726
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2022-12-02 22:11:26 -0800

    Day 3 part 2 done.

--- advent2022/03/Day03.java
+++ advent2022/03/Day03.java
@@ -9,10 +9,12 @@ public class Day03 {
         Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\03\\input.txt");
         List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
         int totalPriority = 0;
-        // int totalScorePart2 = 0;
+        String[] group = new String[3];
+        int groupIndex = 0;
+        int totalPriorityPart2 = 0;
         for (String line : lines) {
             if (line.length() != 0) {
-                // String[] fields = line.replaceAll("\\s+$", "").split(" ");
+                // Part 1
                 String comp1 = line.substring(0, line.length() / 2);
                 String comp2 = line.substring(line.length() / 2, line.length());
                 char duplicate = 0;
@@ -24,99 +26,28 @@ public class Day03 {
                 }
                 int priority = (duplicate >= 'a' && duplicate <= 'z') ? duplicate - 'a' + 1 : duplicate - 'A' + 27;
                 totalPriority += priority;
-                // totalScorePart2 += processTurnPart2(fields);
-                continue;
+
+                // Part 2
+                group[groupIndex] = line;
+                if (groupIndex == 2) {
+                    duplicate = 0;
+                    for (int i = 0; i < group[0].length(); i++) {
+                        duplicate = group[0].charAt(i);
+                        if (group[1].indexOf(duplicate) > -1 && group[2].indexOf(duplicate) > -1) {
+                            break;
+                        }
+                    }
+                    priority = (duplicate >= 'a' && duplicate <= 'z') ? duplicate - 'a' + 1 : duplicate - 'A' + 27;
+                    totalPriorityPart2 += priority;
+                    groupIndex = 0;
+                } else {
+                    groupIndex++;
+                }
             }
         }
 
         System.out.println("# lines = " + lines.size());
         System.out.println("totalPriority = " + totalPriority);
-        // System.out.println("totalScore for part2 = " + totalScorePart2);
-    }
-
-    static int processTurn(String[] fields) {
-        String theirs = fields[0];
-        String mine = fields[1];
-        if (theirs.equals("A")) {
-            // Rock
-            if (mine.equals("X")) {
-                // Rock
-                return 1 + 3;
-            } if (mine.equals("Y")) {
-                // Paper
-                return 2 + 6;
-            } if (mine.equals("Z")) {
-                // Scissors
-                return 3 + 0;
-            }
-        } if (theirs.equals("B")) {
-            // Paper
-            if (mine.equals("X")) {
-                // Rock
-                return 1 + 0;
-            } if (mine.equals("Y")) {
-                // Paper
-                return 2 + 3;
-            } if (mine.equals("Z")) {
-                // Scissors
-                return 3 + 6;
-            }
-        } if (theirs.equals("C")) {
-            // Scissors
-            if (mine.equals("X")) {
-                // Rock
-                return 1 + 6;
-            } if (mine.equals("Y")) {
-                // Paper
-                return 2 + 0;
-            } if (mine.equals("Z")) {
-                // Scissors
-                return 3 + 3;
-            }
-        }
-        return 0;
-    }
-
-    static int processTurnPart2(String[] fields) {
-        String theirs = fields[0];
-        String mine = fields[1];
-        if (theirs.equals("A")) {
-            // Rock
-            if (mine.equals("X")) {
-                // Lose, Scissors
-                return 3 + 0;
-            } if (mine.equals("Y")) {
-                // Draw, Rock
-                return 1 + 3;
-            } if (mine.equals("Z")) {
-                // Win, Paper
-                return 2 + 6;
-            }
-        } if (theirs.equals("B")) {
-            // Paper
-            if (mine.equals("X")) {
-                // Lose, Rock
-                return 1 + 0;
-            } if (mine.equals("Y")) {
-                // Draw, Paper
-                return 2 + 3;
-            } if (mine.equals("Z")) {
-                // Win, Scissors
-                return 3 + 6;
-            }
-        } if (theirs.equals("C")) {
-            // Scissors
-            if (mine.equals("X")) {
-                // Lose, Paper
-                return 2 + 0;
-            } if (mine.equals("Y")) {
-                // Draw, Scissors
-                return 3 + 3;
-            } if (mine.equals("Z")) {
-                // Win, Rock
-                return 1 + 6;
-            }
-        }
-        return 0;
+        System.out.println("totalPriority for part2 = " + totalPriorityPart2);
     }
 }
