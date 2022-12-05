45dee38552c0bca040020a92494bb1c2b72a275b
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2022-12-04 22:09:25 -0800

    Day 5 part 1 done.

--- advent2022/05/Day05.java
+++ advent2022/05/Day05.java
@@ -5,41 +5,74 @@ import java.util.*;
 
 public class Day05 {
     public static void main(String[] args) throws IOException {
-        Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\05\\sample_input.txt");
-        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\05\\input.txt");
+        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\05\\sample_input.txt");
+        Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\05\\input.txt");
         List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
-        int numFullyContained = 0;
-        int numOverlapped = 0;
+        // int numStacks = 3;
+        int numStacks = 9;
+        ArrayList<ArrayDeque<String>> stacks = new ArrayList<ArrayDeque<String>>();
+        for (int i = 0; i < numStacks; i++) {
+            stacks.add(new ArrayDeque<String>());
+        }
+        boolean isMoveSection = false;
         for (String line : lines) {
             if (line.length() != 0) {
                 // Part 1
-                String[] ranges = line.split(",");
-                String range1 = ranges[0];
-                String range2 = ranges[1];
 
-                String[] range1Bounds = range1.split("\\-");
-                int range1Lo = Integer.parseInt(range1Bounds[0]);
-                int range1Hi = Integer.parseInt(range1Bounds[1]);
+                if (isMoveSection == false) {
+                    // stack configuration section
+                    int fromIndex = 0;
+                    while ((fromIndex = line.indexOf("[", fromIndex)) > -1) {
+                        fromIndex++;
+                        int stackIndex = fromIndex / 4;
+                        ArrayDeque<String> stack = stacks.get(stackIndex);
+                        stack.addFirst(line.substring(fromIndex, fromIndex + 1));
+                    }
+                    continue;
+                }
 
-                String[] range2Bounds = range2.split("\\-");
-                int range2Lo = Integer.parseInt(range2Bounds[0]);
-                int range2Hi = Integer.parseInt(range2Bounds[1]);
+                printStacks(stacks);
 
-                if (range1Lo <= range2Lo && range1Hi >= range2Hi
-                    || range2Lo <= range1Lo && range2Hi >= range1Hi) {
-                    numFullyContained++;
+                // move section
+                String[] words = line.split(" ");
+                int quantity = Integer.parseInt(words[1]);
+                int sourceIndex = Integer.parseInt(words[3]) - 1;
+                int destIndex = Integer.parseInt(words[5]) - 1;
+                ArrayDeque<String> sourceStack = stacks.get(sourceIndex);
+                ArrayDeque<String> destStack = stacks.get(destIndex);
+                for (int n = 0; n < quantity; n++) {
+                    destStack.addLast(sourceStack.removeLast());
                 }
 
                 // Part 2
-                if (!(range1Lo < range2Lo && range1Hi < range2Lo)
-                    && !(range1Lo > range2Hi && range1Hi > range2Hi)) {
-                        numOverlapped++;
-                }
+
+                continue;
             }
+            isMoveSection = true;
+        }
+
+        printStacks(stacks);
+
+        StringBuilder message = new StringBuilder();
+        for (int i = 0; i < numStacks; i++) {
+            ArrayDeque<String> stack = stacks.get(i);
+            message.append(stack.removeLast());
         }
 
         System.out.println("# lines = " + lines.size());
-        System.out.println("numFullyContained = " + numFullyContained);
-        System.out.println("numOverlapped for part2 = " + numOverlapped);
+        System.out.println("message = " + message.toString());
+    }
+
+    static void printStacks(ArrayList<ArrayDeque<String>> stacks) {
+        int numStacks = stacks.size();
+        for (int i = 0; i < numStacks; i++) {
+            System.out.print("stack[" + (i + 1) + "] = ");
+            Object[] stack = stacks.get(i).toArray();
+            for (int j = 0; j < stack.length; j++) {
+                System.out.print(stack[j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
