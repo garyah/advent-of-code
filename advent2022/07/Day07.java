cad8846d1ac22a061df28a69340dde264e55ac21
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2022-12-07 00:23:15 -0800

    Day 7 part 1 done.

--- advent2022/07/Day07.java
+++ advent2022/07/Day07.java
@@ -2,8 +2,120 @@ import java.io.*;
 import java.nio.charset.*;
 import java.nio.file.*;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class Day07 {
+    static Map<String, Object> createDir(String dirName) {
+        Map<String, Object> newDir = new HashMap<String, Object>();
+        newDir.put("0size", 0);
+        newDir.put("0name", dirName);
+        return newDir;
+    }
+    static Map<String, Object> changeParentDir(Map<String, Object> currDir, ArrayDeque<Map<String, Object>> pathStack) {
+        Integer currDirSize = (Integer)(currDir.get("0size"));
+        Map<String, Object> parentDir = pathStack.removeLast();
+        int parentNewSize = updateSize(parentDir, currDirSize);
+        System.out.println("for parentDir = " + parentDir + ", new size = " + parentNewSize);
+        return parentDir;
+    }
+    static Map<String, Object> changeDir(Map<String, Object> currDir, String dirName, ArrayDeque<Map<String, Object>> pathStack) {
+        if (currDir != null) {
+            pathStack.addLast(currDir);
+            if (currDir.containsKey(dirName)) {
+                System.out.println("been here before! dirName = " + dirName);
+                return (Map<String, Object>)(currDir.get(dirName));
+            }
+        }
+        Map<String, Object> newDir = createDir(dirName);
+        if (currDir != null) currDir.put(dirName, newDir);
+        return newDir;
+    }
+    static int updateSize(Map<String, Object> dir, int fileOrDirSize) {
+        Integer dirSize = (Integer)(dir.get("0size"));
+        dirSize += fileOrDirSize;
+        dir.put("0size", dirSize);
+        // System.out.println("for dir = " + dir + ", new size = " + dirSize);
+        return dirSize;
+    }
+    static int walkTree(Map<String, Object> rootDir, int maxSize) {
+        int totalSize = 0;
+        int rootDirSize = (Integer)(rootDir.get("0size"));
+        if (rootDirSize <= maxSize) {
+            totalSize = rootDirSize;
+            System.out.println("dir passes filter, totalSize set to " + totalSize + ", rootDir = " + rootDir);
+        }
+        for (Entry<String, Object> entry : rootDir.entrySet()) {
+            if (entry.getKey().startsWith("0")) continue;
+            Map<String, Object> dir = (Map<String, Object>)(entry.getValue());
+            totalSize += walkTree(dir, maxSize);
+            System.out.println("totalSize increased to " + totalSize + ", rootDir = " + rootDir);
+        }
+        System.out.println("totalSize returned as " + totalSize + ", rootDir = " + rootDir);
+        return totalSize;
+    }
+    public static void main(String[] args) throws IOException {
+        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\07\\sample_input.txt");
+        Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\07\\input.txt");
+        List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
+        // boolean isPart2 = true;
+        int level = 0;
+        Map<String, Object> currDir = null;
+        ArrayDeque<Map<String, Object>> pathStack = new ArrayDeque<Map<String, Object>>();
+
+        // read file
+        for (String line : lines) {
+            if (line.length() != 0) {
+                String[] fields = line.split(" ");
+                if (line.startsWith("$ ")) {
+                    // commands
+                    if (line.contains("cd ")) {
+                        // change dir
+                        if (line.contains("..")) {
+                            // go up one level of dir
+                            currDir = changeParentDir(currDir, pathStack);
+                            level--;
+                            continue;
+                        }
+                        // go down one level of dir
+                        currDir = changeDir(currDir, fields[2], pathStack);
+                        level++;
+                        continue;
+                    }
+                    if (line.contains("ls ")) {
+                        // list dir
+                        continue;
+                    }
+                    continue;
+                }
+
+                // results
+                if (line.startsWith("dir ")) {
+                    // directory
+                    continue;
+                }
+                // file
+                int fileSize = Integer.parseInt(fields[0]);
+                updateSize(currDir, fileSize);
+
+                // Part 1
+
+            }
+        }
+
+        // navigate back to root
+        System.out.println("pathStack.size() = " + pathStack.size());
+        while (!pathStack.isEmpty()) {
+            currDir = changeParentDir(currDir, pathStack);
+        }
+
+        // walk whole tree, to get total of all sizes below a certain size
+        int totalSize = walkTree(currDir, 100000);
+
+        System.out.println("# lines = " + lines.size());
+        System.out.println("file ended at level = " + level);
+        System.out.println("totalSize = " + totalSize);
+    }
+
     // Basic data structures:
     // Deque<Integer> maxSums = new ArrayDeque<Integer>();
     // List<Integer> sums = new ArrayList<Integer>();
@@ -44,41 +156,4 @@ public class Day07 {
     //     System.out.print(stack[j] + " ");
     // }
     // System.out.println();
-
-    public static void main(String[] args) throws IOException {
-        Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\07\\sample_input.txt");
-        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\07\\input.txt");
-        List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
-        boolean isPart2 = true;
-        int numDistinct = isPart2 ? 14 : 4;
-        int foundIndex = -1;
-        for (String line : lines) {
-            if (line.length() != 0) {
-                // Part 1
-
-                boolean isMarker = false;
-                int i = 0;
-                for (; i <= line.length() - numDistinct; i++) {
-                    isMarker = true;
-                    for (int j = i; j < i + numDistinct; j++) {
-                        int k = i;
-                        for (; k < i + numDistinct; k++) {
-                            if (j == k) continue;
-                            if (line.charAt(j) == line.charAt(k)) break;
-                        }
-                        if (k < i + numDistinct && (line.charAt(j) == line.charAt(k))) {
-                            isMarker = false;
-                            break;
-                        }
-                    }
-                    if (isMarker) break;
-                }
-                if (isMarker) foundIndex = i + numDistinct;
-
-            }
-        }
-
-        System.out.println("# lines = " + lines.size());
-        System.out.println("foundIndex = " + foundIndex);
-    }
 }
