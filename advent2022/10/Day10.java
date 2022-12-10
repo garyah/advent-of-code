655e1b11497d9d0d1b68b0c8f746d0331893095e
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2022-12-09 22:25:20 -0800

    Day 10 part 2 done, with code cleanup.

--- advent2022/10/Day10.java
+++ advent2022/10/Day10.java
@@ -3,92 +3,26 @@ import java.nio.charset.*;
 import java.nio.file.*;
 import java.util.*;
 
-// class Coordinates {
-//     public int x;
-//     public int y;
-//     Coordinates(int x, int y) {
-//         this.x = x;
-//         this.y = y;
-//     }
-//     boolean equals(Coordinates other) {
-//         return (this.x == other.x && this.y == other.y);
-//     }
-// }
-
 public class Day10 {
-    static Set<String> visited;
-    static int xHead;
-    static int yHead;
-    static int xTail;
-    static int yTail;
-    static int numTailVisited;
-    static boolean isHeadUp() {
-        return xHead == xTail && yHead > yTail;
-    }
-    static boolean isHeadUpAndRight() {
-        return xHead > xTail && yHead > yTail;
-    }
-    static boolean isHeadRight() {
-        return xHead > xTail && yHead == yTail;
-    }
-    static boolean isHeadDownAndRight() {
-        return xHead > xTail && yTail > yHead;
-    }
-    static boolean isHeadDown() {
-        return xHead == xTail && yTail > yHead;
-    }
-    static boolean isHeadDownAndLeft() {
-        return xTail > xHead && yTail > yHead;
-    }
-    static boolean isHeadLeft() {
-        return xTail > xHead && yTail == yHead;
-    }
-    static boolean isHeadUpAndLeft() {
-        return xTail > xHead && yHead > yTail;
-    }
-    static void updatePositions(String command) {
-        int xDelta = command.equals("R") ? 1 : (command.equals("L") ? -1 : 0);
-        int yDelta = command.equals("U") ? 1 : (command.equals("D") ? -1 : 0);
-        if (isHeadUp() && yDelta == 1 || isHeadDown() && yDelta == -1) {
-            yTail += yDelta;
-        } else if (isHeadRight() && xDelta == 1 || isHeadLeft() && xDelta == -1) {
-            xTail += xDelta;
-        } else if (isHeadUpAndRight() && (yDelta == 1 || xDelta == 1)) {
-            xTail += 1;
-            yTail += 1;
-        } else if (isHeadDownAndRight() && (yDelta == -1 || xDelta == 1)) {
-            xTail += 1;
-            yTail -= 1;
-        } else if (isHeadDownAndLeft() && (yDelta == -1 || xDelta == -1)) {
-            xTail -= 1;
-            yTail -= 1;
-        } else if (isHeadUpAndLeft() && (yDelta == 1 || xDelta == -1)) {
-            xTail -= 1;
-            yTail += 1;
-        }
-        String tailCoordinates = new String(xTail + "," + yTail);
-        xHead += xDelta;
-        yHead += yDelta;
-        if (!visited.contains(tailCoordinates)) {
-            // System.out.println("newly visited: head x = " + xHead + " y = " + yHead + ", tail x = " + xTail + " y = " + yTail);
-            numTailVisited++;
-            visited.add(tailCoordinates);
-        }
-        // System.out.println("after: head x = " + xHead + " y = " + yHead + ", tail x = " + xTail + " y = " + yTail);
-    }
-
-
     static int cycleNum;
     static int xRegValue;
     static int totalSignalStrength;
     static void tick() {
+        // Part 2
+        int delta = Math.abs((cycleNum % 40) - xRegValue);
+        // System.out.println("cycleNum % 40 = " + cycleNum % 40 + ", xRegValue = " + xRegValue + ", delta = " + delta);
+        System.out.print((delta <= 1) ? "#" : ".");
+        if (cycleNum > 0 && cycleNum % 40 == 39) System.out.println();
+
         cycleNum += 1;
+
+        // Part 1
         if (cycleNum == 20 || ((cycleNum - 20) % 40 == 0 && cycleNum <= 220)) {
             int signalStrength = cycleNum * xRegValue;
             totalSignalStrength += signalStrength;
-            System.out.println(
-                "cycleNum = " + cycleNum + ", xRegValue = " + xRegValue + ", signalStrength = " + signalStrength
-                 + ", new totalSignalStrength = " + totalSignalStrength);
+            // System.out.println(
+            //     "cycleNum = " + cycleNum + ", xRegValue = " + xRegValue + ", signalStrength = " + signalStrength
+            //      + ", new totalSignalStrength = " + totalSignalStrength);
         }
     }
 
@@ -112,9 +46,6 @@ public class Day10 {
                 }
                 if (instruction.equals("addx")) {
                     int argument = Integer.parseInt(fields[1]);
-                    if (argument == -1) {
-                        System.out.println("before addx -1, cycleNum = " + cycleNum + ", xRegValue = " + xRegValue);
-                    }
                     tick();
                     tick();
                     xRegValue += argument;
@@ -129,8 +60,7 @@ public class Day10 {
 
         System.out.println("cycleNum = " + cycleNum);
         System.out.println("xRegValue = " + xRegValue);
-
-        // Part 2
+        System.out.println("totalSignalStrength = " + totalSignalStrength);
     }
 
     void snippets() {
