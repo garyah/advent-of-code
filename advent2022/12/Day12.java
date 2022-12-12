9c57e558ea81b16f80300937b4ce802955f23e53
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2022-12-12 01:29:06 -0800

    Day 12 part 1, can solve example, but not real input. Something goes astray with Dijkstra, where next node selected from priority queue has infinite weight and causes overflow condition.

--- advent2022/12/Day12.java
+++ advent2022/12/Day12.java
@@ -2,26 +2,152 @@ import java.io.*;
 import java.nio.charset.*;
 import java.nio.file.*;
 import java.util.*;
+import java.text.DecimalFormat;
 
 public class Day12 {
+    static int numRows;
+    static int numCols;
+    static int numPoints;
+    static char[][] grid;
+    static int startRow;
+    static int startCol;
+    static int endRow;
+    static int endCol;
+    static long[] pathWeight;
+    static int[] previous;
+    static DecimalFormat decimalFormat = new DecimalFormat("00000000000000000000");
+    static PriorityQueue<String> remaining;
     static boolean isPart2;
 
-    public static void main(String[] args) throws IOException {
-        Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\12\\sample_input.txt");
-        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\12\\input.txt");
-        List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
-        isPart2 = true;
+    static void printGrid() {
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                if (row == startRow && col == startCol) {
+                    System.out.print("S");
+                    continue;
+                }
+                if (row == endRow && col == endCol) {
+                    System.out.print("E");
+                    continue;
+                }
+                System.out.print((char)(grid[row][col] + 'a'));
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    static List<Integer> getAdjacentPoints(int pointIdx) {
+        List<Integer> adjacentPoints = new ArrayList<Integer>();
+        int row = pointIdx / numCols;
+        int col = pointIdx % numCols;
 
-        // read file
-        for (String line : lines) {
-            if (line.length() != 0) {
-                // String[] fields = line.split(" ");
-                // if (line.contains("Monkey ")) {
-                // }
+        // end point has nothing adjacent
+        if (row == endRow && col == endCol) return adjacentPoints;
+
+        if (col > 0 && grid[row][col - 1] <= grid[row][col] + 1) adjacentPoints.add(pointIdx - 1);
+        if (col < numCols - 1 && grid[row][col + 1] <= grid[row][col] + 1) adjacentPoints.add(pointIdx + 1);
+        if (row > 0 && grid[row - 1][col] <= grid[row][col] + 1) adjacentPoints.add(pointIdx - numCols);
+        if (row < numRows - 1 && grid[row + 1][col] <= grid[row][col] + 1) adjacentPoints.add(pointIdx + numCols);
+        return adjacentPoints;
+    }
+    static void initDijkstra() {
+        pathWeight = new long[numPoints];
+        previous = new int[numPoints];
+        remaining = new PriorityQueue<String>();
+
+        int pointIdx = 0;
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++, pointIdx++) {
+                if (row == startRow && col == startCol) {
+                    pathWeight[pointIdx] = 0;
+                    String point = decimalFormat.format(0) + "," + decimalFormat.format(pointIdx);
+                    // System.out.println(point);
+                    remaining.add(point);
+                    continue;
+                }
+                pathWeight[pointIdx] = Long.MAX_VALUE;
+                String point = decimalFormat.format(Long.MAX_VALUE) + "," + decimalFormat.format(pointIdx);
+                // System.out.println(point);
+                remaining.add(point);
             }
         }
+    }
+    static boolean dijkstra() {
+        initDijkstra();
+        while (!remaining.isEmpty()) {
+            String point = remaining.remove();
+            System.out.print(point + ": ");
+            String[] fields = point.split(",");
+            int pointIdx = Integer.parseInt(fields[1]);
+            for (int adjacentPointIdx : getAdjacentPoints(pointIdx)) {
+                System.out.print(adjacentPointIdx + " ");
 
-        System.out.println("# lines = " + lines.size());
+                long candidateWeight = pathWeight[pointIdx] + 1;
+                if (candidateWeight < 0) {
+                    System.out.println("Overflow detected, pointIdx = " + pointIdx + ", candidateWeight = " + candidateWeight);
+                    return false;
+                    // throw new Exception("Overflow");
+                }
+                long currentWeight = pathWeight[adjacentPointIdx];
+                if (candidateWeight < currentWeight) {
+                    pathWeight[adjacentPointIdx] = candidateWeight;
+                    previous[adjacentPointIdx] = pointIdx;
+
+                    String adjacentPoint = decimalFormat.format(currentWeight) + "," + decimalFormat.format(adjacentPointIdx);
+                    remaining.remove(adjacentPoint);
+                    adjacentPoint = decimalFormat.format(candidateWeight) + "," + decimalFormat.format(adjacentPointIdx);
+                    remaining.add(adjacentPoint);
+                }
+            }
+            System.out.println();
+        }
+        return true;
+    }
+    public static void main(String[] args) throws IOException {
+        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\12\\sample_input.txt");
+        Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\12\\input.txt");
+        List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
+        numRows = lines.size();
+        numCols = lines.get(0).length();
+        numPoints = numRows * numCols;
+        grid = new char[numRows][numCols];
+        // isPart2 = true;
+
+        // read file
+        int row = 0;
+        for (String line : lines) {
+            // if (lineLength != 0) {
+            // }
+            for (int i = 0; i < line.length() && i < numCols; i++) {
+                char c = line.charAt(i);
+                if (c == 'S') {
+                    startRow = row;
+                    startCol = i;
+                    c = 'a';
+                }
+                if (c == 'E') {
+                    endRow = row;
+                    endCol = i;
+                    c = 'z';
+                }
+                grid[row][i] = (char)(c - 'a');
+            }
+            row++;
+        }
+
+        System.out.println("# lines = " + lines.size() + ", numRows = " + numRows + ", numCols = " + numCols + ", numPoints = " + numPoints);
+
+        // print grid
+        printGrid();
+
+        // run Dijkstra on the grid
+        if (!dijkstra()) {
+            System.out.println("Overflow detected");
+            return;
+        }
+        int endPointIdx = endRow * numCols + endCol;
+        long minNumSteps = pathWeight[endPointIdx];
+        System.out.println("minNumSteps = " + minNumSteps);
     }
 
     void snippets() {
