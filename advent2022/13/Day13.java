f16d31b517e789ae1bc5348a80d5ee9da28f2d07
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2022-12-14 17:30:37 -0800

    Day 13 part 1 tentatively done, no parser and compare doesn't fully work yet.

--- advent2022/13/Day13.java
+++ advent2022/13/Day13.java
@@ -7,19 +7,148 @@ import java.text.DecimalFormat;
 public class Day13 {
     // static boolean isPart2;
 
+    static List<Object> fromString(String string) {
+        // String[] numberStrings = string.replace("[", "").replace("]", "").split(",");
+        String[] numberStrings = string.split(",");
+        // int result[] = new int[strings.length];
+        // for (int i = 0; i < result.length; i++) {
+        //     result[i] = Integer.parseInt(strings[i]);
+        // }
+        List<Object> result = new ArrayList<Object>();
+        for (String numberString : numberStrings) {
+            result.add(Integer.parseInt(numberString));
+        }
+        return result;
+    }
+    static List<Object> parseLine(String line) {
+        List<Object> parseResult = new ArrayList<Object>();
+        return parseResult;
+    }
+    static int compareObjects(Object firstObj, Object secondObj) {
+        // compare two numbers
+        if (firstObj.getClass() == Integer.class && secondObj.getClass() == Integer.class) {
+            Integer firstNum = (Integer)firstObj;
+            Integer secondNum = (Integer)secondObj;
+            return Integer.compare(firstNum, secondNum); // right order when this returns < 0, indicating first less than right
+        }
+
+        // compare number with list or list with number, need to convert number to list
+        if (firstObj.getClass() == Integer.class && secondObj.getClass() == ArrayList.class) {
+            // compare number with list
+            List<Object> tempList = new ArrayList<Object>();
+            tempList.add(firstObj);
+            firstObj = tempList;
+        }
+        else if (firstObj.getClass() == ArrayList.class && secondObj.getClass() == Integer.class) {
+            // compare list with number
+            List<Object> tempList = new ArrayList<Object>();
+            tempList.add(secondObj);
+            secondObj = tempList;
+        }
+
+        // at this point, must be compare two lists
+        List<Object> firstList = (List<Object>)firstObj;
+        List<Object> secondList = (List<Object>)secondObj;
+        if (firstList.size() < secondList.size()) return -1; // right order
+        if (firstList.size() > secondList.size()) return 1; // wrong order
+
+        // compare two lists, number of elements same
+        int index = 0;
+        int numElems = firstList.size();
+        for (; index < numElems; index++) {
+            Object firstElem = firstList.get(index);
+            Object secondElem = secondList.get(index);
+            int elemCompare = compareObjects(firstElem, secondElem);
+            if (elemCompare != 0) return elemCompare;
+        }
+        return 0;
+    }
+    static int comparePackets(List<Object> firstPacket, List<Object> secondPacket) {
+        if (firstPacket.size() < secondPacket.size()) return -1; // right order
+        if (firstPacket.size() > secondPacket.size()) return 1; // wrong order
+        int result = compareObjects(firstPacket, secondPacket);
+        if (result == 0) {
+            System.out.println("Packets cannot be compared, firstPacket = " + firstPacket + ", secondPacket = " + secondPacket);
+        }
+        return result;
+    }
     public static void main(String[] args) throws IOException {
         Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\13\\sample_input.txt");
         // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\13\\input.txt");
         List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
         // isPart2 = true;
+        int sumCorrectOrderIndices = 0;
 
         // read file
+        boolean isFirst = true;
+        List<Object> firstPacket = null;
+        List<Object> secondPacket = null;
+        int currentIndex = 1;
         for (String line : lines) {
             if (line.length() != 0) {
+                if (isFirst) {
+                    firstPacket = parseLine(line);
+                    isFirst = false;
+                    continue;
+                }
+                // !isFirst (second packet)
+                secondPacket = parseLine(line);
+                isFirst = true;
+                if (comparePackets(firstPacket, secondPacket) == -1) {
+                    sumCorrectOrderIndices += currentIndex;
+                }
+                currentIndex++;
             }
         }
 
         System.out.println("# lines = " + lines.size());
+
+        System.out.println("currentIndex = " + currentIndex);
+        System.out.println("sumCorrectOrderIndices = " + sumCorrectOrderIndices);
+
+        int result;
+        int expectedResult;
+        List<Object> tempList1;
+
+        firstPacket = new ArrayList<Object>();
+        firstPacket.add(1);
+        firstPacket.add(1);
+        firstPacket.add(3);
+        firstPacket.add(1);
+        firstPacket.add(1);
+        secondPacket = new ArrayList<Object>();
+        secondPacket.add(1);
+        secondPacket.add(1);
+        secondPacket.add(5);
+        secondPacket.add(1);
+        secondPacket.add(1);
+        result = comparePackets(firstPacket, secondPacket);
+        System.out.println("firstPacket = " + firstPacket);
+        System.out.println("secondPacket = " + secondPacket);
+        System.out.println("result = " + result + (result < 0 ? " (right order)" : " (wrong order)"));
+        expectedResult = -1;
+        System.out.println("test 1 " + (result == expectedResult ? "PASS" : "FAIL"));
+
+        firstPacket = new ArrayList<Object>();
+        tempList1 = new ArrayList<Object>();
+        tempList1.add(1);
+        firstPacket.add(tempList1);
+        tempList1 = new ArrayList<Object>();
+        tempList1.add(2);
+        tempList1.add(3);
+        tempList1.add(4);
+        firstPacket.add(tempList1);
+        secondPacket = new ArrayList<Object>();
+        tempList1 = new ArrayList<Object>();
+        tempList1.add(1);
+        secondPacket.add(tempList1);
+        secondPacket.add(4);
+        result = comparePackets(firstPacket, secondPacket);
+        System.out.println("firstPacket = " + firstPacket);
+        System.out.println("secondPacket = " + secondPacket);
+        System.out.println("result = " + result + (result < 0 ? " (right order)" : " (wrong order)"));
+        expectedResult = -1;
+        System.out.println("test 1 " + (result == expectedResult ? "PASS" : "FAIL"));
     }
 
     void snippets() {
