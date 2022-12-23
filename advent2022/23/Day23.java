9f446d4197d673e0a52f9e31785add3f0f8ff09b
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2022-12-23 14:36:17 -0800

    Prep day 23 from still empty day 16, with sample input filled in.

---
+++ advent2022/23/Day23.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.nio.charset.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class Day23 {
+    // static boolean isPart2;
+
+    public static void main(String[] args) throws IOException {
+        Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\23\\sample_input.txt");
+        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2022\\23\\input.txt");
+        List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
+        // isPart2 = true;
+
+        // read file
+        for (String line : lines) {
+            if (line.length() != 0) {
+            }
+        }
+
+        System.out.println("# lines = " + lines.size());
+    }
+
+    void snippets() {
+        // Basic data structures:
+        // Deque<Integer> maxSums = new ArrayDeque<Integer>();
+        // List<Integer> sums = new ArrayList<Integer>();
+        // ArrayList<ArrayDeque<String>> stacks = new ArrayList<ArrayDeque<String>>();
+
+        // Use of Integer.parseInt():
+        // sum += Integer.parseInt(line);
+
+        // Use of replaceAll() and split():
+        // String[] fields = line.replaceAll("\\s+$", "").split(" ");
+
+        // Use of substring():
+        // String comp1 = line.substring(0, line.length() / 2);
+        // String comp2 = line.substring(line.length() / 2, line.length());
+
+        // Use of indexOf():
+        // if (comp2.indexOf(duplicate) > -1) {
+        //     break;
+        // }
+
+        // Ranges in input:
+        // String[] ranges = line.split(",");
+        // String range1 = ranges[0];
+        // String range2 = ranges[1];
+        // String[] range1Bounds = range1.split("\\-");
+        // int range1Lo = Integer.parseInt(range1Bounds[0]);
+        // int range1Hi = Integer.parseInt(range1Bounds[1]);
+        // String[] range2Bounds = range2.split("\\-");
+        // int range2Lo = Integer.parseInt(range2Bounds[0]);
+        // int range2Hi = Integer.parseInt(range2Bounds[1]);
+
+        // Moving from one deque to another, as stack:
+        // destStack.addLast(sourceStack.removeLast());
+
+        // Using toArray to convert to array, print it out:
+        // Object[] stack = stacks.get(i).toArray();
+        // for (int j = 0; j < stack.length; j++) {
+        //     System.out.print(stack[j] + " ");
+        // }
+        // System.out.println();
+
+        // Part 2 flagging:
+        // boolean isPart2 = true;
+
+        // Perform set number of rounds:
+        // int interval = isPart2 ? 1000 : 1;
+        // int numRounds = isPart2 ? 10000 : 20;
+        // for (int round = 1; round <= numRounds; round++) {
+        //     if (round == 1 || round == 20 || round % interval == 0) System.out.println("Round: " + round);
+        //     performRound();
+        //     // if (!isPart2) printWorryLevels();
+        //     // if (isPart2) printWorryLevelMods();
+        //     if (round == 1 || round == 20 || round % interval == 0) printNumInspects();
+        // }
+
+        // Rows and columns to make a greid:
+        // numRows = lines.size();
+        // numCols = lines.get(0).length();
+        // numPoints = numRows * numCols;
+        // grid = new char[numRows][numCols];
+    }
+}
