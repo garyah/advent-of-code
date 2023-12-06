73745b0688c6f780ec15eb03d974d846a841798a
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2023-12-05 20:56:35 -0800

    Work in progress on day 5, part 2.

--- advent2023/05/Day05.java
+++ advent2023/05/Day05.java
@@ -4,14 +4,15 @@ import java.nio.file.*;
 import java.util.*;
 
 class Day05Answer {
-    long closest;
+    long closestP1;
+    long closestP2;
 }
 
 public class Day05 {
     public static void main(String[] args) throws IOException {
-        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\05\\sample_input.txt");
+        Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\05\\sample_input.txt");
         // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\05\\sample_input2.txt");
-        Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\05\\input.txt");
+        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\05\\input.txt");
         List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
         int nRows = lines.size();
         System.out.println("# lines / nRows = " + nRows);
@@ -21,11 +22,11 @@ public class Day05 {
 
         Day05Answer answer = solve(lines, scanner);
 
-        // Part 1: closest location
-        System.out.println("part 1: closest location = " + answer.closest);
+        // Part 1: closest location, with discrete list of seeds
+        System.out.println("part 1: closest location = " + answer.closestP1);
 
-        // Part 2: ...
-        System.out.println("part 2: ... = " + answer);
+        // Part 2: closest location, with seed list describing ranges
+        System.out.println("part 2: closest location = " + answer.closestP2);
     }
 
     private static Day05Answer solve(List<String> lines, Scanner scanner) {
@@ -35,14 +36,117 @@ public class Day05 {
         List<List<Long[]>> sections = parseSections(scanner);
 
         // Part 1
-        answer.closest = findClosest(seeds, sections);
+        answer.closestP1 = findClosestP1(seeds, sections);
 
         // Part 2
+        Map<Long, Long> seedMap = seedListToMap(seeds);
+        List<Map<Long, Long[]>> sectionMaps = sectionsListToMap(sections);
+        answer.closestP2 = findClosestP2(seedMap, sectionMaps);
 
         return answer;
     }
 
-    private static long findClosest(List<Long> seeds, List<List<Long[]>> sections) {
+    private static long findClosestP2(Map<Long, Long> seedMap, List<Map<Long, Long[]>> sectionMaps) {
+        long target = -1;
+
+        Map<Long, Long[]> lastSectionMap = sectionMaps.get(sectionMaps.size() - 1);
+        for (Map.Entry<Long, Long[]> entry : lastSectionMap.entrySet()) {
+            target = entry.getKey();
+            Long[] srcValues = entry.getValue();
+            int i = sectionMaps.size() - 2;
+            for (; i >= 0; i--) {
+                Map<Long, Long[]> nextSectionMap = sectionMaps.get(i);
+                boolean canContinue = false;
+                for (Map.Entry<Long, Long[]> nextEntry : nextSectionMap.entrySet()) {
+                    long nextTarget = nextEntry.getKey();
+                    Long[] nextSrcValues = nextEntry.getValue();
+                    if (srcValues[0] >= nextTarget
+                        && srcValues[0] + srcValues[1] <= nextTarget + nextSrcValues[1]) {
+                        srcValues[0] = nextSrcValues[0];
+                        srcValues[1] = nextSrcValues[1];
+                        canContinue = true;
+                        break;
+                    }
+                }
+                if (!canContinue) break;
+            }
+            if (i < 0) {
+                for (Map.Entry<Long, Long> seedEntry : seedMap.entrySet()) {
+                    long seedStart = seedEntry.getKey();
+                    long seedDelta = seedEntry.getValue();
+                    if (srcValues[0] >= seedStart
+                        && srcValues[0] + srcValues[1] <= seedStart + seedDelta) {
+                        ;
+                    }
+                }
+                break;
+            }
+        }
+
+        return target;
+    }
+
+    private static Map<Long, Long> seedListToMap(List<Long> seeds) {
+        Map<Long, Long> seedMap = new TreeMap<>();
+
+        for (int i = 0; i < seeds.size(); i += 2) {
+            seedMap.put(seeds.get(i), seeds.get(i + 1));
+        }
+
+        return seedMap;
+    }
+
+    private static List<Map<Long, Long[]>> sectionsListToMap(List<List<Long[]>> sections) {
+        List<Map<Long, Long[]>> sectionMaps = new ArrayList<>();
+
+        for (List<Long[]> section : sections) {
+            Map<Long, Long[]> sectionMap = new TreeMap<>();
+            for (Long[] rangeValues : section) {
+                Long[] srcValues = new Long[] {rangeValues[1], rangeValues[2]};
+                sectionMap.put(rangeValues[0], srcValues);
+            }
+
+            // copy keys, before iterating with map modification
+            List<Long> sectionKeys = new ArrayList<>();
+            for (long key : sectionMap.keySet()) {
+                sectionKeys.add(key);
+            }
+
+            // fixup map, for gaps in mapping
+            long dest1Value = sectionKeys.get(0);
+            if (dest1Value > 0) {
+                System.out.println("gap in front!");
+                Long[] nextSrcValues = new Long[] {0L, dest1Value};
+                sectionMap.put(0L, nextSrcValues);
+            }
+            int i = 0;
+            for (; i < sectionKeys.size() - 1; i++) {
+                dest1Value = sectionKeys.get(i);
+                Long[] src1Values = sectionMap.get(dest1Value);
+                long nextDestValue = dest1Value + src1Values[1];
+                long dest2Value = sectionKeys.get(i + 1);
+                if (nextDestValue < dest2Value) {
+                    System.out.println("gap in middle!");
+                    Long[] nextSrcValues = new Long[] {nextDestValue, dest2Value - nextDestValue};
+                    sectionMap.put(nextDestValue, nextSrcValues);
+                }
+            }
+            dest1Value = sectionKeys.get(i);
+            Long[] src1Values = sectionMap.get(dest1Value);
+            long nextDestValue = dest1Value + src1Values[1];
+            if (nextDestValue <= Long.MAX_VALUE) {
+                System.out.println("gap in back!");
+                Long[] nextSrcValues = new Long[] {nextDestValue, Long.MAX_VALUE - nextDestValue};
+                sectionMap.put(nextDestValue, nextSrcValues);
+            }
+
+            sectionMaps.add(sectionMap);
+        }
+
+        return sectionMaps;
+    }
+
+    private static long findClosestP1(List<Long> seeds, List<List<Long[]>> sections) {
         long closest = Integer.MAX_VALUE;
 
         for (long value : seeds) {
@@ -79,7 +183,7 @@ public class Day05 {
     }
 
     private static List<List<Long[]>> parseSections(Scanner scanner) {
-        List<List<Long[]>> sections = new ArrayList<List<Long[]>>();
+        List<List<Long[]>> sections = new ArrayList<>();
 
         String[] sectionTitles = {
             "seed-to-soil",
@@ -93,20 +197,20 @@ public class Day05 {
         int i = 0;
         for (String sectionTitle : sectionTitles) {
             sections.add(new ArrayList<>());
-            processSection(scanner, sectionTitle, sections.get(i++));
+            parseSection(scanner, sectionTitle, sections.get(i++));
         }
 
         return sections;
     }
 
-    private static void processSection(Scanner scanner, String sectionTitle, List<Long[]> sectionValues) {
+    private static void parseSection(Scanner scanner, String sectionTitle, List<Long[]> sectionValues) {
         // System.out.println(sectionTitle);
         scanner.next(sectionTitle);
         scanner.next("map:");
         scanner.nextLine();
         while (scanner.hasNextLong()) {
-            Long[] values = new Long[] {scanner.nextLong(), scanner.nextLong(), scanner.nextLong()};
-            sectionValues.add(values);
+            Long[] rangeValues = new Long[] {scanner.nextLong(), scanner.nextLong(), scanner.nextLong()};
+            sectionValues.add(rangeValues);
             scanner.nextLine();
         }
         if (scanner.hasNextLine()) scanner.nextLine();
