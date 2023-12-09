5bc44da6b0593eaca09aa0579f06e7fe9f209ea7
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2023-12-08 22:24:28 -0800

    Prep day 10.

---
+++ advent2023/10/Day10.java
@@ -0,0 +1,128 @@
+import java.io.*;
+import java.nio.charset.*;
+import java.nio.file.*;
+import java.util.*;
+
+class Day10Answer {
+    // long sumExtrapolatedP1;
+    // long sumExtrapolatedP2;
+}
+
+public class Day10 {
+    public static void main(String[] args) throws IOException {
+        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\10\\sample_input.txt");
+        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\10\\sample_input2.txt");
+        Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\10\\input.txt");
+        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\05\\input.txt");
+        List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
+        int nRows = lines.size();
+        System.out.println("# lines / nRows = " + nRows);
+        // int nCols = lines.get(0).length();
+        // System.out.println("first line length / nCols = " + nCols);
+        Scanner scanner = new Scanner(myPath);
+
+        Day10Answer answer = solve(lines, scanner);
+
+        // Part 1: ...
+        // System.out.println("part 1: ... = " + answer.sumExtrapolatedP1);
+
+        // Part 2: ...
+        // System.out.println("part 2: ... = " + answer.sumExtrapolatedP2);
+    }
+
+    private static Day10Answer solve(List<String> lines, Scanner scanner) {
+        Day10Answer answer = new Day10Answer();
+        
+        // List<List<Long>> histories = parseHistories(scanner);
+
+        // Part 1
+        // answer.sumExtrapolatedP1 = findSumExtrapolatedP1(histories);
+
+        // Part 2
+        // answer.sumExtrapolatedP2 = findSumExtrapolatedP2(histories);
+
+        return answer;
+    }
+
+    private static List<List<Long>> parseHistories(Scanner scanner) {
+        List<List<Long>> histories = new ArrayList<>();
+
+        scanner.useDelimiter("[ \\r]+");
+        while (scanner.hasNextLine()) {
+            List<Long> history = new ArrayList<>();
+            while (scanner.hasNextLong()) {
+                Long value = scanner.nextLong();
+                // System.out.print(value + " ");
+                history.add(value);
+            }
+            histories.add(history);
+            scanner.nextLine();
+            // System.out.println();
+        }
+
+        return histories;
+    }
+
+    void snippets() {
+        // Basic data structures:
+        // Deque<Integer> maxSums = new ArrayDeque<Integer>();
+        // List<Integer> sums = new ArrayList<Integer>();
+        // ArrayList<ArrayDeque<String>> stacks = new ArrayList<ArrayDeque<String>>();
+
+        // Use of Integer.parseInt():
+        // sum += Integer.parseInt(line);
+
+        // Use of replaceAll() and split():
+        // String[] fields = line.replaceAll("\\s+$", "").split(" ");
+
+        // Use of substring():
+        // String comp1 = line.substring(0, line.length() / 2);
+        // String comp2 = line.substring(line.length() / 2, line.length());
+
+        // Use of indexOf():
+        // if (comp2.indexOf(duplicate) > -1) {
+        //     break;
+        // }
+
+        // Ranges in input:
+        // String[] ranges = line.split(",");
+        // String range1 = ranges[0];
+        // String range2 = ranges[1];
+        // String[] range1Bounds = range1.split("\\-");
+        // int range1Lo = Integer.parseInt(range1Bounds[0]);
+        // int range1Hi = Integer.parseInt(range1Bounds[1]);
+        // String[] range2Bounds = range2.split("\\-");
+        // int range2Lo = Integer.parseInt(range2Bounds[0]);
+        // int range2Hi = Integer.parseInt(range2Bounds[1]);
+
+        // Moving from one deque to another, as stack:
+        // destStack.addLast(sourceStack.removeLast());
+
+        // Using toArray to convert to array, print it out:
+        // Object[] stack = stacks.get(i).toArray();
+        // for (int j = 0; j < stack.length; j++) {
+        //     System.out.print(stack[j] + " ");
+        // }
+        // System.out.println();
+
+        // Part 2 flagging:
+        // boolean isPart2 = true;
+
+        // Perform set number of rounds:
+        // int interval = isPart2 ? 1000 : 1;
+        // int numRounds = isPart2 ? 10000 : 20;
+        // for (int round = 1; round <= numRounds; round++) {
+        //     if (round == 1 || round == 20 || round % interval == 0) System.out.println("Round: " + round);
+        //     performRound();
+        //     // if (!isPart2) printWorryLevels();
+        //     // if (isPart2) printWorryLevelMods();
+        //     if (round == 1 || round == 20 || round % interval == 0) printNumInspects();
+        // }
+
+        // Rows and columns to make a grid:
+        // numRows = lines.size();
+        // numCols = lines.get(0).length();
+        // numPoints = numRows * numCols;
+        // grid = new char[numRows][numCols];
+    }
+}
