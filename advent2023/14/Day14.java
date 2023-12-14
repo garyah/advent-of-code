5aeaa25c1944225618de3b0a2e6b5fb7b3721dfa
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2023-12-13 17:14:57 -0800

    Prep day 14.

---
+++ advent2023/14/Day14.java
@@ -0,0 +1,174 @@
+import java.io.*;
+import java.nio.charset.*;
+import java.nio.file.*;
+import java.util.*;
+
+class Day13Answer {
+    long patternsSummaryP1;
+    // long sumShortestPathsP2;
+}
+
+public class Day14 {
+    public static void main(String[] args) throws IOException {
+        Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\14\\sample_input.txt");
+        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\14\\sample_input2.txt");
+        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\14\\input.txt");
+        // Path myPath = Paths.get("C:\\Users\\garya\\ws\\advent-of-code\\advent2023\\05\\input.txt");
+        List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
+        int nRows = lines.size();
+        System.out.println("# lines / nRows = " + nRows);
+        // int nCols = lines.get(0).length();
+        // System.out.println("first line length / nCols = " + nCols);
+        // Scanner scanner = new Scanner(myPath);
+
+        Day13Answer answer = solve(lines/*, nRows, nCols, scanner*/);
+
+        // Part 1: Summary of pattern reflections
+        // 24923 is too low!!!
+        // 594123 is too high!!!
+        System.out.println("part 1: Summary of pattern reflections = " + answer.patternsSummaryP1);
+
+        // Part 2: Sum of shortest paths
+        // System.out.println("part 2: Sum of shortest paths, with much more expansion = " + answer.sumShortestPathsP2);
+    }
+
+    private static Day13Answer solve(List<String> lines/*, int nRows, int nCols, Scanner scanner*/) {
+        Day13Answer answer = new Day13Answer();
+        
+        List<List<String>> patterns = parseInput(lines);
+
+        // Part 1
+        answer.patternsSummaryP1 = findAnswerP1(patterns);
+
+        // Part 2
+        // answer.sumShortestPathsP2 = findAnswerP2(input);
+
+        return answer;
+    }
+
+    private static long findAnswerP1(List<List<String>> patterns) {
+        long patternsSummaryP1 = 0;
+
+        // for (int i = 0; i < patterns.size(); i++) {
+        //     List<String> pattern = patterns.get(i);
+
+        //     patternsSummaryP1 += summarizePattern(pattern);
+        // }
+
+        return patternsSummaryP1;
+    }
+
+    private static String reverse(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            char c = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(j));
+            sb.setCharAt(j, c);
+        }
+        return sb.toString();
+    }
+
+    private static List<List<String>> parseInput(
+        List<String> lines/*, int nRows, int nCols, */
+        // Scanner scanner
+    ) {
+        List<List<String>> patterns = new ArrayList<>();
+        List<String> pattern = new ArrayList<>();
+
+        for (String line : lines) {
+            if (line.length() != 0) {
+                pattern.add(line);
+                // System.out.println(line);
+                continue;
+            }
+            if (pattern.size() != 0) {
+                patterns.add(pattern);
+                pattern = new ArrayList<>();
+            }
+            // System.out.println();
+        }
+
+        if (pattern.size() != 0) {
+            patterns.add(pattern);
+        }
+        return patterns;
+
+        // scanner.useDelimiter("[ \\r]+");
+        // scanner.useDelimiter("[ ]+");
+        // while (scanner.hasNextLine()) {
+        //     while (scanner.hasNext()) {
+        //         String row = scanner.next();
+        //         // pattern.add(row);
+        //         System.out.println(row);
+        //         scanner.nextLine();
+        //     }
+        //     while (scanner.hasNextLine() && !scanner.hasNext()) {
+        //         System.out.println();
+        //         scanner.nextLine();
+        //     }
+        // }
+    }
+
+    void snippets() {
+        // Basic data structures:
+        // Deque<Integer> maxSums = new ArrayDeque<Integer>();
+        // List<Integer> sums = new ArrayList<Integer>();
+        // ArrayList<ArrayDeque<String>> stacks = new ArrayList<ArrayDeque<String>>();
+
+        // Use of Integer.parseInt():
+        // sum += Integer.parseInt(line);
+
+        // Use of replaceAll() and split():
+        // String[] fields = line.replaceAll("\\s+$", "").split(" ");
+
+        // Use of substring():
+        // String comp1 = line.substring(0, line.length() / 2);
+        // String comp2 = line.substring(line.length() / 2, line.length());
+
+        // Use of indexOf():
+        // if (comp2.indexOf(duplicate) > -1) {
+        //     break;
+        // }
+
+        // Ranges in input:
+        // String[] ranges = line.split(",");
+        // String range1 = ranges[0];
+        // String range2 = ranges[1];
+        // String[] range1Bounds = range1.split("\\-");
+        // int range1Lo = Integer.parseInt(range1Bounds[0]);
+        // int range1Hi = Integer.parseInt(range1Bounds[1]);
+        // String[] range2Bounds = range2.split("\\-");
+        // int range2Lo = Integer.parseInt(range2Bounds[0]);
+        // int range2Hi = Integer.parseInt(range2Bounds[1]);
+
+        // Moving from one deque to another, as stack:
+        // destStack.addLast(sourceStack.removeLast());
+
+        // Using toArray to convert to array, print it out:
+        // Object[] stack = stacks.get(i).toArray();
+        // for (int j = 0; j < stack.length; j++) {
+        //     System.out.print(stack[j] + " ");
+        // }
+        // System.out.println();
+
+        // Part 2 flagging:
+        // boolean isPart2 = true;
+
+        // Perform set number of rounds:
+        // int interval = isPart2 ? 1000 : 1;
+        // int numRounds = isPart2 ? 10000 : 20;
+        // for (int round = 1; round <= numRounds; round++) {
+        //     if (round == 1 || round == 20 || round % interval == 0) System.out.println("Round: " + round);
+        //     performRound();
+        //     // if (!isPart2) printWorryLevels();
+        //     // if (isPart2) printWorryLevelMods();
+        //     if (round == 1 || round == 20 || round % interval == 0) printNumInspects();
+        // }
+
+        // Rows and columns to make a grid:
+        // numRows = lines.size();
+        // numCols = lines.get(0).length();
+        // numPoints = numRows * numCols;
+        // grid = new char[numRows][numCols];
+    }
+}
